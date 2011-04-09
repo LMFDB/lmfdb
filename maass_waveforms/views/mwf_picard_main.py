cb3f6627b20dc504a4873c623e0591bb5c927028
Fredrik314 <Fredrik314@gmail.com>
2011-04-08 18:17:58 -0700

    addded some maass form files...

--- maass_waveforms/views/mwf_picard_main.py
+++ maass_waveforms/views/mwf_picard_main.py
@@ -1,8 +1,12 @@
+import flask
 from flask import render_template,url_for,request
 import bson
 import base
 
-@base.app.route("/ModularForm/GL2/C/Maass/")
+mwfp = flask.Module(__name__,'mwfp')
+
+#@base.app.route("/ModularForm/GL2/C/Maass/")
+@mwfp.route("/",methods=['GET','POST'])
 def maass_form_picard():
    htp = base.getDBConnection().HTPicard.picard
    docid = request.args.get('id', None)   
@@ -11,4 +15,4 @@ def maass_form_picard():
    data = None
    if docid:
      data = htp.find_one({'_id' : docid })
-   return render_template("maass_form_picard.html", title = "Maass cusp forms on PSL(2,Z[i])", data = data, id=docid, info=info, ds=ds)
+   return render_template("mfw/maass_form_picard.html", title = "Maass cusp forms on PSL(2,Z[i])", data = data, id=docid, info=info, ds=ds)
