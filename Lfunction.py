6d94a300363d93bb5fd9f76694fa430ece13c561
Sally Koutsoliotas <koutslts@bucknell.edu>
2011-04-29 13:05:45 -0700

    Colour dot picture added

--- Lfunction.py
+++ Lfunction.py
@@ -347,7 +347,10 @@ def render_plotLfunction(args):
 
 def render_browseGraph(args):
     print args
-    data = LfunctionPlot.paintSvgFile(args['group'], int(args['level']), args['sign'])
+    if 'sign' in args:
+      data = LfunctionPlot.paintSvgFileAll([[args['group'], int(args['level']), args['sign']]])
+    else:
+      data = LfunctionPlot.paintSvgFileAll([[args['group'], int(args['level'])]])
     response = make_response(data)
     response.headers['Content-type'] = 'image/svg+xml'
     return response
