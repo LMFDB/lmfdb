92f3c2dabe0e568af95e3bdbe7b77fcd5a8d0a72
Harald Schilly <harald.schilly@gmail.com>
2011-08-19 20:15:51 +0200

    knowledge database: some initial ideas, macro and context_processor variable is ready; outfitting one l-function template with the necessary macro calls; many loose ends

--- knowledge/knowl.py
+++ knowledge/knowl.py
@@ -1,6 +1,40 @@
 # -*- coding: utf-8 -*-
 # the basic knowlege object, with database awareness, …
+from base import getDBConnection
+_C = getDBConnection()
 
+
+def get_knowl(kid, fields=None):
+  if fields:
+    return _C.knowledge.knowls.find_one({'_id' : kid}, fields=fields)
+  return _C.knowledge.knowls.find_one({'_id' : kid})
 
 class Knowl(object):
-  pass
+  def __init__(self, *args, **kwargs):
+    self.args = args
+    self.kwargs = kwargs
+    self._id = args[0]
+
+  @property
+  def id(self):
+    return self._id
+
+  @property
+  def content(self):
+    return "this is the \(\LaTeX\) aware content."
+
+  @property
+  def title(self):
+    """
+    This just returns the "title" string, which is exactly the one
+    that will be visible in the websites. 
+    Example: KNOWL('algebra.dirichlet_series') should be replaced
+    with "Dirichlet Series" and nothing else. 
+
+    TODO: Since this happens several times for each page, it's
+    godsend for implementing memcache optimization.
+    """
+    return get_knowl(self._id, fields=["title"])
+
+  def __unicode__(self):
+    return "args: %s, kwargs: %s" % (self.args, self.kwargs)
--- knowledge/knowledge.py
+++ knowledge/knowledge.py
@@ -10,11 +10,34 @@ import flask
 from base import app, getDBConnection
 from flask import render_template, request, abort, Blueprint, url_for
 from flaskext.login import login_required, current_user
+from knowl import Knowl
 
 knowledge_page = Blueprint("knowledge", __name__, template_folder='templates')
 
 @knowledge_page.route("/edit/<ID>")
 @login_required
 def edit(ID):
-  return "ID %s" % ID
+  knowl = Knowl(ID)
+  return render_template("knowl_edit.html", 
+         title="Edit Knowl '%s'" % ID,
+         K = knowl)
 
+@knowledge_page.route("/edit", methods=["POST"])
+@login_required
+def edit_form():
+  ID = request.form['id']
+  return flask.redirect(url_for(".edit", ID=ID))
+
+@knowledge_page.route("/save", methods=["POST"])
+@login_required
+def save_form():
+  return "not saved"
+
+@knowledge_page.route("/")
+def index():
+  return render_template("knowl_index.html", title="Knowledge Database")
+
+
+@app.context_processor
+def ctx_knowledge():
+  return {'Knowl' : Knowl}
