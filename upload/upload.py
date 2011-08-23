48bee00fca9b57d530623c56fe31cf428a138a49
Ralph Furmaniak <rfurman@stanford.edu>
2011-08-23 05:58:23 -0400

    Added file upload and moderation console

--- upload/upload.py
+++ upload/upload.py
@@ -8,9 +8,14 @@
 import logging
 import pymongo
 import flask
+import datetime
+import json
 from base import app, getDBConnection, fmtdatetime
 from flask import render_template, request, abort, Blueprint, url_for
 from flaskext.login import login_required, current_user
+from gridfs import GridFS
+from pymongo.objectid import ObjectId
+
 
 upload_page = Blueprint("upload", __name__, template_folder='templates')
 
@@ -30,11 +35,51 @@ def index():
 @upload_page.route("/upload", methods = ["POST"])
 @login_required
 def upload():
-  name = request.form['name']
-  you = current_user.name
-  from datetime import datetime
-  current_time = datetime.utcnow()
-  flask.flash("Upload Form: name = %s, user = %s, time = %s" % (name, you, fmtdatetime(current_time)))
+  metadata = {
+    "name": request.form['name'],
+    "full_description": request.form['full_description'],
+    "data_format": request.form['data_format'],
+    "creator": request.form['creator'],
+    "reference": request.form['reference'],
+    "bibtex": request.form['bibtex'],
+    "uploader": current_user.name,
+    "time": str(datetime.datetime.utcnow()),
+    "original_file_name": request.files['file'].filename,
+    "status": "unmoderated",
+    "version": "1"
+  }
+  flask.flash("Received file: " + request.files['file'].filename)
+
+  upload_db = getDBConnection().upload
+  upload_fs = GridFS(upload_db)
+  upload_fs.put(request.files['file'].read(), metadata = metadata, filename=request.files['file'].filename)
+  
   return flask.redirect(url_for(".index"))
 
+@upload_page.route("/admin", methods = ["POST"])
+def admin_update():
 
+  db = getDBConnection().upload
+  id = request.form['id']
+
+  if request.form.has_key('approve'):
+    db.fs.files.update({"_id" : ObjectId(id)}, {"$set": {"metadata.status" : "approved"}})
+    flask.flash('Approved')
+  if request.form.has_key('disapprove'):
+    db.fs.files.update({"_id" : ObjectId(id)}, {"$set": {"metadata.status" : "disapproved"}})
+    flask.flash('Disapproved')
+
+  return flask.redirect(url_for(".admin"))
+
+
+@upload_page.route("/admin", methods = ["GET"])
+def admin():
+
+  db = getDBConnection().upload
+  fs = GridFS(db)
+
+  unmoderated = [ fs.get(x['_id']) for x in db.fs.files.find({"metadata.status" : "unmoderated"}) ]
+  approved = [ fs.get(x['_id']) for x in db.fs.files.find({"metadata.status" : "approved"}) ]
+  disapproved = [ fs.get(x['_id']) for x in db.fs.files.find({"metadata.status" : "disapproved"}) ]
+
+  return render_template("upload-admin.html", title = "Data Upload", bread = get_bread(), unmoderated=unmoderated, approved=approved, disapproved=disapproved)
