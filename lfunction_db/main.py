90b9f5041dd836329780754eda51980cf9d88aac
Harald Schilly <harald.schilly@gmail.com>
2011-08-26 18:33:53 +0200

    various small fixes

--- lfunction_db/main.py
+++ lfunction_db/main.py
@@ -8,6 +8,7 @@ from copy import copy
 import pymongo
 
 mod = flask.Blueprint('LfunctionDB', __name__, template_folder="templates")
+title = "First Zero Search Example"
 
 @mod.context_processor
 def body_class():
@@ -31,7 +32,7 @@ def zero_search(**kwargs):
         #        printed_arrow = True
         #    result_string = result_string + str(x['zero']) + " " + str(x['modulus']) + " " + str(x['character']) + "<br>\n"
         #return result_string
-    return render_template('lf-list.html', pagination=pagination)
+    return render_template('lf-list.html', pagination=pagination, title=title)
 
 @mod.route("/query")
 def query(**kwargs):
@@ -68,4 +69,4 @@ def query(**kwargs):
 
     query = C.test.Lfunctions_test2.find(filter).sort(sort, direction)
     pagination = LazyMongoDBPagination(query = query, per_page=50, page=request.args.get('page', 1), endpoint=".query", endpoint_params=dict(request.args))
-    return render_template('lf-list.html', pagination=pagination)
+    return render_template('lf-list.html', pagination=pagination, title=title)
