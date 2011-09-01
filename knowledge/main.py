8c69ee8d0a524f35904e4979e508140a3538ee00
Harald Schilly <harald.schilly@gmail.com>
2011-09-01 22:58:55 +0100

    knowl details, style, breadcrumb

--- knowledge/main.py
+++ knowledge/main.py
@@ -98,7 +98,8 @@ def edit(ID):
 def show(ID):
   k = Knowl(ID)
   r = render(ID, footer="0")
-  b = get_bread([('Wiki', url_for('.show', ID='wiki.index')), ('%s'%k.id, url_for('.show', ID=ID))])
+  b = get_bread([#('Wiki', url_for('.show', ID='wiki.index')), 
+                 ('%s'%k.title, url_for('.show', ID=ID))])
   return render_template("knowl-show.html",
          title = k.title,
          k = k,
