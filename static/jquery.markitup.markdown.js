318695b8bfd0248645e46fbf711e4bfbdeac3236
Ralph Furmaniak <rfurman@stanford.edu>
2011-09-05 13:19:06 +0100

    More compact upload viewer, markdown editor fix to make links more natural

--- static/jquery.markitup.markdown.js
+++ static/jquery.markitup.markdown.js
@@ -31,7 +31,7 @@ myMarkdownSettings = {
 		{separator:'---------------' },
 		{name:'Picture', key:'P', replaceWith:'![[![Alternative text]!]]([![Url:!:http://]!] "[![Title]!]")'},
 		{name:'Knowl', key:'K', replaceWith:"{{ KNOWL('[![Knowl Reference]!]') }}"},
-		{name:'Link', key:'L', openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")', placeHolder:'Your text to link here...' },
+		{name:'Link', key:'L', openWith:'[', closeWith:']([![Url:!:http://]!])', placeHolder:'Your text to link here...' },
 		{separator:'---------------'},	
 		{name:'Quotes', openWith:'> '},
 		{name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'},
