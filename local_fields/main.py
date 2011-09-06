4fa12cc29c91f15dafa4e12ee3f6cb19edddc760
John Jones <jj@asu.edu>
2011-09-06 09:56:44 -0700

    Added table for browsing local fields by prime and degree, results given as a list of fields.

--- local_fields/main.py
+++ local_fields/main.py
@@ -5,8 +5,10 @@
 import pymongo
 ASC = pymongo.ASCENDING
 import flask
+import base
 from base import app, getDBConnection
 from flask import render_template, render_template_string, request, abort, Blueprint, url_for, make_response
+from utils import ajax_more, image_src, web_latex, to_dict, parse_range, coeff_to_poly, pol_to_html
 from local_fields import local_fields_page, logger, logger
 
 def get_bread(breads = []):
@@ -18,6 +20,9 @@ def get_bread(breads = []):
 @local_fields_page.route("/")
 def index():
   bread = get_bread()
+  if len(request.args) != 0:
+    return local_field_search(**request.args)
+  #info['learnmore'] = [('Number Field labels', url_for("render_labels_page")), ('Galois group labels',url_for("render_groups_page")), ('Discriminant ranges',url_for("render_discriminants_page"))]
   return render_template("lf-index.html", title ="Local Fields", bread = bread)
 
 @local_fields_page.route("/search", methods = ["GET", "POST"])
@@ -25,12 +30,29 @@ def search():
   if request.method == "GET":
     val = request.args.get("val", "no value")
     bread = get_bread([("Search for '%s'" % val, url_for('.search'))])
-    return render_template("lf-search.html", title="Local Fields Search", bread = bread, val = val)
+    return render_template("lf-search.html", title="Local Field Search", bread = bread, val = val)
   elif request.method == "POST":
     return "ERROR: we aloways do http get to explicitly display the search parameters"
   else:
     return flask.redirect(404)
 
 
+def local_field_search(**args):
+  info = to_dict(args)
+  bread = get_bread()
+  C = base.getDBConnection()
+  query = {}
+  for param in ['p', 'n']:
+    if info.get(param):
+      ran = info[param]
+      ran = ran.replace('..','-')
+      query[param] = parse_range(ran)
 
+
+  res = C.localfields.fields.find(query).sort([('p',pymongo.ASCENDING),('n',pymongo.DESCENDING),('c',pymongo.ASCENDING)])
+  nres = res.count()
+  info['fields'] = res
+  info['report'] = "found %s fields"%nres
+
+  return render_template("lf-plain.html", info = info, title="Local Field Search Result", bread=bread)
   
