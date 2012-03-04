e99beb0341bf35f6a732dcac2cafd3612014907b
Jonathan Bober <jwbober@gmail.com>
2012-03-04 15:04:18 -0800

    First shot at adding Platt's zeros.

--- website.py
+++ website.py
@@ -25,6 +25,7 @@ import galois_groups
 import number_field_galois_groups
 import OEIS
 import artin_representations
+import zeros
 
 import raw
 from modular_forms.maass_forms.picard import mwfp
---
+++ zeros/main.py
@@ -0,0 +1,18 @@
+from base import app
+import flask
+from flask import render_template, request
+
+mod = flask.Blueprint('zeros', __name__, template_folder="templates")
+title = "zeros"
+
+@mod.context_processor
+def body_class():
+  return { 'body_class' : 'LfunctionDB' }
+
+@mod.route("/")
+def default_route():
+    return ""
+
+@mod.route("/zeta")
+def query(**kwargs):
+    pass
