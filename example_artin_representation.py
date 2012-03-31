a90d0765c7281a996fc834da3dab4e0ab1714272
Paul-Olivier Dehaye <paulolivier@gmail.com>
2012-03-31 23:47:20 +0200

    added comments for Bober

--- example_artin_representation.py
+++ example_artin_representation.py
@@ -6,15 +6,22 @@ base._init(int(37010),"")
 # x is raw
 # a is of type ArtinRepresentation
 
+# all the artin reps that come out of this are safe to appear on the website
+# their L functions need to be fixed however
+
 # if you want to do complex queries, and sort using pymongo's interface:
 for x in ArtinRepresentation.collection().find():
     a = ArtinRepresentation(data = x)
     print a
-    print a.Lfunction()
+    try:
+        print a.Lfunction()
+    except NotImplementedError or SyntaxError:
+        print "Need CYC types and sign"
     
 # if you want to list them all, using a wrapper to directly convert to ArtinRepresentation 
 for a in ArtinRepresentation.find():
     print a
-    print a.Lfunction()
-    
-
+    try:
+        print a.Lfunction()
+    except NotImplementedError or SyntaxError:
+        print "Need CYC types"
