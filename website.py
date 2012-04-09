703270e87f0cd6dd81e8ae336e25d5cae3ff1866
Harald Schilly <harald.schilly@gmail.com>
2012-04-09 23:52:33 +0200

    issue 28: breadcrumbs for intro pages resolved; added /roadmap with stub; renamed intro.py to pages.py because it is more general for single pages

--- website.py
+++ website.py
@@ -4,7 +4,7 @@ add --debug if you are developing (auto-restart, full stacktrace in browser, ...
 """
 from base import *
 
-import intro
+import pages 
 import hilbert_modular_form
 import siegel_modular_form
 import modular_forms
