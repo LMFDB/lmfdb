e1a6095c9dd8bf035da672b4cc0e8d8cdf5baae7
Harald Schilly <harald.schilly@gmail.com>
2012-04-16 15:03:16 +0200

    copyright note in some headers

--- base.py
+++ base.py
@@ -1,4 +1,11 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf8 -*-
+# LMFDB - L-function and Modular Forms Database web-site - www.lmfdb.org
+# Copyright (C) 2010-2012 by the LMFDB authors
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
 
 import sys
 import logging
