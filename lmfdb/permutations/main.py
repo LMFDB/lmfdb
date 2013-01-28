9c4a1f9bb8643cb6630f7d5ef3aca77b77036946
Sébastien Labbé <slabqc@gmail.com>
2013-01-28 21:32:33 +0100

    improved permutations security issue, URL and error page on bad input

--- lmfdb/permutations/main.py
+++ lmfdb/permutations/main.py
@@ -9,7 +9,7 @@ import os
 import re
 from lmfdb.permutations import permutations_page, logger
 import sage.all
-from sage.all import Permutation, DiGraph
+from sage.all import Permutation, Integer
 
 def get_bread(breads=[]):
     bc = [("Permutations", url_for(".index"))]
@@ -17,24 +17,47 @@ def get_bread(breads=[]):
         bc.append(b)
     return bc
 
+@permutations_page.route("/show", methods = ["POST"])
+def parse_and_redirect():
+    r"""
+    This gets called when the user submit some input in the data box of the
+    follwing page:
+
+    http://127.0.0.1:37777/Permutations/
+
+    It then redirects to the appropriate permutation page.
+    """
+    assert request.method == "POST", "request.method is assumed to be POST"
+    data = str(request.form.get('data', ''))
+    data = data.replace(',','.')
+    return redirect(url_for(".show", data=data))
+
 @permutations_page.route("/show", methods = ["GET"])
 def show():
+    r"""
+    This gets called when an adress of that kind gets loaded:
+
+    http://127.0.0.1:37777/Permutations/show?data=3.4.2.1
+    """
+    assert request.method == "GET", "request.method is assumed to be GET"
+    data = str(request.args.get('data', ''))
     try:
-        data = eval(request.args.get('data', None))
-    except SyntaxError:
-        logger.info("Impossible to parse the input.")
-        raise
-    try:
+        data = map(Integer, data.split('.'))
         p = Permutation(data)
-    except ValueError:
+    except (TypeError, ValueError):
         logger.info("Impossible to create a permutation from input.")
-        raise
-    digraph_plot = DiGraph(p.to_matrix()).plot()
+        flask.flash("Ooops, impossible to create a permutation from given input!", "error")
+        return flask.redirect(url_for(".index"))
     return render_template("permutations.html", permutation=p,
-            digraph_plot=digraph_plot, rankbread=get_bread())
+            rankbread=get_bread())
 
 @permutations_page.route("/")
 def index():
+    r"""
+    This gets called when this adress gets loaded:
+
+    http://127.0.0.1:37777/Permutations/
+    """
     bread = get_bread()
     return render_template("permutations-index.html", title="Permutations", bread=bread)
 
