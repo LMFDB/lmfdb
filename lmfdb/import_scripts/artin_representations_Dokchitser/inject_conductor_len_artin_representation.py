848384e90a36a5441a36030728f76008ac535a2a
Paul-Olivier Dehaye <paulolivier@gmail.com>
2013-05-16 11:10:23 +0200

    Moved all the import scripts for artin reps in the import_scripts/ folder

---
+++ lmfdb/import_scripts/artin_representations_Dokchitser/inject_conductor_len_artin_representation.py
@@ -0,0 +1,24 @@
+# Adds a field called field_len to objects in the_collection. It is based on the field called field
+# It encodes the len of the string used
+# This is kind of a hack for mongodb, see lmfdb.utils.len_val_fn for explanation
+
+import sys
+LMFDB_FOLDER = "../../../"
+sys.path.append(LMFDB_FOLDER)
+import lmfdb.base as base
+base._init(37010, "")
+from lmfdb.math_classes import ArtinRepresentation
+the_collection = ArtinRepresentation.collection()
+print "Got", the_collection
+
+field = "Conductor"
+
+from lmfdb.utils import len_val_fn
+
+new_field = field + "_plus"
+
+print "Updating"
+for x in the_collection.find():
+    val = x[field]
+    x[new_field] = len_val_fn(val)
+    the_collection.save(x)
