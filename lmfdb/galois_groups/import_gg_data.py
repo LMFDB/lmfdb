c2a48b19e0c73f36ca72491b114367956dbef359
John Jones <jj@asu.edu>
2013-09-17 10:37:58 -0700

    Improving galois group import script

--- lmfdb/galois_groups/import_gg_data.py
+++ lmfdb/galois_groups/import_gg_data.py
@@ -4,6 +4,8 @@ import time
 import bson
 import sage.all
 from sage.all import *
+import os
+import json
 
 sys.path.append("../")
 import base
@@ -14,57 +16,15 @@ import pymongo
 
 gr = C.transitivegroups.groups
 
+labels = ['label', 'n', 't', 'auts', 'order', 'parity', 'ab', 'prim', 'cyc', 'solv', 'subs', 'repns', 'resolve', 'name', 'pretty']
 
-saving = True
+gps = gr.find({})
+myfile = open('gg_data')
+
+for l in myfile:
+  v= json.loads(l)
+  data = {}
+  for j in range(len(v)):
+    data[labels[j]] = v[j]
 
 
-def make_label(n, t):
-    return str(n) + 'T' + str(t)
-
-# Data comes as a list of lists
-from outlist import li  # this reads in the list called li
-
-tot = len(li)
-print "finished importing li, number = %s" % tot
-count = 0
-
-n = 0
-for F in li:
-    n += 1
-    for g in F:
-        count += 1
-        print "%d" % count
-        t, order, parity, auts, name, res, other, subs = g
-        lab = make_label(n, t)
-        g1 = gr.find_one({'label': lab})
-        order = str(order)
-
-        if g1 is None:
-            print "new group %s" % lab
-            myg = gap.TransitiveGroup(n, t)
-            ab = 1 if myg.IsAbelian() else 0
-            cyc = 1 if myg.IsCyclic() else 0
-            prim = 1 if myg.IsPrimitive() else 0
-            solv = 1 if myg.IsSolvable() else 0
-            info = {
-                'label': lab,
-                'n': n,
-                't': t,
-                'auts': auts,
-                'order': order,
-                'parity': parity,
-                'ab': ab,
-                'prim': prim,
-                'cyc': cyc,
-                'solv': solv,
-                'subs': subs,
-                'repns': other,
-                'resolve': res,
-                'pretty': None,
-                'name': name
-            }
-            if saving:
-                print "Saved"
-                gr.save(info)
-        else:
-            print "group %s already in database" % lab
