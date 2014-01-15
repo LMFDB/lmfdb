5b1d138dc627ac78ae6c190e3299ec7f1b605611
Martin Dickson <martin.dickson34@gmail.com>
2014-01-15 18:32:53 +0100

    Updated interface for tensor products

--- lmfdb/tensor_products/main.py
+++ lmfdb/tensor_products/main.py
@@ -12,10 +12,13 @@ from lmfdb.utils import to_dict
 from lmfdb.transitive_group import *
 from string import split
 from sets import Set
+from sage.schemes.elliptic_curves.constructor import EllipticCurve
 
 from lmfdb.math_classes import *
 from lmfdb.WebNumberField import *
 from tensor_products_defs import TensorProduct
+from galois_reps import GaloisRepresentation
+from lmfdb.WebCharacter import *
 
 def get_bread(breads=[]):
     bc = [("Tensor products", url_for(".index"))]
@@ -25,37 +28,66 @@ def get_bread(breads=[]):
 
 @tensor_products_page.route("/")
 def index():
+    bread = get_bread()
+    return render_template("tensor_products_index.html", title="Tensor products", bread=bread)
+
+@tensor_products_page.route("/navigate/")
+def navigate():
     args = request.args
     bread = get_bread()
-    if len(args)==0:
-        return render_template("tensor_products_index.html", title="Tensor products", bread=bread)
+
+    type1 = args.get('type1')
+    type2 = args.get('type2')
+    return render_template("tensor_products_navigate.html", title="Tensor products navigation", bread=bread, type1=type1 , type2=type2)
+
+@tensor_products_page.route("/show/")
+def show():
+    args = request.args
+    bread = get_bread()
+
+    objLinks = args # an immutable dict of links to objects to tp
+
+    objPaths = []
+    for k, v in objLinks.items():
+        objPaths.append(v.split('/')) 
+
+    galoisRepObjs = []
+    for p in objPaths:
+        galoisRepObjs.append(galois_rep_from_path(p))
+
+    # currently only implemented tp of two things
+    if len(galoisRepObjs)==2:
+#         tp = mult(galoisRepObjs[0], galoisRepObjs[1]) # form the multiplication in the galois reps class, which implements tensor product
+
+#         info = {'conductor':tp.conductor()}
+
+#         properties2 = {'Conductor':info['conductor']}
+            
+#         friends = []
+#         friends.append(('L function', ''))
+#         friends.append(('Elliptic Curve %s' % obj1[2], url_for("ec.by_ec_label", label=obj1[2])))
+#         friends.append(('Dirichlet Character $\chi_{%s}(%s, \cdot)$' % (obj2[2], obj2[3]), url_for("characters.render_Dirichletwebpage", modulus=int(obj2[2]), number=int(obj2[3])) ))
+#         friends.append(('L-function for Elliptic Curve %s' % obj1[2], url_for("l_functions.l_function_ec_page", label=obj1[2])))
+#         friends.append(('L-function for Dirichlet Character $\chi_{%s}(%s, \cdot)$' % (obj2[2], obj2[3]), url_for("l_functions.l_function_dirichlet_page", modulus = int(obj2[2]), number=int(obj2[3])) ))
+            
+#         t = "Tensor product of Elliptic Curve %s and Dirichlet Character $\chi_{%s}(%s, \cdot)$" % (obj1[2], obj2[2], obj2[3])
+
+        return render_template("tensor_products_show.html", title='', bread=bread, info=[], friends=[])
+
     else:
-        obj1link = args.get('obj1')
-        obj2link = args.get('obj2')
-        obj1 = obj1link.split('/')
-        obj2 = obj2link.split('/')
-        obj1type = obj1[0]
-        obj2type = obj2[0]
+        return render_template("not_yet_implemented.html")
 
-        if (obj1type, obj2type)==('EllipticCurve', 'Character'):
-            tp = TensorProduct(obj1[2], int(obj2[2]), int(obj2[3]))
-            info = {'conductor':tp.conductor()}
-
-            properties2 = {'Conductor':info['conductor']}
-            
-            friends = []
-            friends.append(('L function', ''))
-            friends.append(('Elliptic Curve %s' % obj1[2], url_for("ec.by_ec_label", label=obj1[2])))
-            friends.append(('Dirichlet Character $\chi_{%s}(%s, \cdot)$' % (obj2[2], obj2[3]), url_for("characters.render_Dirichletwebpage", modulus=int(obj2[2]), number=int(obj2[3])) ))
-            friends.append(('L-function for Elliptic Curve %s' % obj1[2], url_for("l_functions.l_function_ec_page", label=obj1[2])))
-            friends.append(('L-function for Dirichlet Character $\chi_{%s}(%s, \cdot)$' % (obj2[2], obj2[3]), url_for("l_functions.l_function_dirichlet_page", modulus = int(obj2[2]), number=int(obj2[3])) ))
-            
-            t = "Tensor product of Elliptic Curve %s and Dirichlet Character $\chi_{%s}(%s, \cdot)$" % (obj1[2], obj2[2], obj2[3])
-        else:
-            tp = None
-
-        if tp:
-            return render_template("tensor_products_show.html", title=t, bread=bread, info=info, friends=friends)
-        else:
-            return render_template("not_yet_implemented.html")
-
+def galois_rep_from_path(p):
+    C = getDBConnection()
+    if p[0]=='EllipticCurve':
+        # create the sage elliptic curve then create Galois rep object
+        data = C.elliptic_curves.curves.find_one({'lmfdb_label':p[2]})
+        ainvs = [int(a) for a in data['ainvs']]
+        E = EllipticCurve(ainvs)
+        return GaloisRepresentation(E)
+    elif (p[0]=='Character' and p[1]=='Dirichlet'):
+        dirichletArgs = {'type':'Dirichlet', 'modulus':int(p[2]), 'number':int(p[3])}
+        chi = WebDirichletCharacter(**dirichletArgs)
+        return GaloisRepresentation(chi) 
+    else:
+        return
