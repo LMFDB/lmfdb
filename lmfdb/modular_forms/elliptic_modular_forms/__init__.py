5cacd2e256b1754b4f3758b75c03d2200bed99b4
Fredrik Stromberg <fredrik314@gmail.com>
2014-01-20 18:32:13 +0000

    added a different precision for displayed data than data used for computations

--- lmfdb/modular_forms/elliptic_modular_forms/__init__.py
+++ lmfdb/modular_forms/elliptic_modular_forms/__init__.py
@@ -4,7 +4,8 @@ import flask
 ## Some common definitions to use in this module.
 
 default_prec = 10   # The default number of terms in a q-expansion
-default_bprec = 26  # The default number of bits of precision to display in floating point data
+default_bprec = 53  # The default number of bits of precision to compute for floating point data
+default_display_bprec = 26  # The default number of bits of precision to display for floating point data
 EMF_TOP = "Holomorphic Modular Forms"  # The name to use for the top of this catergory
 EMF = "emf"  # The current blueprint name
 emf = flask.Blueprint(EMF, __name__, template_folder="views/templates", static_folder="views/static")
