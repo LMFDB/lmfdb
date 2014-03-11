a5e1569d6501f48f6975841ec98424c3f04e3e1d
Harald Schilly <harald.schilly@gmail.com>
2014-03-11 02:06:35 +0100

    codestyle

--- lmfdb/siegel_modular_forms/__init__.py
+++ lmfdb/siegel_modular_forms/__init__.py
@@ -3,14 +3,15 @@ from lmfdb.base import app
 from lmfdb.utils import make_logger
 from flask import Blueprint
 
-smf_page = Blueprint( 'siegel_modular_forms', __name__,
-                 template_folder = 'templates', static_folder = 'static')
-smf_logger = make_logger( smf_page)
+smf_page = Blueprint('siegel_modular_forms', __name__,
+                     template_folder='templates', static_folder='static')
+smf_logger = make_logger(smf_page)
+
 
 @smf_page.context_processor
 def body_class():
-    return { 'body_class': 'siegel_modular_forms'}
+    return {'body_class': 'siegel_modular_forms'}
 
 from siegel_modular_form import *
 
-app.register_blueprint( smf_page, url_prefix ='/ModularForm/GSp/Q')
+app.register_blueprint(smf_page, url_prefix='/ModularForm/GSp/Q')
