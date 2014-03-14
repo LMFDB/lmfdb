7ba035cdb41dfa3e2bb9fa567881b67697731d0d
Pascal <molin.maths@gmail.com>
2014-03-14 01:04:21 +0100

    new conrey scheme => kronecker has changed

--- lmfdb/test_lfunction.py
+++ lmfdb/test_lfunction.py
@@ -195,7 +195,8 @@ class LfunctionTest(LmfdbTest):
 
     def test_paintSVGchar(self):
         svg = paintSvgChar(1,20,1,12)
-        assert "/L/Character/Dirichlet/8/7" in svg
+        print svg
+        assert "/L/Character/Dirichlet/8/5" in svg
 
 ##    def test_number_of_coefficients_needed(self):
 ##        nr = number_of_coefficients_needed(1 / sqrt(math.pi),
