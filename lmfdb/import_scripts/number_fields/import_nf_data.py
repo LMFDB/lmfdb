11c31532a824122cf0e83bff1f803f43c45c4d24
John Jones <jj@asu.edu>
2014-07-30 10:43:05 -0700

    Changing how subfields of a number field are stored.  We used to use a list of coefficients of a defining polynomial stored as integers, but ran over the 8-bit limit for mongo integers.  Now the coefficients are stored as a string.

--- lmfdb/import_scripts/number_fields/import_nf_data.py
+++ lmfdb/import_scripts/number_fields/import_nf_data.py
@@ -6,7 +6,7 @@ checks if the entry exists, if so returns that and updates with
 new information. If the entry does not exist then it creates it 
 and returns that.
 
-Initial version (Warwick 2014)
+Initial version (Warwick 2014), modified 7/14
 
 Data is imported to the collection 'fields' in the database 'numberfields'.
 The structure of the database entries is described in lmfdb/Database-info.
@@ -159,7 +159,8 @@ def do_import(ll):
         label = base_label(d,sig[0],absD,index)
         data['label'] = label
         data['ramps'] = [str(x) for x in prime_factors(D)]
-        data['subfields'] = subs
+        subs = [[makels(z[0]), z[1]] for z in subs]
+        data['subs'] = subs
 ############
         if h>0:
             data['class_number'] = h
