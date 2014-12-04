97d9185905bb69c501f1d4db34c272b1aa601b23
Fredrik Stromberg <fredrik314@gmail.com>
2014-12-04 16:46:03 +0000

    added the /ranges rout here

--- lmfdb/modular_forms/elliptic_modular_forms/views/experimental/routes.py
+++ lmfdb/modular_forms/elliptic_modular_forms/views/experimental/routes.py
@@ -17,14 +17,17 @@ def test(**kwds):
 @emf.route("/Dots/<min_level>/<max_level>/<min_weight>/<max_weight>/",methods=met)
 def show_dots(min_level, max_level, min_weight, max_weight):
     info = {}
-    info['contents'] = [paintSvgHolomorphic(min_level, max_level, min_weight, max_weight)]
+    info['contents'] = [paintSvgHolomorphic(min_level, max_level, min_weight, max_weight,char=1)]
     info['min_level'] = min_level
     info['max_level'] = max_level
     info['min_weight'] = min_weight
     info['max_weight'] = max_weight
     return render_template("experimental/emf_browse_graph.html", title='Browsing dimensions of modular forms in the database', **info)
 
-      
+from lmfdb.modular_forms.elliptic_modular_forms.views.emf_render_navigation import _browse_web_modform_spaces_in_ranges
+@emf.route("/ranges", methods=["GET"])
+def browse_web_modform_spaces_in_ranges(**kwds):
+    return _browse_web_modform_spaces_in_ranges(**kwds)
 
 
       
