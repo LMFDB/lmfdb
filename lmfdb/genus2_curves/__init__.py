74a42118efc40841ed81011f46c2338ae098ce24
Dan Yasaki <d_yasaki@uncg.edu>
2015-03-24 14:41:19 +0000

    rename genus_2 to genus2

---
+++ lmfdb/genus2_curves/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from lmfdb.base import app
+from lmfdb.utils import make_logger
+from flask import Blueprint
+
+g2c_page = Blueprint("g2c", __name__, template_folder='templates', static_folder="static")
+g2c_logger = make_logger(g2c_page)
+g2c_logger.info("Initializing genus 2 curves blueprint")
+
+
+@g2c_page.context_processor
+def body_class():
+    return {'body_class': 'g2c'}
+
+import genus2_curve
+
+app.register_blueprint(g2c_page, url_prefix="/Genus2Curve/Q")
