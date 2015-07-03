c8a447eab2cfdcdf3e846df66dd2da23190f315a
Nicolas Mascot <n.a.v.mascot@warwick.ac.uk>
2015-07-03 12:20:01 +0100

    Yet more ugly workarounds to add a lengend in spite of Sage's will

--- lmfdb/ecnf/WebEllipticCurve.py
+++ lmfdb/ecnf/WebEllipticCurve.py
@@ -40,7 +40,9 @@ def make_field(label):
 def EC_R_plot(ainvs,xmin,xmax,ymin,ymax,colour,legend):
    x=var('x')
    y=var('y')
-   return implicit_plot(y**2+ainvs[0]*x*y+ainvs[2]*y-x**3-ainvs[1]*x**2-ainvs[3]*x-ainvs[4],(x,xmin,xmax),(y,ymin,ymax),plot_points=1000,aspect_ratio="automatic",color=colour)+plot(0,xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax,aspect_ratio="automatic",color=colour,legend_label=legend) # Add an extra plot outside the visible frame because implicit plots are buugy in that their legend does not show (http://trac.sagemath.org/ticket/15903) 
+   c=(xmin+xmax)/2
+   d=(xmax-xmin)
+   return implicit_plot(y**2+ainvs[0]*x*y+ainvs[2]*y-x**3-ainvs[1]*x**2-ainvs[3]*x-ainvs[4],(x,xmin,xmax),(y,ymin,ymax),plot_points=1000,aspect_ratio="automatic",color=colour)+plot(0,xmin=c-1e-5*d,xmax=c+1e-5*d,ymin=ymin,ymax=ymax,aspect_ratio="automatic",color=colour,legend_label=legend) # Add an extra plot outside the visible frame because implicit plots are buugy in that their legend does not show (http://trac.sagemath.org/ticket/15903) 
 
 def EC_nf_plot(E,base_field_gen_name):
     K = E.base_field()
