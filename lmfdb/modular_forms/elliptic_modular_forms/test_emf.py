951c4f78811b91e3a4ec05901be65374ac547c40
Fredrik Stromberg <fredrik314@gmail.com>
2015-08-03 23:27:32 +0100

    Added test for non-trivial character and for data which is not yet in the database

--- lmfdb/modular_forms/elliptic_modular_forms/test_emf.py
+++ lmfdb/modular_forms/elliptic_modular_forms/test_emf.py
@@ -41,7 +41,16 @@ class EmfTest(LmfdbTest):
         ## We also check that the L-function works
         page = self.tc.get('/L/ModularForm/GL2/Q/holomorphic/11/2/0/a/0/')
         assert '0.25384' in page.data
-        
+
+    def test_non_triv_character(self):
+        r"""
+        Check that non-trivial characters are also working.
+        """
+        page = self.tc.get("/ModularForm/GL2/Q/holomorphic/10/3/1/")                
+        assert '2\zeta_{4}q^{4}' in page.data
+        page = self.tc.get("/ModularForm/GL2/Q/holomorphic/10/3/1/a/")                
+        assert '2\zeta_{4}q^{4}' in page.data
+
         
     def test_get_args(self):
         page = self.tc.get("/ModularForm/GL2/Q/holomorphic/13/10/0/")
@@ -50,4 +59,13 @@ class EmfTest(LmfdbTest):
     def test_empty(self):
         page = self.tc.get("ModularForm/GL2/Q/holomorphic/2/12/0/")
         assert 'is empty' in page.data
+
+
+    def test_not_in_db(self):
+        page = self.tc.get("ModularForm/GL2/Q/holomorphic/20000/12/0/")
+        assert 'not available yet' in page.data
+        page = self.tc.get("ModularForm/GL2/Q/holomorphic/20000/12/0/a/")        
+        assert 'could not be found in the database' in page.data        
+
+    
 
