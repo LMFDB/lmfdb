67b55a474ead7db1d3faaa815f2f76464cf096a8
John Cremona <john.cremona@gmail.com>
2015-10-09 17:53:38 +0100

    refactor homepage link tests & add tests that the URLs are on the page

--- lmfdb/test_homepage.py
+++ lmfdb/test_homepage.py
@@ -3,7 +3,17 @@ from lmfdb.base import LmfdbTest
 import math
 import unittest2
 
+
 class HomePageTest(LmfdbTest):
+
+    def check(self,homepage,path,text):
+        assert path in homepage
+        assert text in self.tc.get(path).data
+
+    def check_external(self, homepage, path, text):
+        import urllib2
+        assert path in homepage
+        assert text in urllib2.urlopen(path).read()
 
     # All tests should pass: these are all the links in the home page as specified in index_boxes.yaml
     #
@@ -12,16 +22,12 @@ class HomePageTest(LmfdbTest):
         r"""
         Check that the links in Box 1 work.
         """
-        page = self.tc.get("/L/degree2/")
-        assert '9.53369' in page.data
-        page = self.tc.get("/EllipticCurve/Q/?conductor=1-99")
-        assert '[1, 0, 1, -14, -64]' in page.data
-        page = self.tc.get("/ModularForm/GL2/Q/Maass/")
-        assert 'The entire database consists of 16599 Maass forms.' in page.data
-        page = self.tc.get("/zeros/first/")
-        assert 'Riemann zeta function' in page.data # the interesting numbers are filled in dynamically
-        page = self.tc.get("/NumberField/?degree=2")
-        assert '"/NumberField/2.0.8.1">2.0.8.1' in page.data
+        homepage = self.tc.get("/").data
+        self.check(homepage, "/L/degree2/", '9.53369')
+        self.check(homepage, "/EllipticCurve/Q/?conductor=1-99", '[1, 0, 1, -14, -64]')
+        self.check(homepage, "/ModularForm/GL2/Q/Maass/",  'The entire database consists of 16599 Maass forms.')
+        self.check(homepage, "/zeros/first/", 'Riemann zeta function') # the interesting numbers are filled in dynamically
+        self.check(homepage, "/NumberField/?degree=2", '"/NumberField/2.0.8.1">2.0.8.1')
 
     #
     # Box 2
@@ -29,67 +35,52 @@ class HomePageTest(LmfdbTest):
         r"""
         Check that the links in Box 2 work.
         """
-        page = self.tc.get("/L/Riemann/")
-        assert r'\[\zeta(1/2) \approx -1.4603545088\]' in page.data
-        page = self.tc.get("/NumberField/3.1.23.1")
-        assert r'Regulator:<td>&nbsp;&nbsp;<td>\( 0.281199574322962 \)' in page.data
-        page = self.tc.get("/ModularForm/GL2/Q/holomorphic/1/12/1/a/")
-        assert '0.2993668' in page.data
-        page = self.tc.get("/L/ModularForm/GL2/Q/holomorphic/1/12/1/a/0/")
-        assert 'approx 0.839345512' in page.data
-        page = self.tc.get("/EllipticCurve/Q/234446/a/1")
-        assert r'y^2 + x y = x^{3} -  x^{2} - 79 x + 289' in page.data
-        page = self.tc.get("/L/EllipticCurve/Q/234446.a/")
-        assert 'L-function $L(s,E)$ for the Elliptic Curve Isogeny Class 234446.a' in page.data
+        homepage = self.tc.get("/").data
+        self.check(homepage,"/L/Riemann/",  r'\[\zeta(1/2) \approx -1.4603545088\]')
+        self.check(homepage,"/NumberField/3.1.23.1", r'Regulator:<td>&nbsp;&nbsp;<td>\( 0.281199574322962 \)')
+        self.check(homepage,"/ModularForm/GL2/Q/holomorphic/1/12/1/a/", '0.2993668')
+        self.check(homepage,"/L/ModularForm/GL2/Q/holomorphic/1/12/1/a/0/", 'approx 0.839345512')
+        self.check(homepage,"/EllipticCurve/Q/234446/a/1", r'y^2 + x y = x^{3} -  x^{2} - 79 x + 289')
+        self.check(homepage,"/L/EllipticCurve/Q/234446.a/", 'L-function $L(s,E)$ for the Elliptic Curve Isogeny Class 234446.a')
 
     # Box 3
     def test_box3(self):
         r"""
         Check that the links in Box 3 work.
         """
-        page = self.tc.get("/L/")
-        assert 'Holomorphic cusp form' in page.data
-        page = self.tc.get("/ModularForm/")
-        assert r'Maass Forms on \(\GL(2,\Q) \)' in page.data
-        page = self.tc.get("/EllipticCurve/Q/")
-        assert 'curve, label or isogeny class label' in page.data
-        page = self.tc.get("/NumberField/")
-        assert 'x^7 - x^6 - 3 x^5 + x^4 + 4 x^3 - x^2 - x + 1' in page.data
+        homepage = self.tc.get("/").data
+        self.check(homepage, "/L/", 'Holomorphic cusp form')
+        self.check(homepage, "/ModularForm/", r'Maass Forms on \(\GL(2,\Q) \)')
+        self.check(homepage, "/EllipticCurve/Q/", 'curve, label or isogeny class label')
+        self.check(homepage, "/NumberField/", 'x^7 - x^6 - 3 x^5 + x^4 + 4 x^3 - x^2 - x + 1')
 
     # Box 4
     def test_box4(self):
         r"""
         Check that the links in Box 4 work.
         """
-        page = self.tc.get("/L/degree4/MaassForm/")
-        assert 'data on L-functions associated to Maass cusp forms for GSp(4) of level 1' in page.data
-        page = self.tc.get("/EllipticCurve/Q/102/c/")
-        assert r'1 &amp; 2 &amp; 4 &amp; 4 &amp; 8 &amp; 8' in page.data
+        homepage = self.tc.get("/").data
+        self.check(homepage, "/L/degree4/MaassForm/", 'data on L-functions associated to Maass cusp forms for GSp(4) of level 1')
+        self.check(homepage, "/EllipticCurve/Q/102/c/", r'1 &amp; 2 &amp; 4 &amp; 4 &amp; 8 &amp; 8')
 
     # Box 5
     def test_box5(self):
         r"""
         Check that the links in Box 5 work.
         """
-        page = self.tc.get("/bigpicture")
-        assert 'some varieties are modular' in page.data
-        page = self.tc.get("/knowledge/")
-        assert 'Recently modified Knowls' in page.data
+        homepage = self.tc.get("/").data
+        self.check(homepage, "/bigpicture", 'some varieties are modular')
+        self.check(homepage, "/knowledge/", 'Recently modified Knowls')
 
     # Box 6
     def test_box6(self):
         r"""
         Check that the links in Box 6 work.
         """
-        import urllib2
-        page = urllib2.urlopen("https://github.com/LMFDB/lmfdb").read()
-        assert 'Modular Forms Database' in page
-        page = urllib2.urlopen("http://sagemath.org/").read()
-        assert 'mathematics software system' in page
-        page = urllib2.urlopen("http://pari.math.u-bordeaux.fr/").read()
-        assert 'PARI/GP is a widely used computer algebra system' in page
+        homepage = self.tc.get("/").data
+        self.check_external(homepage, "https://github.com/LMFDB/lmfdb", 'Modular Forms Database')
+        self.check_external(homepage, "http://sagemath.org/", 'mathematics software system')
+        self.check_external(homepage, "http://pari.math.u-bordeaux.fr/", 'PARI/GP is a widely used computer algebra system')
         # I could not get this one to work -- JEC
-        #page = urllib2.urlopen("http://magma.maths.usyd.edu.au/magma/").read()
-        #assert 'Magma is a large, well-supported software package' in page
-        page = urllib2.urlopen("https://www.python.org/").read()
-        assert 'Python Logo' in page
+        #self.check_external(homepage, "http://magma.maths.usyd.edu.au/magma/", 'Magma is a large, well-supported software package')
+        self.check_external(homepage, "https://www.python.org/", 'Python Logo')
