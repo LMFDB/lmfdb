96713d1294476c160c956e7728bb45a0bffe3fd5
Harald Schilly <harald.schilly@gmail.com>
2015-10-30 20:56:10 +0100

    api: text vs objectid _id entries work, some styling

--- lmfdb/api/api.py
+++ lmfdb/api/api.py
@@ -8,12 +8,16 @@ import lmfdb.base as base
 from datetime import datetime
 from flask import render_template, request, make_response, url_for
 from lmfdb.api import api_page, api_logger
+from bson.objectid import ObjectId
 
-### INITIALIZATION
+# caches the database information
 _databases = None
 
 
 def censor(entries):
+    """
+    hide some of the databases and collection from the public
+    """
     dontstart = ["system.", "test", "upload", "admin", "contrib"]
     censor = ["local", "userdb"]
     for entry in entries:
@@ -35,44 +39,97 @@ def init_database_info():
 def index():
     init_database_info()
     databases = _databases
-    title = "API Overview"
+    title = "LMFDB API"
     return render_template("api.html", **locals())
 
+
+@api_page.route("/<db>/<collection>/<id>")
+def api_query_id(db, collection, id):
+    return api_query(db, collection, id = id)
+
+
 @api_page.route("/<db>/<collection>")
-def api_query(db, collection):
+def api_query(db, collection, id = None):
     init_database_info()
+
+    # check what is queried for
     if db not in _databases or collection not in _databases[db]:
         return flask.abort(404)
 
-    format = request.args.get("_format", "json")
-    offset = request.args.get("_offset", 0)
+    # parsing the meta parameters _format and _offset
+    format = request.args.get("_format", "html")
+    offset = int(request.args.get("_offset", 0))
+    if offset > 10000:
+        if format != "html":
+            flask.abort(404)
+        else:
+            flask.flash("offset too large, please refine your query.", "error")
+            return flask.redirect(url_for(".api_query", db=db, collection=collection))
+
+    # preparing the actual database query q
     C = base.getDBConnection()
+    q = dict(request.args)
 
-    qargs = dict(request.args)
+    if id is not None:
+        if id.startswith("ObjectId("):
+            q["_id"] = ObjectId(id[9:-1])
+        else:
+            q["_id"] = id
+        single_object = True
+    else:
+        single_object = False
 
-    q = list(C[db][collection].find().skip(offset).limit(100))
-    for document in q:
-        document["_id"] = str(document["_id"])
-    offset += len(q)
-    qargs["_offset"] = offset
-    next = url_for(".api_query", db=db, collection=collection, **qargs)
+    q.pop("_offset", None)
+    q.pop("_format", None)
 
+    # executing the query "q" and replacing the _id in the result list
+    data = list(C[db][collection].find(q).skip(offset).limit(100))
+    for document in data:
+        oid = document["_id"]
+        if type(oid) == ObjectId:
+            document["_id"] = "ObjectId(%s)" % oid
+        elif isinstance(oid, basestring):
+            document["_id"] = str(oid)
+
+    # preparing the datastructure
+    start = offset
+    next_req = dict(request.args)
+    next_req["_offset"] = offset
+    url_args = next_req.copy()
+    query = url_for(".api_query", db=db, collection=collection, **next_req)
+    offset += len(data)
+    next_req["_offset"] = offset
+    next = url_for(".api_query", db=db, collection=collection, **next_req)
+
+    # the collected result
     data = {
         "database": db,
         "collection": collection,
         "timestamp": datetime.utcnow().isoformat(),
-        "data": q,
+        "data": data,
+        "start": start,
+        "offset": offset,
+        "query": query,
         "next": next
     }
 
-    if format == "json":
+    # display of the result (default html)
+    if format.lower() == "json":
         return flask.jsonify(**data)
-    elif format == "yaml":
+    elif format.lower() == "yaml":
         y = yaml.dump(data,
                       default_flow_style=False,
                       canonical=False,
                       allow_unicode=True)
         return flask.Response(y, mimetype='application/yaml')
-    elif format == "html":
-        return make_response(str(data))
+    else:
+        location = "%s/%s" % (db, collection)
+        title = "API - " + location
+        bc = [("API", url_for(".index")), (location, query)]
+        return render_template("collection.html",
+                               title=title,
+                               single_object=single_object,
+                               url_args = url_args,
+                               bread=bc,
+                               **data)
 
