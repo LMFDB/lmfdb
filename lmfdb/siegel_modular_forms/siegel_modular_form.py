f7d29d6af1c970adf898659feb7b997bc6dca40d
Watson Ladd <watsonbladd@gmail.com>
2015-11-04 10:37:38 -0500

    temp

--- lmfdb/siegel_modular_forms/siegel_modular_form.py
+++ lmfdb/siegel_modular_forms/siegel_modular_form.py
@@ -62,6 +62,7 @@ def ModularForm_GSp4_Q_Sp4Z_j_space(j=4, k=4):
 def ModularForm_GSp4_Q_Sp4Z_j():
     bread = [('Siegel modular forms', url_for('ModularForm_GSp4_Q_top_level')),
              ('$M_{k,j}(\mathrm{Sp}(4, \mathbb{Z}))$', '/ModularForm/GSp/Q/Sp4Z_j')]
+    error = False
     jrange = xrange(0, 11)
     krange = xrange(10, 20)
     if request.args.get('j'):
@@ -79,14 +80,15 @@ def ModularForm_GSp4_Q_Sp4Z_j():
     try:
         dimtable = dimensions.dimension_table_Sp4Z_j(krange, jrange)
     except:
-        error={'error': 'Not all dimensions are implemented at the moment. Try again with a different range'}
-        return render_template("None.html", **error);
+        error='Not all dimensions are implemented at the moment. Try again with a different range'
+        dimtable=False
     return render_template('ModularForm_GSp4_Q_Sp4Zj.html',
                            title='$M_{k,j}(\mathrm{Sp}(4, \mathbb{Z}))$',
                            bread = bread,
                            dimtable = dimtable,
                            jrange=jrange,
-                           krange=krange)
+                           krange=krange,
+                           error=error)
                     
 
 @app.route('/ModularForm/GSp/Q')
