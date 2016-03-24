1ff118162def092bb7e75f0713025c9ea2d1c214
sanni85 <samuele.anni@gmail.com>
2016-03-24 14:59:34 +0000

    more work done

--- lmfdb/lattice/main.py
+++ lmfdb/lattice/main.py
@@ -228,6 +228,36 @@ def vect_to_sym(v):
 
 
 
+def isom(A,B):
+    # First check that A is a symmetric matrix.
+    if not Matrix(A).is_symmetric():
+        return False
+    # Then check A against the viable database candidates.
+    else:
+        n=len(A[0])
+        m=len(B[0])
+        Avec=[]
+        Bvec=[]
+        for i in range(n):
+            for j in range(i,n):
+                if i==j:
+                    Avec+=[A[i][j]]
+                else:
+                    Avec+=[2*A[i][j]]
+        for i in range(m):
+            for j in range(i,m):
+                if i==j:
+                    Bvec+=[B[i][j]]
+                else:
+                    Bvec+=[2*B[i][j]]
+        Aquad=QuadraticForm(ZZ,len(A[0]),Avec)
+        Bquad=QuadraticForm(ZZ,len(B[0]),Bvec)
+        if Aquad.is_globally_equivalent_to(Bquad)==True:
+            return B
+        else:
+            return False
+
+
 @lattice_page.route('/theta_display/<label>/<number>')
 def theta_display(label, number):
     try:
