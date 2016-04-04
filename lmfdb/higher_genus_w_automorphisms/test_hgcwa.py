27df23b9ebd7039c5152c15e0a1dda8031075a7f
Jen Paulhus <jenpaulhus@gmail.com>
2016-04-04 17:18:50 -0500

    created proper browse and search functionality on index page for Higher Genus curves

--- lmfdb/higher_genus_w_automorphisms/test_hgcwa.py
+++ lmfdb/higher_genus_w_automorphisms/test_hgcwa.py
@@ -10,7 +10,7 @@ class HigherGenusWithAutomorphismsTest(LmfdbTest):
     #
     def test_url_label(self):
 		L = self.tc.get('/HigherGenus/C/aut/2.12T13.0.2-4-6')
-		assert '[ 0; 2, 4, 6 ]' in L.data
+		assert '[0;2,4,6]' in L.data
 
 
     def test_url_naturallabel(self):
@@ -19,3 +19,6 @@ class HigherGenusWithAutomorphismsTest(LmfdbTest):
 
 
 
+    def test_search_genus_group(self):
+               	L = self.tc.get('/HigherGenus/C/aut/?genus=2&trangplabel=6T1&signature=&count=20&Submit=Search')
+                assert '2 matches' in L.data
