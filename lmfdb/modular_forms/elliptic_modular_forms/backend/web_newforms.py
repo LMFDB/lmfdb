210c1090084c5ea94f0218127178f2a4bb571a99
Fredrik Stromberg <fredrik314@gmail.com>
2016-04-08 11:02:06 +0100

    fixed typo

--- lmfdb/modular_forms/elliptic_modular_forms/backend/web_newforms.py
+++ lmfdb/modular_forms/elliptic_modular_forms/backend/web_newforms.py
@@ -494,7 +494,7 @@ class WebNewForm(WebObject, CachedRepresentation):
                 prod *= K(self._coefficients[pr])
             except:
                 if hasattr(self._coefficients[pr],'vector'):
-                    if len(self._coefficients[pr],'vector') == len(K.power_basis()):
+                    if len(self._coefficients[pr].vector()) == len(K.power_basis()):
                         prod *= K(self._coefficients[pr].vector())
                     else:
                         emf_logger.debug("vec={0}".format(self._coefficients[pr].vector()))
