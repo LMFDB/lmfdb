3bfebb78103bfdd4553728e83c65a24fe14c93e2
John Jones <jj@asu.edu>
2016-04-27 15:52:19 +0100

    Build on JEC's pull request fixing download when there is a field whose class group has not been computed, and limiting the number of fields available for download.

--- lmfdb/number_fields/number_field.py
+++ lmfdb/number_fields/number_field.py
@@ -601,13 +601,18 @@ def download_search(info, res):
     Qx = PolynomialRing(QQ,'x')
     str2pol = lambda s: Qx([QQ(str(c)) for c in s.split(',')])
     for f in res:
+        ##  We should try to avoid using database specific information here
+        ##  Kept for now for speed
 #        wnf = WebNumberField.from_data(f)
 #        entry = ', '.join(
 #            [str(wnf.poly()), str(wnf.disc()), str(wnf.galois_t()), str(wnf.class_group_invariants_raw())])
         pol = str2pol(f['coeffs'])
         D = decodedisc(f['disc_abs_key'], f['disc_sign'])
         gal_t = f['galois']['t']
-        cl = string2list(f['class_group'])
+        if 'class_group' in f:
+            cl = string2list(f['class_group'])
+        else:
+            cl = [-1]
         entry = ', '.join([str(pol), str(D), str(gal_t), str(cl)])
         s += '[' + entry + ']' + ',\\\n'
     s = s[:-3]
