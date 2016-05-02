3929ff5def69a24075d8b1eacf3962d4672b6aef
sanni85 <samuele.anni@gmail.com>
2016-05-02 19:57:01 +0100

    fixed search and parsing boxes, fixed broken links, hidden to do list

--- lmfdb/siegel_modular_forms/siegel_modular_form.py
+++ lmfdb/siegel_modular_forms/siegel_modular_form.py
@@ -19,6 +19,9 @@ from lmfdb.siegel_modular_forms import smf_logger
 import json
 import StringIO
 
+from flask import flash, redirect
+
+
 DATA = 'http://data.countnumber.de/Siegel-Modular-Forms/'
 COLNS = None
 DB = None
@@ -102,21 +105,32 @@ def ModularForm_GSp4_Q_Sp4Z_j():
     jrange = xrange(0, 21)
     krange = xrange(10, 21)
     if request.args.get('j'):
-        jr = parse_range(request.args.get('j'))
-        if type(jr) is int:
-            jrange = xrange(jr, jr+20+1);
-        else:
-            jrange = xrange(jr['$gte'], jr['$lte'])
+        try:
+            jr = parse_range(request.args.get('j'))
+            if type(jr) is int:
+                jrange = xrange(jr, jr+20+1);
+            else:
+                jrange = xrange(jr['$gte'], jr['$lte'])
+        except:
+            error="Error parsing input for j.  It needs to be an integer (such as 25), a range of integers (such as 2-10 or 2..10), or a comma-separated list of these (such as 4,9,16 or 4-25, 81-121)." 
+            flash(error, "error")
+            return redirect(url_for(".ModularForm_GSp4_Q_Sp4Z_j"))
+
     if request.args.get('k'):
-        kr = parse_range(request.args.get('k'))
-        if type(kr) is int:
-            if kr<4:
-                kr=4
-            krange = xrange(kr, kr+10+1);
-        else:
-            if kr['$gte']<4:
-                kr['$gte']=4
-            krange = xrange(kr['$gte'], kr['$lte'])
+        try:
+            kr = parse_range(request.args.get('k'))
+            if type(kr) is int:
+                if kr<4:
+                    kr=4
+                krange = xrange(kr, kr+10+1);
+            else:
+                if kr['$gte']<4:
+                    kr['$gte']=4
+                krange = xrange(kr['$gte'], kr['$lte'])
+        except:
+            error="Error parsing input for k.  It needs to be an integer (such as 25), a range of integers (such as 2-10 or 2..10), or a comma-separated list of these (such as 4,9,16 or 4-25, 81-121)." 
+            flash(error, "error")
+            return redirect(url_for(".ModularForm_GSp4_Q_Sp4Z_j"))
     jrange = [x for x in jrange if x%2==0]
     try:
         dimtable = dimensions.dimension_table_Sp4Z_j(krange, jrange)
