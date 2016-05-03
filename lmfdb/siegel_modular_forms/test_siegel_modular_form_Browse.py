da91ef8e62b1d08f5e936d3be8e0889fcebf81c0
sanni85 <samuele.anni@gmail.com>
2016-05-03 18:01:03 +0100

    made more changes, the test pass, but it prints too much

--- lmfdb/siegel_modular_forms/test_siegel_modular_form_Browse.py
+++ lmfdb/siegel_modular_forms/test_siegel_modular_form_Browse.py
@@ -10,28 +10,23 @@ class HomePageTest(LmfdbTest):
         assert path in homepage
         assert text in self.tc.get(path).data
 
-    def check_external(self, homepage, path, text):
-        import urllib2
-        assert path in homepage
-        assert text in urllib2.urlopen(path).read()
-
     # All tests should pass: these are all the links in the browse page 
     def test_Siegel_links_browse_page(self):
         r"""
         Check that the links work.
         """
         homepage = self.tc.get("/ModularForm/GSp/Q/").data
-        self.check(homepage, "/ModularForm/GSp/Q/Sp4Z_j/", 'Enter either a')
-#        self.check(homepage, "/ModularForm/GSp/Q/Kp/", 'in level 277, the')
-#        self.check(homepage, "/ModularForm/GSp/Q/Sp6Z/", 'Miyawaki (1)')
-#        self.check(homepage, "/ModularForm/GSp/Q/Sp8Z/", 'Other_II (2)')
-#        self.check(homepage, "/ModularForm/GSp/Q/Gamma0_2/", 'Gamma_0(2)')
-#        self.check(homepage, "/ModularForm/GSp/Q/Gamma1_2/", 'Gamma_1(2)')
-#        self.check(homepage, "/ModularForm/GSp/Q/Gamma_2/", 'Gamma(2)')
-#        self.check(homepage, "/ModularForm/GSp/Q/Gamma0_3/", 'Gamma_0(3)')
-#        self.check(homepage, "/ModularForm/GSp/Q/Gamma0_3_psi_3/", 'T.Ibukiyama:')
-#        self.check(homepage, "/ModularForm/GSp/Q/Gamma0_4/", 'Gamma_0(4)')
-#        self.check(homepage, "/ModularForm/GSp/Q/Gamma0_4_psi_4/", 'psi_4')
-#        self.check(homepage, "/ModularForm/GSp/Q/Gamma0_4_half/", 'k-1/2')
+        self.check(homepage, "/ModularForm/GSp/Q/Sp4Z_j/",  'Enter either a')
+        self.check(homepage, "/ModularForm/GSp/Q/Kp/",  'in level 277, the')
+        self.check(homepage, "/ModularForm/GSp/Q/Sp6Z/",  'Miyawaki (1)')
+        self.check(homepage, "/ModularForm/GSp/Q/Sp8Z/",  'Other_II (2)')
+        self.check(homepage, "/ModularForm/GSp/Q/Gamma0_2/",  'Gamma_0(2)')
+        self.check(homepage, "/ModularForm/GSp/Q/Gamma1_2/",  'Gamma_1(2)')
+        self.check(homepage, "/ModularForm/GSp/Q/Gamma_2/",  'Gamma(2)')
+        self.check(homepage, "/ModularForm/GSp/Q/Gamma0_3/",  'Gamma_0(3)')
+        self.check(homepage, "/ModularForm/GSp/Q/Gamma0_3_psi_3/",  'T.Ibukiyama:')
+        self.check(homepage, "/ModularForm/GSp/Q/Gamma0_4/",  'Gamma_0(4)')
+        self.check(homepage, "/ModularForm/GSp/Q/Gamma0_4_psi_4/",  'psi_4')
+        self.check(homepage, "/ModularForm/GSp/Q/Gamma0_4_half/",  'k-1/2')
 
 
