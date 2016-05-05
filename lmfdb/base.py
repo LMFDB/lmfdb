cd89002af7b4f14c4c42638ca6375419e6134ef7
Edgar Costa <edgarcosta@math.dartmouth.edu>
2016-05-05 10:08:20 -0400

    forced replicaset to default to None not set in the config file, as it should be

--- lmfdb/base.py
+++ lmfdb/base.py
@@ -47,7 +47,7 @@ def makeDBConnection(port, **kwargs):
         logging.info("establishing db connection at port %s ..." % port)
         import pymongo
         logging.info("using pymongo version %s" % pymongo.version)
-        if pymongo.version_tuple[0] >= 3 or kwargs.get("replicaset") == '':
+        if pymongo.version_tuple[0] >= 3 or kwargs.get("replicaset",None) is None:
             from pymongo import MongoClient
             _C = MongoClient(port = port,  **kwargs)
         else:
