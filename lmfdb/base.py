c0100793db06d0f9c96e43a4f9b10058463af48d
Edgar Costa <edgarcosta@nyu.edu>
2016-05-05 02:32:51 -0400

    for 2.8 it is suggested to use MongoReplicaSetClient

--- lmfdb/base.py
+++ lmfdb/base.py
@@ -47,8 +47,12 @@ def makeDBConnection(port, **kwargs):
         logging.info("establishing db connection at port %s ..." % port)
         import pymongo
         logging.info("using pymongo version %s" % pymongo.version)
-        from pymongo.mongo_client import MongoClient
-        _C = MongoClient(port = port,  **kwargs)
+        if pymongo.version_tuple[0] >= 3:
+            from pymongo import MongoClient
+            _C = MongoClient(port = port,  **kwargs)
+        else:
+            from pymongo import MongoReplicaSetClient
+            _C = MongoReplicaSetClient(port = port,  **kwargs)
         mongo_info = _C.server_info()
         logging.info("mongodb version: %s" % mongo_info["version"])
 
