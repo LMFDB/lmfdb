72e5bbf6cbfb9550ed060dff45ca851eedfc2a84
John Cremona <john.cremona@gmail.com>
2016-05-12 22:46:52 -0700

    Master -> beta (#1328)
    * first commit on mod l modular forms, add to sidebar and made starting module structure
    
    * second commit, added some things to the import script
    
    * stats done and some more fixes
    
    * fixing indes page and main
    
    * more changes to index page
    
    * fixed random function
    
    * fix to sidebar and index page after discussion, start sigle mod l form page
    
    * many more changes done, fixed download and single object
    
    * made more changes
    
    * made more changes, label search works and rendering too
    
    * done some more fixes
    
    * fix pymongo upgrade bug in api
    
    * fixed prime_range
    
    * fixed breadcrumbs

---
+++ lmfdb/import_scripts/modlmf/modlmf.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+r""" Import mod l modular forms.  
+
+Note: This code can be run on all files in any order. Even if you 
+rerun this code on previously entered files, it should have no affect.  
+This code checks if the entry exists, if so returns that and updates 
+with new information. If the entry does not exist then it creates it 
+and returns that.
+
+"""
+
+import sys, time
+import re
+import json
+import sage.all
+from sage.all import os
+from sage.all import Integer, ZZ, QQ, PolynomialRing, NumberField, CyclotomicField, latex, AbelianGroup, polygen, euler_phi, latex, matrix, srange, PowerSeriesRing, sqrt
+from lmfdb.base import getDBConnection
+
+C= getDBConnection()
+import yaml
+pw_dict = yaml.load(open(os.path.join(os.getcwd(), "passwords.yaml")))
+username = pw_dict['data']['username']
+password = pw_dict['data']['password']
+C['mod_l_eigenvalues'].authenticate('editor', password)
+modlmf = C['mod_l_eigenvalues'].modlmf
+
+saving = True 
+
+def sd(f):
+  for k in f.keys():
+    print '%s ---> %s'%(k, f[k])
+
+def makels(li):
+  li2 = [str(x) for x in li]
+  return ','.join(li2)
+
+def string2list(s):
+  s = str(s)
+  if s=='': return []
+  return [int(a) for a in s.split(',')]
+
+
+def base_label(characteristic, deg, level, weight, dirchar):
+    return ".".join([str(characteristic),str(deg),str(level),str(weight), str(dirchar)])
+
+def last_label(base_label, n):
+    return ".".join([str(base_label),str(n)])
+
+# The following create_index command checks if there is an index on
+# label, dimension, determinant and level. 
+
+
+modlmf.create_index('characteristic')
+modlmf.create_index('deg')
+modlmf.create_index('level')
+modlmf.create_index('conductor')
+modlmf.create_index('weight')
+modlmf.create_index('dirchar')
+
+print "finished indices"
+
+
+## Main importing function
+
+label_dict={}
+
+def label_lookup(base_label):
+    if base_label in label_dict:
+	n=label_dict[base_label]+1
+	label_dict[base_label]=n
+    	return n
+    label_dict[base_label]=1
+    return 1	
+
+def do_import(ll):
+    characteristic,deg,level,conductor,min_weight,dirchar,atkinlehner,n_coeffs,coeffs = ll
+    mykeys = ['characteristic','deg','level','conductor','min_weight','dirchar','atkinlehner','n_coeffs','coeffs']
+    data = {}
+    for j in range(len(mykeys)):
+        data[mykeys[j]] = ll[j]
+
+    blabel = base_label(data['characteristic'],data['deg'],data['level'], data['min_weight'], data['dirchar'])
+    data['base_label'] = blabel
+    data['index'] = label_lookup(blabel)
+    label= last_label(blabel, data['index'])
+    data['label'] = label
+# we need still to organize this better with respect to tie breaks 
+
+    modl_mf = modlmf.find_one({'label': label})
+
+    if modl_mf is None:
+        print "new mod l modular form"
+        modl_mf = data
+    else:
+        print "mod l modular form already in the database"
+        modl_mf.update(data)
+    if saving:
+        modlmf.update({'label': label} , {"$set": modl_mf}, upsert=True)
+
+
+
+# Loop over files
+
+for path in sys.argv[1:]:
+    print path
+    filename = os.path.basename(path)
+    fn = gzip.open(path) if filename[-3:] == '.gz' else open(path)
+    for line in fn.readlines():
+        line.strip()
+        if re.match(r'\S',line):
+            l = json.loads(line)
+            do_import(l)
