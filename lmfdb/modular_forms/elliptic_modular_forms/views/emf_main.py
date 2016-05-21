f24ae164c90405ee3009cc5c78fd7b394dc760e1
Stephan Ehlen <stephan.j.ehlen@gmail.com>
2016-05-20 23:04:25 -0400

    correct call of url_for

--- lmfdb/modular_forms/elliptic_modular_forms/views/emf_main.py
+++ lmfdb/modular_forms/elliptic_modular_forms/views/emf_main.py
@@ -181,6 +181,7 @@ def get_downloads(level=None, weight=None, character=None, label=None, **kwds):
 def random_curve():
     label = random_object_from_collection( db_emf() )['hecke_orbit_label']
     level, weight, character, label = parse_newform_label(label)
+    args={}
     args['level'] = level
     args['weight'] = weight
     args['character'] = character
