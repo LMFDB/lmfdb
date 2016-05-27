252ad744ab169577c479148a9e5b63e4ce23c830
Stephan Ehlen <stephan.j.ehlen@gmail.com>
2016-05-27 01:21:15 -0400

    fix sage download

--- lmfdb/modular_forms/elliptic_modular_forms/backend/web_newforms.py
+++ lmfdb/modular_forms/elliptic_modular_forms/backend/web_newforms.py
@@ -899,14 +899,15 @@ class WebNewForm(WebObject, CachedRepresentation):
                   cfgen=str(self.coefficient_field.gen()), cfpol=self.absolute_polynomial
                   )
         elif self.coefficient_field.relative_degree() > 1:
+            s += 'y = polygen(K)'
             s +=  "L.<{cfgen}> = NumberField({cfpol})\n".format(
-                  cfgen=str(self.coefficient_field.gen()), cfpol=self.coefficient_field.relative_polynomial().change_variable_name('x')
+                  cfgen=str(self.coefficient_field.gen()), cfpol=self.coefficient_field.relative_polynomial().change_variable_name('y')
                   )
         s = s + "D = DirichletGroup({N})\n".format(
             N = self.level
             )
         C = self.character.sage_character.parent()
-        if C.zeta_order()>2:
+        if C.zeta_order()>2 and not self.coefficient_field.is_relative():
             s = s + "{Dzeta}=CyclotomicField({Dzord}).gen()\n".format(Dzord=C.zeta_order(), Dzeta = C.zeta())
         s = s + "f = {{'coefficients': {coeffs}, 'level' : {level}, 'weight': {weight}, 'character': D.Element(D,{vog}), 'label': '{label}','dimension': {dim}, 'is_cm': {cm} , 'cm_discriminant': {cm_disc}, 'atkin_lehner': {al}, 'explicit_formulas': {ep}}}".format(coeffs = self.coefficients(range(prec)),
             level=self.level, weight=self.weight, vog = self.character.sage_character.values_on_gens(), label=self.hecke_orbit_label, dim=self.dimension, cm=self.is_cm, cm_disc=None if not self.is_cm else self.cm_disc , al=self.atkin_lehner_eigenvalues(),
