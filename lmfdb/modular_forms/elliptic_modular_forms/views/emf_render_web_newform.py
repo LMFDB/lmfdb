649e9e857873a472163e0811536deff119d1d2c1
Stephan Ehlen <stephan.j.ehlen@gmail.com>
2016-05-29 08:50:27 -0400

    only load the scripts if no error occured

--- lmfdb/modular_forms/elliptic_modular_forms/views/emf_render_web_newform.py
+++ lmfdb/modular_forms/elliptic_modular_forms/views/emf_render_web_newform.py
@@ -70,7 +70,7 @@ def set_info_for_web_newform(level=None, weight=None, character=None, label=None
     try:
         WNF = WebNewForm_cached(level=level, weight=weight, character=character, label=label)
         if not WNF.has_updated():
-            raise IndexError()
+            raise IndexError("Unfortunately, we do not have this newform in the database.")
         info['character_order'] = WNF.character.order
         info['code'] = WNF.code
         emf_logger.debug("defined webnewform for rendering!")
