1240f681c4faf8f8c7cf705c0e488f4513194a35
Stephan Ehlen <stephan.j.ehlen@gmail.com>
2016-06-02 22:23:08 -0400

    no need to "compute" the trivial character.

--- lmfdb/modular_forms/elliptic_modular_forms/backend/web_character.py
+++ lmfdb/modular_forms/elliptic_modular_forms/backend/web_character.py
@@ -118,7 +118,10 @@ class WebChar(WebObject, CachedRepresentation):
         if self.number is not None:            
             emf_logger.debug('number: {0}'.format(self.number))
             self.character = DirichletCharacter_conrey(DirichletGroup_conrey(self.modulus),self.number)
-            self.sage_character = self.character.sage_character()
+            if not self.number == 1:
+                self.sage_character = self.character.sage_character()
+            else:
+                self.sage_character = trivial_character(self.modulus)
             self.name = "Character nr. {0} of modulus {1}".format(self.number,self.modulus)
             if embeddings:
                 from lmfdb.modular_forms.elliptic_modular_forms.backend.emf_utils import dirichlet_character_conrey_galois_orbit_embeddings
