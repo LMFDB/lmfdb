32941117169f2787101d159869198f31ca5ebbcc
AndrewVSutherland <drew@math.mit.edu>
2016-07-04 07:52:42 -0400

    remove unused imports of number_field_galois_groups

--- lmfdb/artin_representations/main.py
+++ lmfdb/artin_representations/main.py
@@ -4,22 +4,21 @@
 
 import pymongo
 ASC = pymongo.ASCENDING
-import flask
-from lmfdb.base import app, getDBConnection
-from flask import render_template, render_template_string, request, abort, Blueprint, url_for, flash, redirect
+from lmfdb.base import getDBConnection
+from flask import render_template, request, url_for, flash, redirect
 from markupsafe import Markup
 
 from lmfdb.artin_representations import artin_representations_page, artin_logger
 from lmfdb.utils import to_dict, random_object_from_collection
-from lmfdb.search_parsing import parse_primes, parse_restricted, parse_galgrp, parse_ints, parse_paired_fields, parse_count, parse_start, clean_input
+from lmfdb.search_parsing import parse_primes, parse_restricted, parse_galgrp, parse_ints, parse_count, parse_start, clean_input
 
-from lmfdb.transitive_group import *
-from lmfdb.WebCharacter import WebDirichletCharacter
+from lmfdb.math_classes import ArtinRepresentation
+from transitive_group import group_display_knowl
+
+from sage.all import ZZ
+
 import re, random
 
-
-from lmfdb.math_classes import *
-from lmfdb.WebNumberField import *
 
 def initialize_indices():
     try:
@@ -161,8 +160,6 @@ def render_artin_representation_webpage(label):
 
     title = "Artin representation %s" % label
     the_nf = the_rep.number_field_galois_group()
-    from lmfdb.number_field_galois_groups import nfgg_page
-    from lmfdb.number_field_galois_groups.main import by_data
     if the_rep.sign() == 0:
         processed_root_number = "not computed"
     else:
@@ -196,7 +193,7 @@ def render_artin_representation_webpage(label):
         if cc.modulus == 1 and cc.number == 1:
             friends.append(("L-function", url_for("l_functions.l_function_dirichlet_page", modulus=cc.modulus, number=cc.number)))
         else:
-            lfuncdb = base.getDBConnection().Lfunctions.instances
+            lfuncdb = getDBConnection().Lfunctions.instances
             # looking for Lhash dirichlet_L_modulus.number
             mylhash = 'dirichlet_L_%d.%d'%(cc.modulus,cc.number)
             lres = lfuncdb.find_one({'Lhash': mylhash})
