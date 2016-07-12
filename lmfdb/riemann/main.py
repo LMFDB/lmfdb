e80f85c2857f0f778800fd1a32d52981e635b5c3
Edgar Costa <edgarcosta@nyu.edu>
2016-07-12 17:29:08 +1000

    pyflakes cleanup

--- lmfdb/riemann/main.py
+++ lmfdb/riemann/main.py
@@ -1,6 +1,4 @@
-from lmfdb.base import app
 import flask
-from flask import render_template, request
 
 mod = flask.Blueprint('riemann', __name__, template_folder="templates")
 title = "Riemann zeta function"
