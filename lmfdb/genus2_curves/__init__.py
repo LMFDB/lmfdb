5160b2e665452a9060a21a7037dbc45dd4ae2366
AndrewVSutherland <drew@math.mit.edu>
2016-07-14 14:45:58 -0400

    tweak log colors in utils.py

--- lmfdb/genus2_curves/__init__.py
+++ lmfdb/genus2_curves/__init__.py
@@ -6,7 +6,6 @@ from flask import Blueprint
 g2c_page = Blueprint("g2c", __name__, template_folder='templates',
         static_folder="static")
 g2c_logger = make_logger(g2c_page)
-g2c_logger.info("Initializing genus 2 curves blueprint")
 
 @g2c_page.context_processor
 def body_class():
