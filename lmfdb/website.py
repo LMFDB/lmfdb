d0d7de97fe91e58cea18fe68a7630aa34986d047
AndrewVSutherland <drew@math.mit.edu>
2016-07-14 07:10:47 -0400

    put argument of -m back

--- lmfdb/website.py
+++ lmfdb/website.py
@@ -251,7 +251,7 @@ def get_configuration():
         try:
             opts, args = getopt.getopt(
                                         sys.argv[1:],
-                                        "p:h:l:tm",
+                                        "p:h:l:tm:",
                                        [
                                            "port=",
                                            "host=", 
