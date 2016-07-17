26740f9a04683adb521b551321da95cdaf17ff2f
John Jones <jj@asu.edu>
2016-07-17 05:38:40 +0100

    Fix for bug in Artin representations caused by recent removal of construction of sage number fields.

--- lmfdb/math_classes.py
+++ lmfdb/math_classes.py
@@ -770,8 +770,14 @@ class NumberFieldGaloisGroup(object):
         try:
             return self._residue_field_degrees(p)
         except AttributeError:
-            from number_fields.number_field import residue_field_degrees_function
-            fn_with_pari_output = residue_field_degrees_function(self.wnf())
+            # Try to make WebNumberField, but only helps if the field is in our database
+            wnf = self.wnf()
+            if wnf._data is None:
+                from number_fields.number_field import sage_residue_field_degrees_function
+                fn_with_pari_output = sage_residue_field_degrees_function(self.sage_object())
+            else:
+                from number_fields.number_field import residue_field_degrees_function
+                fn_with_pari_output = residue_field_degrees_function(wnf)
             self._residue_field_degrees = lambda p: map(Integer, fn_with_pari_output(p))
             # This function is better, becuase its output has entries in Integer
             return self._residue_field_degrees(p)
