464622f8ea06656a01682704c700155eeb0b76af
Andrew Sutherland <AndrewVSutherland@users.noreply.github.com>
2016-07-23 08:05:01 -0400

    Update galois_reps.py

--- lmfdb/tensor_products/galois_reps.py
+++ lmfdb/tensor_products/galois_reps.py
@@ -17,7 +17,7 @@ sage: V2 = GaloisRepresentation(chi)
 sage: V2.langlands
 sage: V2.local_euler_factor(101)
 
-sage: from lmfdb.math_classes import ArtinRepresentation
+sage: from lmfdb.artin_representations.math_classes import ArtinRepresentation
 sage: rho = ArtinRepresentation(2,23,1)
 sage: V3 = GaloisRepresentation(rho)
 sage: V3.dim
