09ea60358098d6282dc9e2388d546fc1f55b3bc2
sanni85 <samuele.anni@gmail.com>
2016-10-26 17:19:14 +0200

    search works and download of search results

--- lmfdb/hecke_algebras/main.py
+++ lmfdb/hecke_algebras/main.py
@@ -99,15 +99,8 @@ def hecke_algebras_search(**args):
 
     query = {}
     try:
-        for field, name in (('dim','Dimension'),('det','Determinant'),('level',None),
-                            ('minimum','Minimal vector length'), ('class_number',None), ('aut','Group order')):
+        for field, name in (('level','Level'),('weight','Weight'),('num_orbits', 'Number of Hecke orbits')):
             parse_ints(info, query, field, name)
-        # Check if length of gram is triangular
-        gram = info.get('gram')
-        if gram and not (9 + 8*ZZ(gram.count(','))).is_square():
-            flash(Markup("Error: <span style='color:black'>%s</span> is not a valid input for Gram matrix.  It must be a list of integer vectors of triangular length, such as [1,2,3]." % (gram)),"error")
-            raise ValueError
-        parse_list(info, query, 'gram', process=vect_to_sym)
     except ValueError as err:
         info['err'] = str(err)
         return search_input_error(info)
@@ -115,47 +108,9 @@ def hecke_algebras_search(**args):
     count = parse_count(info,50)
     start = parse_start(info)
 
-#    count_default = 50
-#    if info.get('count'):
-#        try:
-#            count = int(info['count'])
-#        except:
-#            err = "Error: <span style='color:black'>%s</span> is not a valid input. It needs to be a positive integer." % info['count']
-#            flash(Markup("Error: <span style='color:black'>%s</span> is not a valid input. It needs to be a positive integer." % info['count']), "error")
-#            info['err'] = str(err)
-#            return search_input_error(info)
-#    else:
-#        info['count'] = count_default
-#        count = count_default
-
-#    start_default = 0
-#    if info.get('start'):
-#        try:
-#            start = int(info['start'])
-#            if(start < 0):
-#                start += (1 - (start + 1) / count) * count
-#        except:
-#            start = start_default
-#    else:
-#        start = start_default
-
     info['query'] = dict(query)
-    res = C.hecke_algebrass.lat.find(query).sort([('dim', ASC), ('det', ASC), ('level', ASC), ('class_number', ASC), ('label', ASC)]).skip(start).limit(count)
+    res = C.mod_l_eigenvalues.hecke_algebras.find(query).sort([('level', ASC), ('weight', ASC), ('num_orbits', ASC)]).skip(start).limit(count)
     nres = res.count()
-
-    # here we are checking for isometric hecke_algebrass if the user enters a valid gram matrix but not one stored in the database_names, this may become slow in the future: at the moment we compare against list of stored matrices with same dimension and determinant (just compare with respect to dimension is slow)
-
-    if nres==0 and info.get('gram'):
-        A=query['gram'];
-        n=len(A[0])
-        d=matrix(A).determinant()
-        result=[B for B in C.hecke_algebrass.lat.find({'dim': int(n), 'det' : int(d)}) if isom(A, B['gram'])]
-        if len(result)>0:
-            result=result[0]['gram']
-            query_gram={ 'gram' : result }
-            query.update(query_gram)
-            res = C.hecke_algebrass.lat.find(query)
-            nres = res.count()
 
     if(start >= nres):
         start -= (1 + (start - nres) / count) * count
@@ -180,21 +135,20 @@ def hecke_algebras_search(**args):
     for v in res:
         v_clean = {}
         v_clean['label']=v['label']
-        v_clean['dim']=v['dim']
-        v_clean['det']=v['det']
         v_clean['level']=v['level']
-        v_clean['gram']=vect_to_matrix(v['gram'])
+        v_clean['weight']=v['weight']
+        v_clean['num_orbits']=v['num_orbits']
         res_clean.append(v_clean)
 
-    info['hecke_algebrass'] = res_clean
+    info['hecke_algebras'] = res_clean
 
-    t = 'Integral hecke_algebrass Search Results'
+    t = 'Hecke Algebras Search Results'
     bread = [('HeckeAlgebras', url_for(".hecke_algebras_render_webpage")),('Search Results', ' ')]
     properties = []
     return render_template("hecke_algebras-search.html", info=info, title=t, properties=properties, bread=bread, learnmore=learnmore_list())
 
 def search_input_error(info, bread=None):
-    t = 'Integral hecke_algebrass Search Error'
+    t = 'Hecke Algebras Search Error'
     if bread is None:
         bread = [('HeckeAlgebra', url_for(".hecke_algebras_render_webpage")),('Search Results', ' ')]
     return render_template("hecke_algebras-search.html", info=info, title=t, properties=[], bread=bread, learnmore=learnmore_list())
@@ -280,84 +234,23 @@ download_file_suffix = {'magma':'.m','sage':'.sage','gp':'.gp'}
 
 def download_search(info):
     lang = info["submit"]
-    filename = 'integral_hecke_algebrass' + download_file_suffix[lang]
+    filename = 'hecke_algebras' + download_file_suffix[lang]
     mydate = time.strftime("%d %B %Y")
     # reissue saved query here
-
-    res = getDBConnection().hecke_algebrass.lat.find(ast.literal_eval(info["query"]))
-
+    res = getDBConnection().mod_l_eigenvalues.hecke_algebras.find(ast.literal_eval(info["query"])).sort([('level', ASC), ('weight', ASC)])
     c = download_comment_prefix[lang]
     s =  '\n'
-    s += c + ' Integral hecke_algebrass downloaded from the LMFDB on %s. Found %s hecke_algebrass.\n\n'%(mydate, res.count())
+    s += c + ' Hecke Algebras downloaded from the LMFDB on %s. Found %s hecke_algebrass.\n\n'%(mydate, res.count())
     # The list entries are matrices of different sizes.  Sage and gp
     # do not mind this but Magma requires a different sort of list.
     list_start = '[*' if lang=='magma' else '['
     list_end = '*]' if lang=='magma' else ']'
-    s += download_assignment_start[lang] + list_start + '\\\n'
-    mat_start = "Mat(" if lang == 'gp' else "Matrix("
-    mat_end = "~)" if lang == 'gp' else ")"
-    entry = lambda r: "".join([mat_start,str(r),mat_end])
-    # loop through all search results and grab the gram matrix
-    s += ",\\\n".join([entry(r['gram']) for r in res])
-    s += list_end
-    s += download_assignment_end[lang]
-    s += '\n'
+    s += download_assignment_start[lang] + list_start + '\n'
+    s += str(',\n'.join([str([r['level'],r['weight'],r['num_orbits']]) for r in res])) 
+    s += list_end + download_assignment_end[lang]
+    s += '\n\n'
     strIO = StringIO.StringIO()
     strIO.write(s)
     strIO.seek(0)
     return send_file(strIO, attachment_filename=filename, as_attachment=True, add_etags=False)
-
-
-@hecke_algebras_page.route('/<label>/download/<lang>/<obj>')
-def render_hecke_algebras_webpage_download(**args):
-    if args['obj'] == 'shortest_vectors':
-        response = make_response(download_hecke_algebras_full_lists_v(**args))
-        response.headers['Content-type'] = 'text/plain'
-        return response
-    elif args['obj'] == 'genus_reps':
-        response = make_response(download_hecke_algebras_full_lists_g(**args))
-        response.headers['Content-type'] = 'text/plain'
-        return response
-
-
-def download_hecke_algebras_full_lists_v(**args):
-    C = getDBConnection()
-    label = str(args['label'])
-    res = C.hecke_algebrass.lat.find_one({'label': label})
-    mydate = time.strftime("%d %B %Y")
-    if res is None:
-        return "No such hecke_algebras"
-    lang = args['lang']
-    c = download_comment_prefix[lang]
-    outstr = c + ' Full list of normalized minimal vectors downloaded from the LMFDB on %s. \n\n'%(mydate)
-    outstr += download_assignment_start[lang] + '\\\n'
-    if res['name']==['Leech']:
-        outstr += str(res['shortest']).replace("'", "").replace("u", "")
-    else:
-        outstr += str(res['shortest'])
-    outstr += download_assignment_end[lang]
-    outstr += '\n'
-    return outstr
-
-
-def download_hecke_algebras_full_lists_g(**args):
-    C = getDBConnection()
-    label = str(args['label'])
-    res = C.hecke_algebrass.lat.find_one({'label': label})
-    mydate = time.strftime("%d %B %Y")
-    if res is None:
-        return "No such hecke_algebras"
-    lang = args['lang']
-    c = download_comment_prefix[lang]
-    mat_start = "Mat(" if lang == 'gp' else "Matrix("
-    mat_end = "~)" if lang == 'gp' else ")"
-    entry = lambda r: "".join([mat_start,str(r),mat_end])
-
-    outstr = c + ' Full list of genus representatives downloaded from the LMFDB on %s. \n\n'%(mydate)
-    outstr += download_assignment_start[lang] + '[\\\n'
-    outstr += ",\\\n".join([entry(r) for r in res['genus_reps']])
-    outstr += ']'
-    outstr += download_assignment_end[lang]
-    outstr += '\n'
-    return outstr
 
