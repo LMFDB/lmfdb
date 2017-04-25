2961563c1c4cfaed386737b0213f12164e2d3df8
R. van Bommel <bommelrvan@capella.backup.leidenuniv.nl>
2017-04-25 15:48:04 +0200

    (Raymond van Bommel) Tamagawa number for genus 2

--- warwick.sh
+++ warwick.sh
@@ -1,1 +1,1 @@
-ssh -4 -4 -4 -4 -o TCPKeepAlive=yes -o ServerAliveInterval=50 -C -N -L 37010:localhost:37010 mongo-user@lmfdb.warwick.ac.uk
+ssh -o TCPKeepAlive=yes -o ServerAliveInterval=50 -C -N -L 37010:localhost:37010 mongo-user@lmfdb.warwick.ac.uk
