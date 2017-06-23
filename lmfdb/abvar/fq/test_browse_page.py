3770f69e5bf8e9dae5e38e7c574be5de6ba53fd8
Christelle Vincent <christelle.vincent@uvm.edu>
2017-06-22 23:16:42 -0400

    added more searching tests

--- lmfdb/abvar/fq/test_browse_page.py
+++ lmfdb/abvar/fq/test_browse_page.py
@@ -102,8 +102,11 @@ class AVHomeTest(LmfdbTest):
         r"""
         Check that we can search by p-rank
         """
-        self.check_args("/Variety/Abelian/Fq/?q=&simple=any&g=&p_rank=2&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&count=", '2.11.ae_i')
-        self.check_args("/Variety/Abelian/Fq/?start=0&count=50&q=&g=&simple=any&p_rank=2&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=", '2.11.ae_i')
+        self.check_args("/Variety/Abelian/Fq/?q=9&primitive=any&g=2&simple=any&p_rank=2&jacobian=any&angle_ranks=&polarizable=any&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&count=", '2.9.ab_n')
+        self.check_args("/Variety/Abelian/Fq/?start=0&count=50&q=9&g=2&p_rank=2&angle_ranks=&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&simple=any&primitive=any&jacobian=any&polarizable=any", '2.9.ab_n')
+        self.not_check_args("/Variety/Abelian/Fq/?q=9&primitive=any&g=2&simple=any&p_rank=2&jacobian=any&angle_ranks=&polarizable=any&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&count=",'2.9.b_ad')
+        self.not_check_args("/Variety/Abelian/Fq/?start=0&count=50&q=9&g=2&p_rank=2&angle_ranks=&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&simple=any&primitive=any&jacobian=any&polarizable=any",'2.9.b_ad')
+        
         
     def test_search_newton(self):
         r"""
@@ -147,8 +150,66 @@ class AVHomeTest(LmfdbTest):
         self.check_args("/Variety/Abelian/Fq/?q=&simple=any&g=&p_rank=&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=%5B9%2C87%5D&decomposition=&count=", '3.9.ab_d_abs')
         self.check_args("/Variety/Abelian/Fq/?start=0&count=50&q=&g=&simple=any&p_rank=&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=%5B9%2C87%5D&decomposition=", '3.9.ab_d_abs')
 
-
-        #still to test: simple only and isogeny factors decomposition
+    def test_search_anglerank(self):
+        r"""
+        Check that we can search by angle rank
+        """
+        self.check_args("/Variety/Abelian/Fq/?q=3&primitive=any&g=4&simple=any&p_rank=&jacobian=any&angle_ranks=2&polarizable=any&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&count=",'4.3.ag_u_abw_dn')
+        self.check_args("/Variety/Abelian/Fq/?start=0&count=50&q=3&g=4&p_rank=&angle_ranks=2&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&simple=any&primitive=any&jacobian=any&polarizable=any",'4.3.ag_u_abw_dn')
+        self.not_check_args("/Variety/Abelian/Fq/?q=3&primitive=any&g=4&simple=any&p_rank=&jacobian=any&angle_ranks=2&polarizable=any&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&count=",'4.3.a_ad_a_j')
+        self.not_check_args("/Variety/Abelian/Fq/?start=0&count=50&q=3&g=4&p_rank=&angle_ranks=2&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&simple=any&primitive=any&jacobian=any&polarizable=any",'4.3.a_ad_a_j')
+        
+    def test_search_isogfactor(self):
+        r"""
+        Check that we can search by decomposition into isogeny factors
+        """
+        #[3.5.ah_y_ach,*]
+        self.check_args("/Variety/Abelian/Fq/?q=&primitive=any&g=&simple=any&p_rank=&jacobian=any&angle_ranks=&polarizable=any&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=%5B3.5.ah_y_ach%2C*%5D&number_field=&count=",'4.5.ak_by_agk_qb')
+        self.check_args("/Variety/Abelian/Fq/?start=0&count=50&q=&g=&p_rank=&angle_ranks=&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=%5B3.5.ah_y_ach%2C*%5D&number_field=&simple=any&primitive=any&jacobian=any&polarizable=any",'4.5.ak_by_agk_qb')
+        
+    def test_search_numberfield(self):
+        r"""
+        Check that we can search by number field
+        """
+        #4.0.9726525.1
+        self.check_args("/Variety/Abelian/Fq/?q=&primitive=any&g=&simple=any&p_rank=&jacobian=any&angle_ranks=&polarizable=any&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=4.0.9726525.1&count=",'2.193.ax_tn')
+        self.check_args("/Variety/Abelian/Fq/?start=0&count=50&q=&g=&p_rank=&angle_ranks=&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=4.0.9726525.1&simple=any&primitive=any&jacobian=any&polarizable=any",'2.193.ax_tn')
+        
+    def test_search_jacobian(self):
+        r"""
+        Check that we can restrict to Jacobians or non Jacobians
+        """
+        #Jacobians only
+        self.check_args("/Variety/Abelian/Fq/?q=3&primitive=any&g=2&simple=any&p_rank=&jacobian=yes&angle_ranks=&polarizable=any&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&count=",'2.3.ae_i')
+        self.check_args("/Variety/Abelian/Fq/?start=0&count=50&q=3&g=2&p_rank=&angle_ranks=&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&simple=any&primitive=any&jacobian=yes&polarizable=any",'2.3.ae_i')
+        self.not_check_args("/Variety/Abelian/Fq/?q=3&primitive=any&g=2&simple=any&p_rank=&jacobian=yes&angle_ranks=&polarizable=any&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&count=",'2.3.a_af')
+        self.not_check_args("/Variety/Abelian/Fq/?start=0&count=50&q=3&g=2&p_rank=&angle_ranks=&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&simple=any&primitive=any&jacobian=yes&polarizable=any",'2.3.a_af')
+        #non Jacobians only
+        self.check_args("/Variety/Abelian/Fq/?q=3&primitive=any&g=2&simple=any&p_rank=&jacobian=no&angle_ranks=&polarizable=any&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&count=",'2.3.a_af')
+        self.check_args("/Variety/Abelian/Fq/?start=0&count=50&q=3&g=2&p_rank=&angle_ranks=&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&simple=any&primitive=any&jacobian=no&polarizable=any",'2.3.a_af')
+        self.not_check_args("/Variety/Abelian/Fq/?q=3&primitive=any&g=2&simple=any&p_rank=&jacobian=no&angle_ranks=&polarizable=any&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&count=",'2.3.ae_i')
+        self.not_check_args("/Variety/Abelian/Fq/?start=0&count=50&q=3&g=2&p_rank=&angle_ranks=&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&simple=any&primitive=any&jacobian=no&polarizable=any",'2.3.ae_i')
+        
+    def test_search_princpol(self):
+        r"""
+        Check that we can restrict to principally polarizable or not principally polarizable
+        """
+        #princ polarizable only
+        self.check_args("/Variety/Abelian/Fq/?q=5&primitive=any&g=2&simple=any&p_rank=&jacobian=any&angle_ranks=&polarizable=yes&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&count=",'2.5.ab_f')
+        self.check_args("/Variety/Abelian/Fq/?start=0&count=50&q=5&g=2&p_rank=&angle_ranks=&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&simple=any&primitive=any&jacobian=any&polarizable=yes",'2.5.ab_f')
+        self.not_check_args("/Variety/Abelian/Fq/?q=5&primitive=any&g=2&simple=any&p_rank=&jacobian=any&angle_ranks=&polarizable=yes&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&count=",'2.5.ac_ab')
+        self.not_check_args("/Variety/Abelian/Fq/?start=0&count=50&q=5&g=2&p_rank=&angle_ranks=&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&simple=any&primitive=any&jacobian=any&polarizable=yes",'2.5.ac_ab')
+        #not princ polariable only
+        self.check_args("/Variety/Abelian/Fq/?q=5&primitive=any&g=2&simple=any&p_rank=&jacobian=any&angle_ranks=&polarizable=no&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&count=",'2.5.ac_ab')
+        self.check_args("/Variety/Abelian/Fq/?start=0&count=50&q=5&g=2&p_rank=&angle_ranks=&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&simple=any&primitive=any&jacobian=any&polarizable=no",'2.5.ac_ab')
+        self.not_check_args("/Variety/Abelian/Fq/?q=5&primitive=any&g=2&simple=any&p_rank=&jacobian=any&angle_ranks=&polarizable=no&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&count=",'2.5.ab_f')
+        self.not_check_args("/Variety/Abelian/Fq/?start=0&count=50&q=5&g=2&p_rank=&angle_ranks=&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&simple=any&primitive=any&jacobian=any&polarizable=no",'2.5.ab_f')
+        
+    def test_search_maxnumdisplay(self):
+        r"""
+        Check that we can restrict how many classes get displayed
+        """
+        self.check_args("/Variety/Abelian/Fq/?q=5&primitive=any&g=2&simple=any&p_rank=&jacobian=any&angle_ranks=&polarizable=any&newton_polygon=&initial_coefficients=&abvar_point_count=&curve_point_count=&decomposition=&number_field=&count=19",'19')
         
     def test_search_combos(self):
         r"""
