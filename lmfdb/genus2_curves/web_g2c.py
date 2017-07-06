cf1338ba93e7295a9008e5d2e6e9a4e186ce8903
AndrewVSutherland <drew@math.mit.edu>
2017-07-06 16:33:08 +0200

    ratpts v0 debug

--- lmfdb/genus2_curves/web_g2c.py
+++ lmfdb/genus2_curves/web_g2c.py
@@ -622,7 +622,7 @@ class WebG2C(object):
             	data['tama'] += tamgwnr + ' (p = ' + str(item['p']) + ')'
             	if (i+1 < tama.count()):
             		data['tama'] += ', '
-            if ratpts and ratpts.get('ratpts'):
+            if ratpts and ratpts.get('rat_pts'):
                 data['rat_pts'] = ','.join(web_latex(tuple([ZZ(c) for c in ratpts['rat_pts']])))
                 data['rat_pts_v'] = ratpts['rat_pts_v']
                 print "rat_pts", data['rat_pts']
