7a78b3a0916a5ce6967ad6ca3ef7401352370a58
David Neill Asanza <davidhneill@gmail.com>
2017-10-05 18:39:36 -0500

    Added script to update basic statics of passports collection.

---
+++ scripts/higher_genus_w_automorphisms/import_cap_data.py
@@ -0,0 +1,16 @@
+# To run this go into the top-level lmfdb directory, run sage and give
+# the command
+# %runfile scripts/higher_genus_w_automorphisms/import_cap_data.py
+
+import lmfdb
+from pymongo import MongoClient
+from data_mgt.utilities.rewrite import (update_attribute_stats, update_joint_attribute_stats)
+
+
+# Get database connection
+C = MongoClient('localhost', 37010)
+cap = C.curve_automorphisms
+
+# Basic counts for these attributes:
+print("Colelcting statistics on genus, dim, and r attributes.")
+update_attribute_stats(cap, 'passports', ['genus', 'dim', 'r'])
