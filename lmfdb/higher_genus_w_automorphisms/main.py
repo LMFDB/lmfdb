32675f49098fb9cd0aba1d492d5458e224141904
Lex Martin <alexandermartin006@gmail.com>
2017-10-27 18:24:21 -0500

    faster searching, remove download button if no results, download button now displays all results (not just current page), swapped Group and Group Orders(s) in search page, GAP

--- lmfdb/higher_genus_w_automorphisms/main.py
+++ lmfdb/higher_genus_w_automorphisms/main.py
@@ -160,14 +160,6 @@ def by_label(label):
 def by_passport_label(label):
     return render_passport({'passport_label': label})
 
-# IMPORTANT: There cannot be any instances of the character "$" in this dictionary
-query_variables = {
-    'g': '(this.genus)',
-    'G': '(parseInt(this.group.split(",")[0].split("[")[1]))',
-    #'d': '(this.dim)',
-    #'n': '(this.ndim)'
-}
-
 cur_expr = None
 cur_index = 0
 
@@ -208,183 +200,140 @@ def expr_expect_char(char):
         return None
 
 def read_num():
+    num = ""
     c = expr_peekc()
     while c != None and c.isdigit():
+        num += c
         expr_getc()
         c = expr_peekc()
+    return int(num)
 
-def expect_var(variables):
+def expect_var(vars):
     #print "var"
     c = expr_peekc()
     is_valid_var = False
-    for var in variables:
+    for var in vars.keys():
         if var == c:
             is_valid_var = True
             break
 
     if is_valid_var:
-        expr_getc()
-        return None
+        var = expr_getc()
+        return (None, vars[var])
     else:
-        return expr_error("'" + c + "' is not a recognized variable")
+        return (expr_error("'" + c + "' is not a recognized variable"), None)
 
-def expect_factor():
+def expect_factor(vars):
     #print "factor"
+
     c = expr_peekc()
     if c == None:
-        return expr_error("expected factor here")
+        return (expr_error("expected factor here"), None)
     elif c.isdigit():
-        read_num()
-        return None
+        return (None, read_num())
     elif is_letter(c):
-        return expect_var(query_variables.keys())
+        err, result = expect_var(vars)
+        return err, result
     elif c == '(':
         expr_getc()
-        err = expect_expr()
-        if err != None: return err
+        err, result = expect_expr(vars)
+        if err != None: return (err, None)
         err = expr_expect_char(')')
-        return err
+        if err != None: return (err, None)
+        else: return (None, result)
     else:
-        return expr_error("'" + c + "' unexpected symbol")
+        return (expr_error("'" + c + "' unexpected symbol"), None)
 
-def expect_term():
+def expect_term(vars):
     #print "term"
-    err = expect_factor()
-    if err != None: return err
+    err, result = expect_factor(vars)
+    if err != None: return (err, None)
     
     c = expr_peekc()
     while c != None and (c.isdigit() or is_letter(c) or c == '('):
-        err = expect_factor()
-        if err != None: return err
+        err, factor_result = expect_factor(vars)
+        if err != None: return (err, None)
+        result *= factor_result
         c = expr_peekc()
-    return None
 
-def expect_expr():
+    return (None, result)
+
+def expect_expr(vars):
     #print "expr"
-    err = expect_term()
-    if err != None: return err
+    err, result = expect_term(vars)
+    if err != None: return (err, None)
     
     c = expr_peekc()
     while c == "+" or c == "-":
         expr_getc()
-        err = expect_term()
-        if err != None: return err
+        err, term_result = expect_term(vars)
+        if err != None: return (err, None)
+        result += term_result
         c = expr_peekc()
 
-    return None
+    return (None, result)
 
-def is_valid_expr(expr):
+def evaluate_expr(expr, vars):
     global cur_expr, cur_index
     cur_expr = expr
     cur_index = 0
-    result = expect_expr()
-    if result == None:
+    err, result = expect_expr(vars)
+
+    if err == None:
         if expr_peekc() != None:
-            return expr_error("unexpected symbol")
-    return result
+            return (expr_error("unexpected symbol"), None)
 
-def get_token(expr, i):
-    j = i
-    if i < len(expr):
-        if expr[i].isdigit():
-            while i < len(expr) and expr[i].isdigit():
-                i += 1
-            return expr[j:i], i
-        else:
-            i += 1
-            return expr[j], i
-    else: return None, i
+    return (err, result)
 
-def is_number(string):
-    result = True
-    for x in string:
-        if not x.isdigit():
-            result = False
-            break
-    return result
-
-def is_value(token):
-    return is_number(token) or is_letter(token)
-
-def add_mul(expr):
-    result = ""
-    i = 0
-    prev, i = get_token(expr, i)
-    result += prev
-    while prev != None:
-        cur, i = get_token(expr, i)
-        if cur == None: break
-        two_vars = (is_value(prev) and is_value(cur))
-        var_expr = (is_value(prev) and cur == '(')
-        expr_var = (prev == ')' and is_value(cur))
-        two_expr = (prev == ')' and cur == '(')
-        mult_between = two_vars or var_expr or expr_var or two_expr
-
-        if mult_between:
-            result += "*"
-        
-        result += cur
-        prev = cur
-    return result
-
-# Convert pretty expression to something mongo can understand
-def build_mongo_expr(expression):
-    for qvar in query_variables:
-        expression = expression.replace('$' + qvar, query_variables[qvar])
-    return expression
-
-def variable_range_query(query_parameter, query_range):
+def add_group_order_range(mongo_query, expr, db):
     # Support -- and .. as range
-    query_range = query_range.replace("--", "..")
-
-    # put a $ before every variable, makes later part easier
-    processed_query_range = add_mul(query_range)
-    for qvar in query_variables:
-        processed_query_range = processed_query_range.replace(qvar, '$' + qvar)
-    
-    raw_parts = query_range.split('..')
-    parts = processed_query_range.split('..')
-
+    query_range = expr.replace("--", "..")
+    raw_parts = expr.split('..')
     raw_parts = filter(lambda x: x != '', raw_parts)    
-    parts = filter(lambda x: x != '', parts)
 
-    if len(parts) == 2:
-        # This is a well formed range query
-        left_half_err = is_valid_expr(raw_parts[0])
-        right_half_err = is_valid_expr(raw_parts[1])
+    min_genus = db.curve_automorphisms.passports.find().sort([("group_order", 1)]).limit(1)[0]["genus"]
+    max_genus = db.curve_automorphisms.passports.find().sort([("group_order", -1)]).limit(1)[0]["genus"]
 
-        if left_half_err == None and right_half_err == None:
-            left_expr = query_variables[query_parameter] + ' >= (' + build_mongo_expr(parts[0]) + ')'
-            right_expr = query_variables[query_parameter] + ' <= (' + build_mongo_expr(parts[1]) + ')'
-            mongo_expr = '(' + left_expr + ') && (' + right_expr + ')'
-            return (None, mongo_expr)
-        elif left_half_err != None:
-            #print "\n\n",left_half_err,"\n\n"
-            return (raw_parts[0], left_half_err)
-        else:
-            #print "\n\n",right_half_err,"\n\n"
-            return (raw_parts[1], right_half_err)
-    elif len(parts) == 1 and query_range.find('..') != -1:
+    if len(raw_parts) == 2:
+        mongo_expr = []
+
+        for cur_genus in range(min_genus, max_genus + 1):
+            left_err, left_value   = evaluate_expr(raw_parts[0], {'g': cur_genus})
+            right_err, right_value = evaluate_expr(raw_parts[1], {'g': cur_genus})
+            if left_err == None and right_err == None:
+                mongo_expr.append({"group_order": {"$gte": left_value, "$lte" : right_value}, "genus": cur_genus})
+            elif left_err != None:
+                mongo_query["$or"] = [{"genus": {"$lte": 0}}]
+                #print "\n\nleft error\n\n"
+                return (raw_parts[0], left_half_err)
+            else:
+                mongo_query["$or"] = [{"genus": {"$lte": 0}}]
+                #print "\n\nright error\n\n"
+                return (raw_parts[1], right_half_err)
+
+        mongo_query["$or"] = mongo_expr 
+        return (None, None)
+    elif len(raw_parts) == 1:
         condition = ""
-        expr = parts[0]
-        if processed_query_range.index("..") == 0:
-            condition = "<="
+
+        if query_range.find('..') != -1:
+            if query_range.index("..") == 0:
+                condition = "$lte"
+            else:
+                condition = "$gte"
         else:
-            condition = ">="
-        err = is_valid_expr(raw_parts[0])
-        if err == None:
-            mongo_expr = query_variables[query_parameter] + ' ' + condition + ' (' + build_mongo_expr(expr) + ')'
-            return (None, mongo_expr)
-        else:
-            return (raw_parts[0], err)
-    elif len(parts) == 1:
-        expr = parts[0]
-        err = is_valid_expr(raw_parts[0])
-        if err == None:
-            mongo_expr = query_variables[query_parameter] + ' == (' + build_mongo_expr(expr) + ')'
-            return (None, mongo_expr)
-        else:
-            return(raw_parts[0], err)
+            condition = "$eq"
+
+        for cur_genus in range(min_genus, max_genus + 1):
+            err, value   = evaluate_expr(raw_parts[0], {'g': cur_genus})
+            if err == None:
+                mongo_expr = []
+                mongo_expr.append({"group_order": {condition: value}, "genus": {"$eq": cur_genus}})
+                mongo_query["$or"] = mongo_expr 
+                return (None, None)
+            else:
+                mongo_query["$or"] = [{"genus": {"$lte": 0}}]
+                return (raw_parts[0], err) 
     else:
         return ("", "You must either specify a group size or range in the format Min..Max")
 
@@ -439,14 +388,10 @@ def higher_genus_w_automorphisms_search(**args):
     start = parse_start(info)
 
     if 'groupsize' in info and info['groupsize'] != '':
-        err, result = variable_range_query('G', info['groupsize'])
-        if err == None:
-            #print "\n\n", result, "\n\n"
-            query['$where'] = result 
-        else:
-            flash_error('<font face="Courier New"> Parse error on group size range <br />' + err + '<br />' + result + '</font>')
-            query['$where'] = "false" 
-
+        err, result = add_group_order_range(query, info['groupsize'], C)
+        if err != None:
+            flash_error('Parse error on group order field. <font face="Courier New"><br />Given: ' + err + '<br />-------' + result + '</font>')
+    """
     res = C.curve_automorphisms.passports.find(query).sort([(
          'genus', pymongo.ASCENDING), ('dim', pymongo.ASCENDING),
         ('cc'[0],pymongo.ASCENDING)])
@@ -459,14 +404,16 @@ def higher_genus_w_automorphisms_search(**args):
         start = 0
 
 
-    code = ""
-    download_code = 'download' in info
-
     L = [ ]
-    first_download_entry = True 
     for field in res:
         field['signature'] = ast.literal_eval(field['signature'])    
         L.append(field)
+
+    code = ""
+    download_code = 'download' in info
+    first_download_entry = True
+    for field in L:
+        field['signature'] = ast.literal_eval(field['signature'])    
         if download_code:
             if first_download_entry:
                 code += '\n'.join(hgcwa_code(label=field['passport_label'], download_type='magma').split('\n')[1:])
@@ -474,14 +421,63 @@ def higher_genus_w_automorphisms_search(**args):
                 code += hgcwa_code(label=field['passport_label'], download_type='magma').split('result_record:=[];')[1]
             first_download_entry = False
 
+    
+    
+
     if 'download' in info:
         response = make_response(code)
         response.headers['Content-type'] = 'text/plain'
         return response
+    """
+
+    res = C.curve_automorphisms.passports.find(query).sort([(
+         'genus', pymongo.ASCENDING), ('dim', pymongo.ASCENDING),
+        ('cc'[0],pymongo.ASCENDING)])
+
+    if 'download_magma' in info:
+        code = ""    
+        first_download_entry = True
+        for field in res:
+            print field['group']
+            if first_download_entry:
+                code += '\n'.join(hgcwa_code(label=field['passport_label'], download_type='magma').split('\n')[1:])
+            else:
+                code += hgcwa_code(label=field['passport_label'], download_type='magma').split('result_record:=[];')[1]
+            first_download_entry = False
+        response = make_response(code)
+        response.headers['Content-type'] = 'text/plain'
+        return response
+    elif 'download_gap' in info:
+        code = ""    
+        first_download_entry = True
+        for field in res:
+            print field['group']
+            if first_download_entry:
+                code += '\n'.join(hgcwa_code(label=field['passport_label'], download_type='gap').split('\n')[1:])
+            else:
+                code += hgcwa_code(label=field['passport_label'], download_type='magma').split('result_record:=[];')[1]
+            first_download_entry = False
+        response = make_response(code)
+        response.headers['Content-type'] = 'text/plain'
+        return response
+
+    nres = res.count()
+    res = res.skip(start).limit(count)
+
+    if(start >= nres):
+        start -= (1 + (start - nres) / count) * count
+    if(start < 0):
+        start = 0
+        
+    L = [ ]    
+    for field in res:
+        field['signature'] = ast.literal_eval(field['signature'])    
+        L.append(field)
 
     info['fields'] = L    
     info['number'] = nres
     info['group_display'] = sg_pretty
+    info['show_downloads'] = len(L) > 0
 
     info['sign_display'] = sign_display
     info['start'] = start
