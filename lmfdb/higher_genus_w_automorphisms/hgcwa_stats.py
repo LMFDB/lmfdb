70649274e160d7ea0be7adbff9e4ee471b018191
David Neill Asanza <davidhneill@gmail.com>
2017-11-01 04:10:44 -0500

    Added single instance of HGCWAstats object for fetching computed stats.

--- lmfdb/higher_genus_w_automorphisms/hgcwa_stats.py
+++ lmfdb/higher_genus_w_automorphisms/hgcwa_stats.py
@@ -1,15 +1,29 @@
+# based on /lmfdb/elliptic_curves/ec_stats.py
+
+import re
+from lmfdb.base import getDBConnection
+from lmfdb.utils import make_logger
+
 logger = make_logger("hgcwa")
 
 the_HGCWAstats = None
 
 def db_hgcwa_stats():
-    return getDBConnection().elliptic_curves.curves.stats
+    return getDBConnection().curve_automorphisms.passports.stats
 
-def get_stats():
+def get_stats_object():
     global the_HGCWAstats
     if the_HGCWAstats is None:
         the_HGCWAstats = HGCWAstats()
     return the_HGCWAstats
+
+def max_group_order(counts):
+    orders = []
+    for count in counts:
+        group = count[0]
+        order = int(re.search(r'\[(\d+)', group).group(1))
+        orders.append(order)
+    return max(orders)
 
 class HGCWAstats(object):
     """
@@ -20,9 +34,12 @@ class HGCWAstats(object):
         logger.debug("Constructing an instance of HGCWAstats")
         self.hgcwa_stats = db_hgcwa_stats()
         self._counts = {}
-        self._stats = get_hgcwa_stats()
+        self._stats = self.compute_stats()
 
-    def get_hgcwa_stats(self):
+    def stats(self):
+        return self._stats
+
+    def compute_stats(self):
         logger.debug("Computing elliptic curve stats...")
 
         db = self.hgcwa_stats
