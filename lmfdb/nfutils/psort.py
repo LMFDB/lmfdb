28c247583e4299dfcf2985e2e74e040594b77a1a
John Cremona <john.cremona@gmail.com>
2018-01-30 16:34:50 +0000

    fixed deprecation warning for p-adic expansions

--- lmfdb/nfutils/psort.py
+++ lmfdb/nfutils/psort.py
@@ -40,7 +40,7 @@ Zp_key = lambda a: a.list(start_val=0)
 # always start with the p^0 coefficient.
 
 def padded_list(c,k):
-    a = c.list(start_val=0)
+    a = list(c.expansion(start_val=0))
     return a[:k] + [ZZ(0)]* (k-len(a))
 
 def ZpX_key(k):
