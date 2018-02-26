0ca0fca7a5a6303ab8de9845a26ee1828610e139
David Lowry-Duda <david.j.lowry@gmail.com>
2018-02-26 11:51:29 +0000

    Allow sorting by id or by count

--- lmfdb/inventory_app/static/record_viewer.js
+++ lmfdb/inventory_app/static/record_viewer.js
@@ -1,6 +1,6 @@
-
 /*jshint esversion: 6 */
 var tooltip_dict = {'dummy': 'Record does not exist', 'extends': 'Schema is displayed as diff from numbered record', 'base' : 'This is the base record which others extend'};
+var vals_to_sort = ['count'];
 
 function BaseRecord(num, name){
   //Holds the info about the base record for the diffed record types
@@ -44,6 +44,7 @@ function populateRecordViewerPage(blockList, startVisible=true){
   var table_div = document.createElement('div');
   var table = document.createElement('table');
   table.class = 'viewerTable';
+  table.id = 'viewerTable';
   entry_styles = ['table_tag'];
 
   base_record = unpackBaseRecord(blockList);
@@ -73,21 +74,30 @@ function createRecordRow(blockList, field, id_start, base_record, header=false){
 //field is the set id, in this case sequential numbers, used as record Num
   var table_row = document.createElement('tr');
   var clas = header ? 'viewerTableHeaders' : 'viewerTableEls';
-  var table_el = document.createElement('td');
   if(header){
+    var table_el = document.createElement('th');
     table_el.innerHTML = 'Record Num';
+    table_el.onclick = function() {sortTable(0) };
   }else{
+    var table_el = document.createElement('td');
     table_el.innerHTML = field;
   }
   table_el.classList.add(clas);
   table_row.appendChild(table_el);
   var is_base = (blockList.getBlock(id_start+'base') ? blockList.getBlock(id_start+'base').text : false);
   for(var j=0; j < record_fields.length; j++){
-    table_el = document.createElement('td');
+    if(header){
+      table_el = document.createElement('th');
+    }else{
+      table_el = document.createElement('td');
+    }
     table_el.classList.add(clas);
     if(header){
       table_el.innerHTML = capitalise(record_fields[j]);
       if(record_fields[j] in tooltip_dict) table_el.title = tooltip_dict[record_fields[j]];
+      if(vals_to_sort.includes(record_fields[j])){
+        table_el.onclick = function() { sortTable(j) };
+      }
     }else{
       var block = blockList.getBlock(id_start+record_fields[j]);
       var text = block ? block.text: '';
@@ -124,5 +134,60 @@ function fillRecordHashMap(data){
     //If this is records, we add entry to the map linking the field to the hash
     var records = ('hash' in contents && 'oschema' in contents);
     recordHashMap.set(field, contents.hash);
+  }
+}
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("viewerTable");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (Number(x.innerHTML) > Number(y.innerHTML)) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (Number(x.innerHTML) < Number(y.innerHTML)) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
   }
 }
