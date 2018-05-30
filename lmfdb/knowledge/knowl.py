09330577ed9a43543e0fc428abe8152bf619fc59
John Cremona <john.cremona@gmail.com>
2018-05-30 16:08:07 +0100

    only append (beta status) when in beta mode

--- lmfdb/knowledge/knowl.py
+++ lmfdb/knowledge/knowl.py
@@ -306,7 +306,8 @@ class Knowl(object):
         with "Dirichlet Series" and nothing else.
         """
         title = self._title
-        if self._quality=='beta':
+        from flask import g
+        if self._quality=='beta' and g.BETA:
             title += " (beta status)"
         return title
 
