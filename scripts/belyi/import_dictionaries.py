6cf36dcd67c212139de3dac24200f70031d8e35e
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-06-13 11:42:00 -0400

    creating collection .rand

--- scripts/belyi/import_dictionaries.py
+++ scripts/belyi/import_dictionaries.py
@@ -8,7 +8,6 @@ galmaps = belyidb['galmaps']
 # import the raw data
 # import ola
 from raw_data import ola
-
 # insert passports one at a time
 for i in range(0,len(ola)):
     passports.insert_one(ola[i][0])
@@ -18,5 +17,12 @@ for i in range(0,len(ola)):
     for j in range(0,len(ola[i][1])):
         galmaps.insert_one(ola[i][1][j])
 
-passports.find().count()
-galmaps.find().count()
+print passports.find().count()
+print galmaps.find().count()
+
+import os, sys, inspect
+filename = inspect.getframeinfo(inspect.currentframe())[0];
+folder = os.path.dirname(os.path.abspath(filename));
+sys.path.append(os.path.join(folder, "../../"));
+from data_mgt.utilities.rewrite import create_random_object_index
+create_random_object_index(belyidb, "galmaps");
