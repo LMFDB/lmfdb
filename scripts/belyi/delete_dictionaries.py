e4523498b9981498c88e739b221a4df0d99af435
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-06-13 12:10:25 -0400

    renaming and deleting

--- scripts/belyi/delete_dictionaries.py
+++
@@ -1,9 +0,0 @@
-import db
-Cw = db.getDBconnection_write();
-
-belyidb = Cw['belyi']
-passports = belyidb['passports']
-galmaps = belyidb['galmaps']
-
-passports.delete_many({})
-galmaps.delete_many({})
