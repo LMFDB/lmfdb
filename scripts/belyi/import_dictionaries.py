1dab4c68d021bd04d7ba9d2bcef2e18c86fe0115
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-06-13 12:08:41 -0400

    using rewrite

--- scripts/belyi/import_dictionaries.py
+++ scripts/belyi/import_dictionaries.py
@@ -2,27 +2,31 @@ import db
 Cw = db.getDBconnection_write();
 
 belyidb = Cw['belyi']
-passports = belyidb['passports']
-galmaps = belyidb['galmaps']
+belyidb.drop_collection('passports_new');
+belyidb.drop_collection('galmaps_new');
+passports = belyidb['passports_new']
+galmaps = belyidb['galmaps_new']
 
 # import the raw data
 # import ola
 from raw_data import ola
 # insert passports one at a time
-for i in range(0,len(ola)):
-    passports.insert_one(ola[i][0])
+passports_upload = [ elt[0] for elt in ola];
+galmaps_upload = [];
+for elt in ola:
+    galmaps_upload += elt[1];
 
-# insert galmaps one at a time
-for i in range(0,len(ola)):
-    for j in range(0,len(ola[i][1])):
-        galmaps.insert_one(ola[i][1][j])
-
+passports.insert_many(passports_upload);
+galmaps.insert_many(galmaps_upload);
 print passports.find().count()
 print galmaps.find().count()
+
 
 import os, sys, inspect
 filename = inspect.getframeinfo(inspect.currentframe())[0];
 folder = os.path.dirname(os.path.abspath(filename));
 sys.path.append(os.path.join(folder, "../../"));
-from data_mgt.utilities.rewrite import create_random_object_index
+from data_mgt.utilities.rewrite import create_random_object_index, rewrite_collection
+rewrite_collection(belyidb,"galmaps","galmaps_new",lambda x: x)
+rewrite_collection(belyidb,"passports","passports_new",lambda x: x)
 create_random_object_index(belyidb, "galmaps");
