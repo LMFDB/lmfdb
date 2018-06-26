670683b366d7022ed3456fde956ac87ccf2a366c
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-06-26 10:31:32 -0400

    more refs

--- lmfdb/lattice/lattice_stats.py
+++ lmfdb/lattice/lattice_stats.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from lmfdb.base import app, getDBConnection
 from lmfdb.utils import make_logger, comma
+from lmfdb.knowledge.main import render_knowl_in_template
 
 logger = make_logger("lattice")
 
@@ -25,9 +26,32 @@ def lattice_summary():
     det_data = latstats.find_one('det')
     max_det = det_data['max']
 
-    return ''.join([r'<p>The database currently contains {} '.format(comma(number)), positivedef,' ', integral,'. It includes data from the ', catalogue,
-                    '.</p><p>The largest ', cn , ' is {}, '.format(comma(max_cn)), ' the largest ', dim, ' is {}, '.format(comma(max_dim)),
-                    'and the largest ', det, ' is {}.</p> '.format(comma(max_det)),'<p>In the case of ', pri ,' ', integral, ' of ', cn, ' one the database is complete, by a calculation of Kirschmer and Lorch.</p>'])
+    #Is this a summary or a completeness statement.
+    #The later should be a knowl!
+    text = ''.join([
+        r'<p>The database currently contains {} '.format(comma(number)),
+        positivedef,
+        ' ',
+        integral,
+        '. It includes data from the ',
+        catalogue,
+        '.</p><p>The largest ',
+        cn ,
+        ' is {}, '.format(comma(max_cn)),
+        ' the largest ',
+        dim,
+        ' is {}, '.format(comma(max_dim)),
+        'and the largest ',
+        det,
+        ' is {}.</p> '.format(comma(max_det)),
+        '<p>In the case of ',
+        pri,
+        ' ',
+        integral,
+        ' of ',
+        cn,
+        ' one the database is complete, by a calculation of Kirschmer and Lorch (\cite{arxiv:1208.5638}, \cite{doi:10.1112/S1461157013000107}, \cite{MR3091733}). </p>']);
+    return render_knowl_in_template(text);
 
 def lattice_summary_data():
     latstats = db_latstats()
