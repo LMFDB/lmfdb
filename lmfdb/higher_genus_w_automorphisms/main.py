1459717c7c687d1402bca7cfe46fc4d4e81c5c8d
Jen Paulhus <jenpaulhus@gmail.com>
2018-07-24 17:04:33 -0500

    more updates

--- lmfdb/higher_genus_w_automorphisms/main.py
+++ lmfdb/higher_genus_w_automorphisms/main.py
@@ -933,3 +933,4 @@ def hgcwa_code_download_search(res,download_type):
     strIO.write(code)
     strIO.seek(0)
     return send_file(strIO, attachment_filename=filename, as_attachment=True, add_etags=False)
+
