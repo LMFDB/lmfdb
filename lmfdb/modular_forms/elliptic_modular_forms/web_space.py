b9c3bfd15cd155e52e43ca49402d602e68a0db92
Alex J. Best <alex.j.best@gmail.com>
2018-08-29 16:47:40 -0400

    first pass at webnewformspace

--- lmfdb/modular_forms/elliptic_modular_forms/web_space.py
+++ lmfdb/modular_forms/elliptic_modular_forms/web_space.py
@@ -3,16 +3,55 @@
 
 
 from lmfdb.db_backend import db
+from sage.all import latex, ZZ
 
 class WebNewformSpace(object):
-    def __init__(self, data):
+    def __init__(self, newspace, newforms, oldspaces):
         # Need to set mf_dim, eis_dim, cusp_dim, new_dim, old_dim
-        pass
+        self.make_object(newspace, newforms, oldspaces)
 
     @staticmethod
     def by_label(label):
-        # search in db.mf_newforms
-        pass
+        """
+        Searches for a specific modular forms space by its label.
+        Constructs the WebModformSpace object if the space is found, raises an error otherwise
+        """
+        try:
+            slabel = label.split(".")
+            if len(slabel) == 3:
+                newspace = db.mf_newspaces.lookup(label)
+            else:
+                raise ValueError("Invalid modular forms space label %s." % label)
+        except AttributeError:
+            raise ValueError("Invalid modular forms space label %s." % label)
+        oldspaces = list(db.mf_oldsubs.search({"space_label": newspace['label']}))
+        newforms = list(db.mf_newforms.search({"level": newspace['level'], "weight": newspace['weight'], "char_orbit": newspace['char_orbit']})) # TODO change this to search on space_label
+        for newform in newforms:
+            newform['qexp'] = list(db.mf_hecke_nf.search({"hecke_orbit_code": newform['hecke_orbit_code']}, ['n','an'], sort=['n'])) # TODO use webnewform instead
+        return WebG2C(newspace, newforms, oldspaces)
+
+
+    def make_object(self, newspace, newforms, oldspaces):
+        from lmfdb.modular_forms.elliptic_modular_forms.main import url_for_newspace_label # TODO remove if unused
+
+        data = self.data = {}
+
+        data['label'] = newspace['label']
+        data['slabel'] = data['label'].split('.')
+
+        data['level'] = ZZ(newspace['level'])
+        data['weight'] = ZZ(newspace['weight'])
+        data['char_orbit'] = ZZ(newspace['char_orbit'])
+        data['minimal_conrey'] = ZZ(newspace['minimal_conrey'])
+        data['conductor'] = ZZ(newspace['conductor'])
+        data['sturm_bound'] = ZZ(newspace['sturm_bound'])
+        data['dim'] = ZZ(newspace['dim'])
+        data['hecke_orbit_dims'] = newspace['hecke_orbit_dims']
+        data['eis_dim'] = ZZ(newspace['eis_dim'])
+        data['cusp_dim'] = ZZ(newspace['cusp_dim'])
+        data['mf_dim'] = ZZ(newspace['mf_dim'])
+        data['newforms'] = newforms
+        data['oldspaces'] = oldspaces
 
     def mf_latex(self):
         pass
@@ -24,16 +63,18 @@ class WebNewformSpace(object):
         pass
 
     def new_latex(self):
-        pass
+        return "S_{%d}"
 
     def old_latex(self):
         pass
 
     def decomposition(self):
-        # returns a list of 5-tuples (label, url, dim, field, qexp
+        from lmfdb.modular_forms.elliptic_modular_forms.main import url_for_newform_label
+        # returns a list of 5-tuples (label, url, dim, field, qexp)
         # Field may need to be augmented to support pretty printing, minimal poly, knowl, etc.
-        pass
+        for newform in self.data['newforms']:
+            yield (newform['label'], url_for_newform_label(newform['label']), newform['dim'], newform['nf_label'], )
 
     def oldspace_decomposition(self):
         # Returns a latex string giving the decomposition of the old part.  These come from levels M dividing N, with the conductor of the character dividing M.
-        pass
+        return "\oplus".join("S_{{{k}}}^{{\mathrm{{new}}}}({N}, [\chi_{{{chi_modulus}}}({chi_index}, \cdot))^{{\oplus 2}}"%(k=old['weight'], N=old['level'], chi_modulus=old['level'], chi_index=old['minimal_conrey']) for old in self.data['oldspaces'])
