eb9685a1bb6ae9026dd44099a2dd8631bcbf6430
David Roe <roed.math@gmail.com>
2018-09-30 20:36:29 -0400

    Change some h3s to h2s, list people in the credits, add subspaces know

--- lmfdb/classical_modular_forms/main.py
+++ lmfdb/classical_modular_forms/main.py
@@ -25,8 +25,7 @@ def learnmore_list_remove(matchstring):
     return filter(lambda t:t[0].find(matchstring) <0, learnmore_list())
 
 def credit():
-    # FIXME
-    return "???"
+    return "Alex Best, Jonathan Bober, Andrew Booker, Edgar Costa, John Cremona, David Roe, Andrew Sutherland, John Voight"
 
 def set_info_funcs(info):
     info["mf_url"] = lambda mf: url_for_label(mf['label'])
