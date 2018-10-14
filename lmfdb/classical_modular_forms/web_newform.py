1e12d14d5a4f407da199914fbf916167ba3a4daf
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-10-14 16:44:54 -0400

    missing )

--- lmfdb/classical_modular_forms/web_newform.py
+++ lmfdb/classical_modular_forms/web_newform.py
@@ -469,7 +469,7 @@ function switch_basis(btype) {
     def _display_re(self, x, prec):
         if abs(x) < 10**(-prec):
             return ""
-        return r"%s"%(display_float(x, prec).replace('-','&minus;')
+        return r"%s"%(display_float(x, prec).replace('-','&minus;'))
 
     def _display_im(self, y, prec):
         if abs(y) < 10**(-prec):
