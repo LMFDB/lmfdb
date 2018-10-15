0d6f31bd96f357629bd9c2b85bef09c210a2fb21
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-10-15 13:47:05 -0400

    todo and verify data

--- scripts/classical_modular_forms/verify_data.py
+++ scripts/classical_modular_forms/verify_data.py
@@ -1,11 +1,40 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),"../.."))
 from lmfdb.db_backend import db, SQL
-from sage.all import CDF
+from sage.all import CDF, dimension_new_cusp_forms
+from dirichlet_conrey import DirichletGroup_conrey, DirichletCharacter_conrey
 
 
 # TODO
 # check that the first root matches the other root to the right precision
+def verify_dimensions(n,k):
+    totaldim = 0
+    for ns in db.mf_newspaces.search({'level':n, 'weight':k}, projection = ['hecke_orbit_dims', 'char_orbit_index','char_labels','label']):
+        index = ns['char_orbit_index']
+        hecke_orbit_dims = ns['hecke_orbit_dims']
+        hecke_orbit_dims.sort()
+        dim = sum(hecke_orbit_dims)
+        totaldim += dim
+        char_labels = ns['char_labels']
+        sage_dim, sage_indexes = dim_and_char_labels(n,k,index)
+        assert dim == sage_dim
+        assert sorted(sage_indexes) == sorted(char_labels)
+        hecke_orbit_dims_nf = sorted(list(db.mf_newforms.search({'space_label': ns['label'])}, projection = 'dim'))
+        assert hecke_orbit_dims == hecke_orbit_dims_nf
+
+    assert totaldim = dimension_new_cusp_forms(Gamma1(n), k)
+
+
+
+
+def dim_and_char_labels(n,k,index):
+    G = DirichletGroup_conrey(n)
+    char = G[67]
+    indexes = [elt.number() for elt in char.galois_orbit()]
+    dim = dimension_new_cusp_forms(char.sage_character(),k)
+    return dim, indexes
+
+
 # compute newform dimensions in Sage and compare with dims in mf_newspaces
 # compare with sum of dims in mf_newforms
 
@@ -69,6 +98,12 @@ if len(sys.argv) == 3:
             print "%.2f %s" % (100*float(i)/bound, nf['label'])
             verify_Lfunctions(nf)
             verify_embeddings(nf)
+    bound = db.mf_newforms.max('Nk2')
+    for level in range(1, db.mf_newforms.max('level') + 1):
+        for weight in range(2, db.mf_newforms.max('weight') + 1):
+            if level*weight*weight <= bound and (level + weight)%k == start:
+                verify_dimensions(level, weight)
+
 else:
     print r"""Usage:
         You should run this on legendre as: (this will use 40 cores):
