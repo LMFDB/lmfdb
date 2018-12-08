5cc7a2bc9dcd773a7e683141191aa99bbf1d2274
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-12-07 22:34:53 -0500

    reset start

--- lmfdb/classical_modular_forms/main.py
+++ lmfdb/classical_modular_forms/main.py
@@ -652,7 +652,7 @@ def common_parse(info, query):
     parse_character(info, query, 'char_label', qfield='char_orbit_index')
     parse_character(info, query, 'prim_label', qfield='prim_orbit_index', level_field='char_conductor', conrey_field=None)
     parse_ints(info, query, 'char_order', name="Character order")
-    prime_mode = info.get('prime_quantifier','exact')
+    prime_mode = info['prime_quantifier'] = info.get('prime_quantifier','exact')
     parse_primes(info, query, 'level_primes', name='Primes dividing level', mode=prime_mode, radical='level_radical')
 
 def preparse_self_twist(info):
--- lmfdb/static/lmfdb.js
+++ lmfdb/static/lmfdb.js
@@ -356,14 +356,43 @@ function simult_change(event) {
 };
 
 
+
+function resetStart()
+{
+  // resets start if not changing search_type
+  $('input[name=start]').val('')
+  // this will be cleaned by the cleanSubmit
+}
+
 function cleanSubmit(id)
 {
-    var myForm = document.getElementById(id);
-    var allInputs = myForm.getElementsByTagName('input');
-    var allSelects = myForm.getElementsByTagName('select');
-    var item, i, n = 0;
-
-    for(i = 0; item = allInputs[i]; i++) { if (item.getAttribute('name') ) { if (!item.value) { item.setAttribute('name', ''); } else { n++ }; } }
-    for(i = 0; item = allSelects[i]; i++) { if (item.getAttribute('name') ) { if (!item.value) { item.setAttribute('name', ''); } else { n++ }; } }
-    if (!n) { var all = document.createElement('input'); all.type='hidden'; all.name='all'; all.value='1'; myForm.appendChild(all); }
+  var myForm = document.getElementById(id);
+  var allInputs = myForm.getElementsByTagName('input');
+  var allSelects = myForm.getElementsByTagName('select');
+  var item, i, n = 0;
+  for(i = 0; item = allInputs[i]; i++) {
+    if (item.getAttribute('name') ) {
+      if (!item.value) {
+        item.setAttribute('name', '');
+      } else {
+        n++
+      };
+    }
+  }
+  for(i = 0; item = allSelects[i]; i++) {
+    if (item.getAttribute('name') ) {
+      if (!item.value) {
+        item.setAttribute('name', '');
+      } else {
+        n++
+      };
+    }
+  }
+  if (!n) {
+    var all = document.createElement('input');
+    all.type='hidden';
+    all.name='all';
+    all.value='1';
+    myForm.appendChild(all);
+  }
 }
