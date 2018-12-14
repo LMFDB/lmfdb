171fc267abb6c7ff725e7896e5927690865889d7
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-12-14 13:33:31 -0500

    limiting downloads of traces to 1000 forms

--- lmfdb/classical_modular_forms/main.py
+++ lmfdb/classical_modular_forms/main.py
@@ -499,7 +499,7 @@ class CMF_download(Downloader):
     def download_multiple_traces(self, info):
         lang = info.get(self.lang_key,'text').strip()
         query = literal_eval(info.get('query', '{}'))
-        forms = list(db.mf_newforms.search(query, projection=['label', 'hecke_orbit_code']))
+        forms = list(db.mf_newforms.search(query, projection=['label', 'hecke_orbit_code'],limit=1000))
         codes = [form['hecke_orbit_code'] for form in forms]
         traces = db.mf_hecke_nf.search({'hecke_orbit_code':{'$in':codes}}, projection=['hecke_orbit_code', 'n', 'trace_an'], sort=[])
         trace_dict = defaultdict(dict)
