5be45b3abc4d6d347da8ec8442f4d555afe5a26b
Alex J. Best <alex.j.best@gmail.com>
2018-12-19 03:16:34 -0500

    experiment debouncing

--- lmfdb/static/lmfdb.js
+++ lmfdb/static/lmfdb.js
@@ -165,8 +165,7 @@ function knowl_click_handler($el) {
  * @see jquery's doc about 'live'! the handler function does the 
  *  download/show/hide magic. also add a unique ID, 
  *  necessary when the same reference is used several times. */
-$(function() {
-  $("body").on("click", "*[knowl]", function(evt) {
+function knowl_handle(evt) {
       evt.preventDefault();
       var $knowl = $(this);
       if(!$knowl.attr("knowl-uid")) {
@@ -175,7 +174,9 @@ $(function() {
         knowl_id_counter++;
       }
       knowl_click_handler($knowl, evt);
-  });
+  }
+$(function() {
+  $("body").on("click", "*[knowl]", debounce(knowl_handle,500, true));
 });
 
 /*** end knowl js section ***/
@@ -254,4 +255,73 @@ function get_count_callback(res) {
         $("#download-msg").html("");
         $("#download-form").show();
     }
+};
+
+
+
+/**
+ * https://github.com/component/debounce
+ * Returns a function, that, as long as it continues to be invoked, will not
+ * be triggered. The function will be called after it stops being called for
+ * N milliseconds. If `immediate` is passed, trigger the function on the
+ * leading edge, instead of the trailing. The function also has a property 'clear' 
+ * that is a function which will clear the timer to prevent previously scheduled executions. 
+ *
+ * @source underscore.js
+ * @see http://unscriptable.com/2009/03/20/debouncing-javascript-methods/
+ * @param {Function} function to wrap
+ * @param {Number} timeout in ms (`100`)
+ * @param {Boolean} whether to execute at the beginning (`false`)
+ * @api public
+ */
+function debounce(func, wait, immediate){
+	var timeout, args, context, timestamp, result;
+	if (null == wait) wait = 100;
+
+	function later() {
+		var last = Date.now() - timestamp;
+
+		if (last < wait && last >= 0) {
+			timeout = setTimeout(later, wait - last);
+		} else {
+			timeout = null;
+			if (!immediate) {
+				result = func.apply(context, args);
+				context = args = null;
+			}
+		}
+	};
+
+	var debounced = function(){
+		context = this;
+		args = arguments;
+		timestamp = Date.now();
+		var callNow = immediate && !timeout;
+		if (!timeout) timeout = setTimeout(later, wait);
+		if (callNow) {
+			result = func.apply(context, args);
+			context = args = null;
+		}
+
+		return result;
+	};
+
+	debounced.clear = function() {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = null;
+		}
+	};
+
+	debounced.flush = function() {
+		if (timeout) {
+			result = func.apply(context, args);
+			context = args = null;
+
+			clearTimeout(timeout);
+			timeout = null;
+		}
+	};
+
+	return debounced;
 };
