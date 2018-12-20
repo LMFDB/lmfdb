9f8baffaa14e55706a6d626896d2a735bb7089c7
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-12-20 15:26:57 +0000

    slideshow after render

--- lmfdb/static/lmfdb.js
+++ lmfdb/static/lmfdb.js
@@ -207,7 +207,6 @@ function knowl_click_handler($el) {
       log("cache hit: " + knowl_id);
       $output.hide();
       $output.html(knowl_cache[knowl_id]);
-      $output.slideDown("slow");
       try
       {
         renderMathInElement($output.get(0), katexOpts);
@@ -215,6 +214,8 @@ function knowl_click_handler($el) {
       catch(err) {
         log("err:" + err)
       }
+      log("rendered?")
+      $output.slideDown("slow");
 
     } else {
       $output.addClass("loading");
