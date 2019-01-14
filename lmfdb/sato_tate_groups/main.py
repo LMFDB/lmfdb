2a1beb275ccfa90dd5c6288357af48ee2d70a618
AndrewVSutherland <drew@math.mit.edu>
2019-01-14 17:37:59 -0500

    add dynamic support for SU(2) x mu(n)

--- lmfdb/sato_tate_groups/main.py
+++ lmfdb/sato_tate_groups/main.py
@@ -422,7 +422,6 @@ def nu1_mu_info(w,n):
 def nu1_mu_portrait(n):
     """ returns an encoded scatter plot of the nth roots of unity in the complex plane """
     if n == 1:
-        print db.gps_sato_tate_lookup('1.2.1.2.1a')
         return db.gps_sato_tate.lookup('1.2.1.2.1a').get('trace_histogram')
     if n <= 120:
         plot =  sum([line2d([(-2*cos(2*pi*m/n),-2*sin(2*pi*m/n)),(2*cos(2*pi*m/n),2*sin(2*pi*m/n))],thickness=3) for m in range(n)]) + circle((0,0),0.3,fill=True)
