97bfdeece55f9358f6bb03bf9d4a3ba1870bc1fc
Edgar Costa <edgarcosta@math.dartmouth.edu>
2019-01-19 17:50:30 -0500

    new L-fun

--- lmfdb/classical_modular_forms/web_newform.py
+++ lmfdb/classical_modular_forms/web_newform.py
@@ -248,7 +248,7 @@ class WebNewform(object):
         res.append(('Newspace ' + ns_label, ns_url))
         nf_url = ns_url + '/' + self.hecke_orbit_label
 
-        if self.Nk2 <= 4000:
+        if self.Nk2 <= 20000:
             if db.lfunc_instances.exists({'url': nf_url[1:]}):
                 res.append(('L-function ' + self.label, '/L' + nf_url))
             if len(self.conrey_indexes)*self.rel_dim > 50:
