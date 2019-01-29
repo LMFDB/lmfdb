90cfc219db46a26ac2cef51913027f2abf3cf24f
David Roe <roed.math@gmail.com>
2019-01-29 02:56:38 -0500

    Don't katex bigint knowls

--- lmfdb/utils.py
+++ lmfdb/utils.py
@@ -683,8 +683,8 @@ def bigint_knowl(n, cutoff=16, max_width=100, sides=2):
             width = 1 + (len(lng)-1) // lines
             lng = [lng[i:i+width] for i in range(0,len(lng),width)]
             for i in range(len(lng)-1):
-                lng[i] = r"<tr><td>\(%s\)\</td></tr>" % lng[i]
-            lng[-1] = r"<tr><td>\(%s\)</td></tr>" % lng[-1]
+                lng[i] = r"<tr><td>%s\</td></tr>" % lng[i]
+            lng[-1] = r"<tr><td>%s</td></tr>" % lng[-1]
             lng = "<table>" + "".join(lng) + "</table>"
         else:
             lng = r"\(%s\)" % lng
