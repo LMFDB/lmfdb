50a7c5393f52d483caf115840738d904358b16b4
Edgar Costa <edgarcosta@math.dartmouth.edu>
2019-01-31 18:03:03 -0500

    trivial change

--- lmfdb/abvar/__init__.py
+++ lmfdb/abvar/__init__.py
@@ -6,7 +6,6 @@ from flask import Blueprint
 abvar_page = Blueprint("abvar", __name__, template_folder='templates', static_folder="static")
 abvar_logger = make_logger(abvar_page)
 
-
 @abvar_page.context_processor
 def body_class():
     return {'body_class': 'abvar'}
