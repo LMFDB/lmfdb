24ad492a2c79a2059463d46cfd45bc944c0db693
Edgar Costa <edgarc@mit.edu>
2019-02-24 19:18:58 -0500

    pyflakes

--- scripts/classical_modular_forms/check_ap2_amn_parallel.py
+++ scripts/classical_modular_forms/check_ap2_amn_parallel.py
@@ -1,22 +1,12 @@
-import traceback, time, sys, os, inspect, argparse, textwrap
-from timeout_decorator import timeout, TimeoutError
+import sys, os
 try:
     # Make lmfdb available
     sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),"../.."))
 except NameError:
     pass
-from lmfdb.backend.database import db, SQL, Composable, IdentifierWrapper as Identifier, Literal
-from types import MethodType
-from collections import defaultdict
-from lmfdb.utils import names_and_urls
-from sage.all import (Integer, prod, floor, mod, euler_phi, prime_pi,
-        cached_function, ZZ, RR, ComplexField, Gamma1, Gamma0, PolynomialRing,
-        dimension_new_cusp_forms, dimension_eis, prime_range,
-        dimension_cusp_forms, dimension_modular_forms, kronecker_symbol,
-        gap, psi, infinity, CC, gcd)
-from dirichlet_conrey import DirichletGroup_conrey, DirichletCharacter_conrey
-from datetime import datetime
-
+from lmfdb.backend.database import db
+from sage.all import CC, prime_range, gcd
+from dirichlet_conrey import DirichletGroup_conrey
 
 
 def check_ap2_slow(rec):
@@ -35,13 +25,13 @@ def check_ap2_slow(rec):
             return False
     return True
 
+pairs = [(m,n) for m in range(2,1000) for n in range(m, 1000) if gcd(m,n) == 1 and m*n <= 1000]
+
 def check_amn_slow(rec):
     Z = [0] + [CC(*elt) for elt in rec['an_normalized']]
-    for pp in prime_range(len(Z)-1):
-        for k in range(1, (len(Z) - 1)//pp + 1):
-            if gcd(k, pp) == 1:
-                if (Z[pp*k] - Z[pp]*Z[k]).abs() > 1e-11:
-                    return False
+    for m, n in pairs:
+        if (Z[m*n] - Z[m]*Z[n]).abs() > 1e-11:
+            return False
     return True
 
 import sys
@@ -53,13 +43,13 @@ if len(sys.argv) == 3:
     assert j >= 0
     chunk_size = bound/k + 1
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'mf_hecke_cc_parallel.%d.log' % j), 'w') as F:
-    for rec in db.mf_hecke_cc.search({'id':{'$gte':j*chunk_size, '$lt':(j+1)*chunk_size}},['lfunction_label', 'an_normalized']):
-        if !check_amn_slow(rec):
-            F.write('%s:amn\n' % rec['lfunction_label'])
-            F.flush()
-        if !check_ap2_slow(rec):
-            F.write('%s:ap2\n' % rec['lfunction_label'])
-            F.flush()
+        for rec in db.mf_hecke_cc.search({'id':{'$gte':j*chunk_size, '$lt':(j+1)*chunk_size}},['lfunction_label', 'an_normalized']):
+            if not check_amn_slow(rec):
+                F.write('%s:amn\n' % rec['lfunction_label'])
+                F.flush()
+            if not check_ap2_slow(rec):
+                F.write('%s:ap2\n' % rec['lfunction_label'])
+                F.flush()
 else:
     print r"""Usage:
         You should run this on legendre as: (this will use 40 cores):
