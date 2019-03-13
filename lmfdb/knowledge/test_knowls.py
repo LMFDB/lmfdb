9720d77bea23b2005af5444c62c145e07ef11a13
Edgar Costa <edgarc@mit.edu>
2019-03-13 16:48:47 -0400

    todo

--- lmfdb/knowledge/test_knowls.py
+++ lmfdb/knowledge/test_knowls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # testing users blueprint
 from lmfdb.tests import LmfdbTest
-from lmfdb.knowledge.knowl import knowldb
+#from lmfdb.knowledge.knowl import knowldb
 
 class KnowlTestCase(LmfdbTest):
     pass
