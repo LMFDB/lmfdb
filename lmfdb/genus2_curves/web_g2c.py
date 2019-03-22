98480ba17421ee923509746f31bfbdc929305a08
AndrewVSutherland <drew@math.mit.edu>
2019-03-22 16:13:35 -0400

    find cmf friends by trace hash

--- lmfdb/genus2_curves/web_g2c.py
+++ lmfdb/genus2_curves/web_g2c.py
@@ -10,6 +10,7 @@ from lmfdb.number_fields.web_number_field import nf_display_knowl
 from lmfdb.galois_groups.transitive_group import group_display_knowl
 from lmfdb.sato_tate_groups.main import st_link_by_name
 from lmfdb.genus2_curves import g2c_logger
+from lmfdb.classical_modular_forms.main import url_for_label as url_for_cmf
 from sage.all import latex, ZZ, QQ, CC, PolynomialRing, factor, implicit_plot, point, real, sqrt, var,  nth_prime
 from sage.plot.text import text
 from flask import url_for
@@ -633,6 +634,8 @@ class WebG2C(object):
                     add_friend (friends, lfunction_friend_from_url(url))
             else:
                 add_friend (friends, lfunction_friend_from_url(friend_url))
+        for friend_label in db.mf_newforms.search({'trace_hash':data['Lhash']},"label"):
+            add_friend (friends, ("Modular form " + friend_label, url_for_cmf(friend_label)))
         if 'split_labels' in data:
             for friend_label in data['split_labels']:
                 if is_curve:
