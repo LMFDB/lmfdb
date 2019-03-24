1d75538ea3d0bc5b49c37f81b9ef9e7011c6e7ae
AndrewVSutherland <drew@math.mit.edu>
2019-03-24 02:14:30 -0400

    tweak re matching

--- lmfdb/modular_forms/maass_forms/maass_waveforms/views/mwf_main.py
+++ lmfdb/modular_forms/maass_forms/maass_waveforms/views/mwf_main.py
@@ -80,17 +80,17 @@ def render_maass_waveforms(level=0, weight=-1, character=-1, r1=0, r2=0, **kwds)
     if info.get('maass_id', None) and info.get('db', None):
         return render_one_maass_waveform_wp(**info)
     if info['search'] or (info['browse'] and int(info['weight']) != 0):
-        if info['level']:
-            if not re.match(POSINT_RE, info['level']):
-                if "-" in info['level']:
-                    info['level'] = "..".join(info['level'].split("-"))
-                if not re.match(POSINT_RANGE_RE, info['level']):
-                    flash_error("%s is not a level, please specify a positive integer <span style='color:black'>n</span> or postivie integer range <span style='color:black'>m..n</span>.", info['level'])
+        if info['level_range']:
+            if not re.match(POSINT_RE, info['level_range']):
+                if "-" in info['level_range']:
+                    info['level'] = "..".join(info['level_range'].split("-"))
+                if not re.match(POSINT_RANGE_RE, info['level_range']):
+                    flash_error("%s is not a level, please specify a positive integer <span style='color:black'>n</span> or postivie integer range <span style='color:black'>m..n</span>.", info['level_range'])
                     return render_template('mwf_navigate.html', **info)
         if info['character']:
-            if info['level'] and not re.match(POSINT_RE, info['level']):
+            if info['level_range'] and not re.match(POSINT_RE, info['level_range']):
                 flash_error("Character cannot be specified in combination with a range of levels.", info['character'])
-            N = int(info['level']) if info['level'] else 0
+            N = int(info['level_range']) if info['level'] else 0
             if not re.match(r'^[1-9][0-9]*\.[1-9][0-9]*$', info['character']):
                 flash_error("%s is not a valid label for a Dirichlet character.  It should be of the form <span style='color:black'>q.n</span>, where q and n are coprime positive integers with n < q, or q=n=1.", info['character'])
                 return render_template('mwf_navigate.html', **info)
