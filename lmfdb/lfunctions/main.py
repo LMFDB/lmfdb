130a084932b202883b45a2573d16acc1c48c2d2b
AndrewVSutherland <drew@math.mit.edu>
2019-04-04 12:03:38 -0400

    fix plot (again)

--- lmfdb/lfunctions/main.py
+++ lmfdb/lfunctions/main.py
@@ -974,7 +974,7 @@ def generateLfunctionFromUrl(*args, **kwds):
 
     elif args[0] == "ArtinRepresentation":
         try:
-            return ArtinLfunctionDB(ArtinLfunctionDB,label=str(args[1]))
+            return ArtinLfunctionDB(label=str(args[1]))
         except:
             pass
         return ArtinLfunction(label=str(args[1]))
