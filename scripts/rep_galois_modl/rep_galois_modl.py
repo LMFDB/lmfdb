c886d1cdb6babc60a2e39d08ee7cd904814a8f27
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-08 10:02:15 -0700

    Fixed PYTHON_E225 issues in scripts/rep_galois_modl/rep_galois_modl.py

--- scripts/rep_galois_modl/rep_galois_modl.py
+++ scripts/rep_galois_modl/rep_galois_modl.py
@@ -16,7 +16,7 @@ import sys
 import gzip
 from lmfdb.base import getDBConnection
 
-C= getDBConnection()
+C = getDBConnection()
 import yaml
 pw_dict = yaml.load(open(os.path.join(os.getcwd(), "passwords.yaml")))
 username = pw_dict['data']['username']
@@ -37,7 +37,7 @@ def makels(li):
 
 def string2list(s):
   s = str(s)
-  if s=='': return []
+  if s == '': return []
   return [int(a) for a in s.split(',')]
 
 
@@ -61,32 +61,32 @@ print "finished indices"
 
 ## Main importing function
 
-label_dict={}
+label_dict = {}
 
 def label_lookup(base_label):
     if base_label in label_dict:
-	n=label_dict[base_label]+1
-	label_dict[base_label]=n
+	n = label_dict[base_label]+1
+	label_dict[base_label] = n
     	return n
-    label_dict[base_label]=1
+    label_dict[base_label] = 1
     return 1	
 
 
 def do_import(ll):
     base_field,dim,field,conductor,primes_conductor,weight,abs_irr,rep_type,image_type,image_label,image_at,image_order,degree_proj_field,projective_type,projective_label,bad_prime_list,good_prime_list,poly_ker,poly_proj_ker,related_objects = ll
-    mykeys=['base_field','dim','field','conductor','primes_conductor',
+    mykeys = ['base_field','dim','field','conductor','primes_conductor',
 'weight','abs_irr','rep_type','image_type','image_label','image_at',
 'image_order','degree_proj_field','projective_type','projective_label','bad_prime_list','good_prime_list','poly_ker','poly_proj_ker','related_objects']
     data = {}
     for j in range(len(mykeys)):
         data[mykeys[j]] = ll[j]
-    data['field_char']=data['field'][0]
-    data['field_deg']=data['field'][1]
-    data['field_order']=pow(data['field_char'], data['field_deg'])
+    data['field_char'] = data['field'][0]
+    data['field_deg'] = data['field'][1]
+    data['field_order'] = pow(data['field_char'], data['field_deg'])
     blabel = base_label(data['base_field'],data['field_order'],data['dim'], data['conductor'])
     data['base_label'] = blabel
     data['index'] = label_lookup(blabel)
-    label= last_label(blabel, data['index'])
+    label = last_label(blabel, data['index'])
     data['label'] = label
 # we need still to organize this better with respect to tie breaks 
 
