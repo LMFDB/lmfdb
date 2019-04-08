a3762cf8e69a817cc11c9a6cef25d3060e2fc899
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-08 10:01:29 -0700

    Fixed PYTHON_E225 issues in scripts/artin_representations/pull-reps.py

--- scripts/artin_representations/pull-reps.py
+++ scripts/artin_representations/pull-reps.py
@@ -3,22 +3,22 @@ pw_filename = "../../../xyzzy"
 password = open(pw_filename, "r").readlines()[0].strip()
 
 from pymongo.mongo_client import MongoClient
-C= MongoClient(port=37010)
+C = MongoClient(port=37010)
 C['artin'].authenticate('editor', password)
 C['numberfields'].authenticate('editor', password)
 C['limbo'].authenticate('editor', password)
 
-art=C.artin
-rep=art.representations
-nfgal=art.field_data
+art = C.artin
+rep = art.representations
+nfgal = art.field_data
 
 print "rep is artin representations"
 print "nfgal is the nfgalois group database"
 
 artargs = {'Dim': {'$gte': 2, '$lte': 9}, 'Hide': 0}
-allarts=rep.find(artargs)
+allarts = rep.find(artargs)
 
-logfile=open("artlist", "w")
+logfile = open("artlist", "w")
 
 for a in allarts:
   print "."
