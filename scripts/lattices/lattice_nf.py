8155b337d6a9828a8da0c96f278f232dbbce62a5
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-08 10:06:04 -0700

    Fixed PYTHON_E225 issues in scripts/lattices/lattice_nf.py

--- scripts/lattices/lattice_nf.py
+++ scripts/lattices/lattice_nf.py
@@ -18,7 +18,7 @@ from lmfdb.number_fields.number_field import poly_to_field_label
 
 
 from pymongo.mongo_client import MongoClient
-C= MongoClient(port=37010)
+C = MongoClient(port=37010)
 import yaml
 pw_dict = yaml.load(open(os.path.join(os.getcwd(), "passwords.yaml")))
 username = pw_dict['data']['username']
@@ -34,36 +34,36 @@ saving = True
 
 def add_lattice_nf(ll):
     n_field,gram_input = ll
-    gram_input=[[int(i) for i in l] for l in gram_input] 
+    gram_input = [[int(i) for i in l] for l in gram_input] 
 
     R = PolynomialRing(QQ, 'x');
     nf_label = poly_to_field_label(R(n_field))
 
     lattice = l1.find_one({'gram': gram_input })
     if lattice is None:
-        n=len(gram_input[0])
-        d=matrix(gram_input).determinant()
-        result=[B for B in l1.find({'dim': int(n), 'det' : int(d)}) if isom(gram_input, B['gram'])]
-        if len(result)==1:
-            lat_label =result[0]['label']
+        n = len(gram_input[0])
+        d = matrix(gram_input).determinant()
+        result = [B for B in l1.find({'dim': int(n), 'det' : int(d)}) if isom(gram_input, B['gram'])]
+        if len(result) == 1:
+            lat_label = result[0]['label']
             is_lat_in = "yes"
-        elif len(result)>1:
+        elif len(result) > 1:
             print "... need to be checked ..."
             print "***********"
         else :
             lat_label = "new"
             is_lat_in = gram_input
     else:
-       lat_label=lattice['label']
+       lat_label = lattice['label']
        is_lat_in = "yes"
     
     try:
-        lab=nf_label+lat_label
+        lab = nf_label+lat_label
     except:
         print nf_label, lat_label
         print "fail"
             
-    res=l2.find_one({'label': lab })
+    res = l2.find_one({'label': lab })
     if res is None:
         print "new data"
         if saving:
