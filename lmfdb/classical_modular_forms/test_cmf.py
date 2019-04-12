a0d45c4af3d1fdbb37aaa78ae27020659ed2bd7d
Edgar Costa <edgarc@mit.edu>
2019-04-12 14:02:14 -0400

    updating test

--- lmfdb/classical_modular_forms/test_cmf.py
+++ lmfdb/classical_modular_forms/test_cmf.py
@@ -18,7 +18,7 @@ class CmfTest(LmfdbTest):
         assert '?search_type=Dimensions&dim=1' in data
         assert '?search_type=SpaceDimensions&char_order=1' in data
         assert "./stats" in data
-        data = self.tc.get("/ModularForm/GL2/Q/holomorphic/?search_type=SpaceDimensions",follow_redirects=True).data
+        data = self.tc.get("/ModularForm/GL2/Q/holomorph/ic/?search_type=SpaceDimensions",follow_redirects=True).data
         assert r'<a href="/ModularForm/GL2/Q/holomorphic/23/12/">229</a>' in data
         data = self.tc.get("/ModularForm/GL2/Q/holomorphic/?search_type=SpaceDimensions&char_order=1", follow_redirects=True).data
         assert r'<a href="/ModularForm/GL2/Q/holomorphic/18/4/a/">1</a>' in data
@@ -340,12 +340,13 @@ class CmfTest(LmfdbTest):
                     assert 'Root' in page.data
 
     def test_embedded_invariants(self):
-        for url in ['/ModularForm/GL2/Q/holomorphic/13/2/e/a/4/1/', '/ModularForm/GL2/Q/holomorphic/13/2/e/a/10/1/']:
+        for url in ['/ModularForm/GL2/Q/holomorphic/13/2/e/a/4/1/',
+                    '/ModularForm/GL2/Q/holomorphic/13/2/e/a/10/1/']:
 
             page = self.tc.get(url)
             # root
             assert 'Root' in page.data
-            assert '0.500000'  in page.data
+            assert '0.500000' in page.data
             assert '0.866025' in page.data
             # p = 13
             for n in ['2.50000', '2.59808', '0.693375', '0.720577']:
@@ -358,7 +359,6 @@ class CmfTest(LmfdbTest):
             assert 'Newspace 13.2.e' in page.data
             assert 'Newform 13.2.e.a' in page.data
             assert 'Dual Form 13.2.e.a.' in page.data
-            assert 'Isogeny class 169.a' in page.data
             assert 'L-function 13.2.e.a.' in page.data
 
             assert '0.103805522628' in page.data
