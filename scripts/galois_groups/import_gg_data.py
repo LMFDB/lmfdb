772ccab46b98f388d2d2dcf1661121b0fa6855d0
John Jones <jj@asu.edu>
2019-04-25 13:20:53 -0400

    Starting script update

--- scripts/galois_groups/import_gg_data.py
+++ scripts/galois_groups/import_gg_data.py
@@ -1,28 +1,33 @@
 # -*- coding: utf-8 -*-
 import sys
-import bson
-assert bson
 import os
 assert os
 import json
 
-sys.path.append("../")
-from lmfdb import base
-from lmfdb.website import dbport
-base._init(dbport, "")
-C = base.getDBConnection()
+sys.path.append("../..")
+from lmfdb import db
 
-gr = C.transitivegroups.groups
+gr = db.gps_transitive
 
-labels = ['label', 'n', 't', 'auts', 'order', 'parity', 'ab', 'prim', 'cyc', 'solv', 'subs', 'repns', 'resolve', 'name', 'pretty']
+# need to compute label, gapidfull, moddecompuniq, pretty
+# need to fix parity
 
-gps = gr.find({})
-myfile = open('gg_data')
+mykeys = ['ab', 'arith_equiv', 'auts', 'cyc', 'gapid', 'n', 'order', 'parity', 'prim', 'repns', 'resolve', 'solv', 'subs', 't', 'name']
+
+#myfile = open('gg_data')
+myfile = open(sys.argv[1])
 
 for l in myfile:
   v= json.loads(l)
-  data = {}
-  for j in range(len(v)):
-    data[labels[j]] = v[j]
+  for dd in v:
+    for vals in dd:
+      data=dict(zip(mykeys, vals))
+      if data['parity'] == 0:
+        data['parity'] = -1
+      data['repns'] = [r for r in data['repns'] if r[0]<24]
+      old= gr.lucky({'n': data['n'], 't': data['t']})
+      for fld in ['ab', 'arith_equiv', 'auts', 'cyc', 'gapid', 'n', 'order', 'parity', 'prim', 'repns', 'resolve', 'solv', 'subs', 't']:
+        if data[fld] != old[fld]:
+          print "Mismatch (%d,%d) on %s: %s --- %s"%(data['n'],data['t'],fld, str(data[fld]), str(old[fld]))
 
 
