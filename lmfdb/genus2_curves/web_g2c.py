224e2a2addb7d26d1d4f88778b3779b24c6b3ea1
AndrewVSutherland <drew@math.mit.edu>
2019-04-25 15:21:46 -0400

    debug

--- lmfdb/genus2_curves/web_g2c.py
+++ lmfdb/genus2_curves/web_g2c.py
@@ -419,6 +419,8 @@ def lfunction_friend_from_url(url):
     if parts[0] == "EllipticCurve":
         cond = convert_IQF_label(parts[1],parts[2])
         label = parts[1] + "-" + cond + "-" + parts[3]
+        print "cond",cond
+        print "label",label
         return ("EC isogeny class " + label, "/" + url)
     if parts[0] == "ModularForm" and parts[1] == "GL2" and parts[2] == "TotallyReal" and parts[4] == "holomorphic":
         label = parts[5]
