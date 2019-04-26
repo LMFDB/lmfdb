ddf96c759697a545d1412d98b281c9c7a845877f
John Jones <jj@asu.edu>
2019-04-26 11:55:02 -0400

    More updates to galois group import script

--- scripts/galois_groups/import_gg_data.py
+++ scripts/galois_groups/import_gg_data.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/sage -python
 # -*- coding: utf-8 -*-
 import sys
 import os
@@ -17,6 +18,8 @@ mykeys = ['ab', 'arith_equiv', 'auts', 'cyc', 'gapid', 'n', 'order', 'parity', '
 #myfile = open('gg_data')
 myfile = open(sys.argv[1])
 
+outrecs=[]
+
 for l in myfile:
   v= json.loads(l)
   for dd in v:
@@ -24,10 +27,17 @@ for l in myfile:
       data=dict(zip(mykeys, vals))
       if data['parity'] == 0:
         data['parity'] = -1
-      data['repns'] = [r for r in data['repns'] if r[0]<24]
-      old= gr.lucky({'n': data['n'], 't': data['t']})
-      for fld in ['ab', 'arith_equiv', 'auts', 'cyc', 'gapid', 'n', 'order', 'parity', 'prim', 'repns', 'resolve', 'solv', 'subs', 't']:
-        if data[fld] != old[fld]:
-          print "Mismatch (%d,%d) on %s: %s --- %s"%(data['n'],data['t'],fld, str(data[fld]), str(old[fld]))
+      data['label'] = "%dT%d"%(data['n'], data['t'])
+      data['gapidfull'] = ""
+      if data['gapid']>0:
+          data['gapidfull'] = "[%d,%d]"%(data['order'],data['gapid'])
+      outrecs.append(data)
+
+if len(outrecs)>0:
+  gr.insert_many(outrecs)
+
+      #for fld in ['ab', 'arith_equiv', 'auts', 'cyc', 'gapid', 'n', 'order', 'parity', 'prim', 'repns', 'resolve', 'solv', 'subs', 't']:
+      #  if data[fld] != old[fld]:
+      #    print "Mismatch (%d,%d) on %s: %s --- %s"%(data['n'],data['t'],fld, str(data[fld]), str(old[fld]))
 
 
