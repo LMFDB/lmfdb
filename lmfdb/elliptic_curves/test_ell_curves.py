2b2a4efa35a6b318084bcb2b8323e8678849fe77
AndrewVSutherland <drew@math.mit.edu>
2019-05-10 08:57:07 -0400

    fix tests

--- lmfdb/elliptic_curves/test_ell_curves.py
+++ lmfdb/elliptic_curves/test_ell_curves.py
@@ -27,15 +27,13 @@ class EllCurveTest(LmfdbTest):
         assert '/EllipticCurve/Q/12350/s/' in L.data
 
     def test_Cremona_label_mal(self):
-        L = self.tc.get('/EllipticCurve/Q/?label=Cremona%3A12qx&jump=label+or+isogeny+class')
-        print L.data
+        L = self.tc.get('/EllipticCurve/Q/?label=Cremona%3A12qx&jump=label+or+isogeny+class', follow_redirects=True)
         assert '12qx' in L.data and 'does not define a recognised elliptic curve' in L.data
 
     def test_missing_curve(self):
-        L = self.tc.get('/EllipticCurve/Q/13.a1')
-        print L.data
+        L = self.tc.get('/EllipticCurve/Q/13.a1', follow_redirects=True)
         assert '13.a1' in L.data and 'No curve' in L.data
-        L = self.tc.get('/EllipticCurve/Q/13a1')
+        L = self.tc.get('/EllipticCurve/Q/13a1', follow_redirects=True)
         assert '13a1' in L.data and 'No curve' in L.data
 
     def test_Cond_search(self):
