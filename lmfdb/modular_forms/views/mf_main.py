2204938802166291b9c066ac717414b4dcff02c8
AndrewVSutherland <drew@math.mit.edu>
2019-05-12 15:51:00 -0400

    pyflakes

--- lmfdb/modular_forms/views/mf_main.py
+++ lmfdb/modular_forms/views/mf_main.py
@@ -1,5 +1,5 @@
 from lmfdb.app import app
-from flask import render_template, url_for
+from flask import redirect, url_for
 from lmfdb.modular_forms import mf, MF, mf_logger
 
 
@@ -12,4 +12,4 @@ mf_logger.debug("EN_V path: {0}".format(app.jinja_loader.searchpath))
 
 @mf.route("/")
 def modular_form_main_page():
-    return flask.redirect(url_for('modular_forms'),301)
+    return redirect(url_for('modular_forms'),301)
