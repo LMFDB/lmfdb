6bd3dacc5a741f921c13a3605ed3ed89d44db9e7
David Roe <roed.math@gmail.com>
2019-05-22 17:03:50 -0700

    Working on subgroup lattice

--- lmfdb/groups/abstract/web_groups.py
+++ lmfdb/groups/abstract/web_groups.py
@@ -35,14 +35,47 @@ class WebAbstractGroup(WebObj):
     def __init__(self, label, data=None):
         WebObj.__init__(self, label, data)
         self.tex_name = group_names_pretty(label) # remove once in database
-        self.subgroups = {subdata['counter']: WebAbstractSubgroup(self, subdata['label'], subdata) for subdata in db.gps_subgroups.search({'ambient':label})} # Should join with gps_groups to get pretty names for subgroup and quotient
-        self.conjugacy_classes = {ccdata['counter']: WebAbstractConjClass(self, ccdata['label'], ccdata) for ccdata in db.gps_groups_cc.search({'group':label})}
-        self.characters = {chardata['counter']: WebAbstractCharacter(self, chardata['label'], chardata) for chardata in db.gps_char.search({'group':label})} # Should join with creps once we have images and join queries
-        self.maximal_subgroup_of = [WebAbstractSupergroup(self, 'sub', supdata['label'], supdata) for supdata in db.gps_subgroups.search({'subgroup':label, 'maximal':True})]
-        self.maximal_quotient_of = [WebAbstractSupergroup(self, 'quo', supdata['label'], supdata) for supdata in db.gps_subgroups.search({'quotient':label, 'minimal_normal':True})]
-        self.direct_products = [sub for sub in self.subgroups.values() if sub.normal and sub.direct and sub.subgroup_order != 1 and sub.quotient_order != 1]
-        self.semidirect_products = [sub for sub in self.subgroups.values() if sub.normal and sub.split and not sub.direct]
-        self.nonsplit_products = [sub for sub in self.subgroups.values() if sub.normal and not sub.split]
+
+    @lazy_attribute
+    def subgroups(self):
+        # Should join with gps_groups to get pretty names for subgroup and quotient
+        return {subdata['counter']: WebAbstractSubgroup(self, subdata['label'], subdata) for subdata in db.gps_subgroups.search({'ambient': self.label})}
+
+    @lazy_attribute
+    def conjugacy_classes(self):
+        return {ccdata['counter']: WebAbstractConjClass(self, ccdata['label'], ccdata) for ccdata in db.gps_groups_cc.search({'group': self.label})}
+
+    @lazy_attribute
+    def characters(self):
+        # Should join with creps once we have images and join queries
+        return {chardata['counter']: WebAbstractCharacter(self, chardata['label'], chardata) for chardata in db.gps_char.search({'group': self.label})}
+
+    @lazy_attribute
+    def maximal_subgroup_of(self):
+        # Could show up multiple times as non-conjugate maximal subgroups in the same ambient group
+        # So we should elimintate duplicates from the following list
+        return [WebAbstractSupergroup(self, 'sub', supdata['label'], supdata) for supdata in db.gps_subgroups.search({'subgroup': self.label, 'maximal':True}, sort=['ambient_order','ambient'], limit=10)]
+
+    @lazy_attribute
+    def maximal_quotient_of(self):
+        # Could show up multiple times as a quotient of different normal subgroups in the same ambient group
+        # So we should elimintate duplicates from the following list
+        return [WebAbstractSupergroup(self, 'quo', supdata['label'], supdata) for supdata in db.gps_subgroups.search({'quotient': self.label, 'minimal_normal':True}, sort=['ambient_order', 'ambient'])]
+
+    @lazy_attribute
+    def direct_products(self):
+        # Need to pick an ordering
+        return [sub for sub in self.subgroups.values() if sub.normal and sub.direct and sub.subgroup_order != 1 and sub.quotient_order != 1]
+
+    @lazy_attribute
+    def semidirect_products(self):
+        # Need to pick an ordering
+        return [sub for sub in self.subgroups.values() if sub.normal and sub.split and not sub.direct]
+
+    @lazy_attribute
+    def nonsplit_products(self):
+        # Need to pick an ordering
+        return [sub for sub in self.subgroups.values() if sub.normal and not sub.split]
 
     @lazy_attribute
     def subgroup_layers(self):
@@ -67,10 +100,26 @@ class WebAbstractGroup(WebObj):
         print [[gp.subgroup for gp in layer] for layer in layers]
         return layers
 
-    def p_sylow(self, p):
+    def sylow_subgroups(self):
+        """
+        Returns a list of pairs (p, P) where P is a WebAbstractSubgroup representing a p-Sylow subgroup.
+        """
+        syl_dict = {}
         for sub in self.subgroups.values():
-            if sub.sylow == p:
-                return sub.subgroup_tex
+            if sub.sylow > 0:
+                syl_dict[sub.sylow] = sub
+        syl_list = []
+        for p, e in self.factored_order:
+            if p in syl_dict:
+                syl_list.append((p, syl_dict[p]))
+        return syl_list
+
+    def series(self):
+        return [('group.%s'%name,
+                 name.replace('_',' ').capitalize(),
+                 [self.subgroups[i] for i in getattr(self, name)],
+                 "-".join(map(str, getattr(self, name))))
+                for name in ['derived_series', 'chief_series', 'lower_central_series', 'upper_central_series']]
 
     def is_null(self):
         return self._data is None
@@ -203,6 +252,14 @@ class WebAbstractSubgroup(WebObj):
         self.subgroup_tex = db.gps_small.lookup(self.subgroup, 'pretty') # temporary
         if 'quotient' in self._data:
             self.quotient_tex = db.gps_small.lookup(self.quotient, 'pretty') # temporary
+
+    def spanclass(self):
+        s = "subgp"
+        if self.characteristic:
+            s += " chargp"
+        elif self.normal:
+            s += " normgp"
+        return s
 
 class WebAbstractConjClass(WebObj):
     table = db.gps_groups_cc
