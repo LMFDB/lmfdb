b627b55c55b2f2f512f10a1da9392558c9cdcbed
Edgar Costa <edgarc@mit.edu>
2019-08-27 10:53:08 -0400

    correctly setting _read_and_write_knowls

--- lmfdb/backend/database.py
+++ lmfdb/backend/database.py
@@ -5558,9 +5558,11 @@ class PostgresDatabase(PostgresBase):
             self._read_and_write_knowls = True
             self._read_and_write_userdb = True
         else:
-            privileges = ['INSERT', 'SELECT', 'UPDATE', 'DELETE']
-            knowls_tables = ['kwl_deleted', 'kwl_history', 'kwl_knowls']
+            privileges = ['INSERT', 'SELECT', 'UPDATE']
+            knowls_tables = ['kwl_knowls']
             cur = sorted(list(self._execute(SQL("SELECT table_name, privilege_type FROM information_schema.role_table_grants WHERE grantee = %s AND table_name IN (" + ",".join(['%s']*len(knowls_tables)) + ") AND privilege_type IN (" + ",".join(['%s']*len(privileges)) + ")"), [self._user] +  knowls_tables + privileges)))
+            print cur
+            print sorted([(table, priv) for table in knowls_tables for priv in privileges])
             self._read_and_write_knowls = cur == sorted([(table, priv) for table in knowls_tables for priv in privileges])
 
             cur = sorted(list(self._execute(SQL("SELECT privilege_type FROM information_schema.role_table_grants WHERE grantee = %s AND table_schema = %s AND table_name=%s AND privilege_type IN (" + ",".join(['%s']*len(privileges)) + ")"), [self._user,  'userdb', 'users'] + privileges)))
