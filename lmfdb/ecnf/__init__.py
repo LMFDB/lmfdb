615c8f94b352e22c5ed337c8dd64ecd2b98e2b71
David Lowry-Duda <davidlowryduda@davidlowryduda.com>
2019-09-19 09:07:44 -0400

    Add elliptic_curve_nf searcher to api2
    I chose the name elliptic_curve_nf. I note that this should essentially
    never change once anyone uses the API.

--- lmfdb/ecnf/__init__.py
+++ lmfdb/ecnf/__init__.py
@@ -2,7 +2,7 @@
 from lmfdb.app import app
 from lmfdb.logger import make_logger
 from flask import Blueprint
-from lmfdb.api2.searchers import register_singleton
+from lmfdb.api2.searchers import register_singleton, register_search_function
 import searchers
 
 ecnf_page = Blueprint("ecnf", __name__, template_folder='templates', static_folder="static")
@@ -18,5 +18,11 @@ assert main # to keep pyflakes quiet
 
 app.register_blueprint(ecnf_page, url_prefix="/EllipticCurve")
 
+register_search_function(
+    "elliptic_curves_nf",
+    "Elliptic Curves over Number Fields",
+    "Search over elliptic curves devined over number fields",
+    auto_search='ec_nfcurves'
+)
 register_singleton('EllipticCurve', 'ec_nfcurves',
     simple_search = searchers.ecnf_simple_label_search)
