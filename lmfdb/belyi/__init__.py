6c6fc8d79fc11350ea0ca1e4156b227950500cad
Edgar Costa <edgarc@mit.edu>
2019-12-17 18:29:01 -0500

    reformatting

--- lmfdb/belyi/__init__.py
+++ lmfdb/belyi/__init__.py
@@ -3,15 +3,18 @@ from lmfdb.app import app
 from lmfdb.logger import make_logger
 from flask import Blueprint
 
-belyi_page = Blueprint("belyi", __name__, template_folder='templates',
-        static_folder="static")
+belyi_page = Blueprint(
+    "belyi", __name__, template_folder="templates", static_folder="static"
+)
 belyi_logger = make_logger(belyi_page)
+
 
 @belyi_page.context_processor
 def body_class():
-    return {'body_class': 'belyi'}
+    return {"body_class": "belyi"}
+
 
 import main
-assert main # silence pyflakes
+assert main  # silence pyflakes
 
 app.register_blueprint(belyi_page, url_prefix="/Belyi")
