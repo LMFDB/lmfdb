b26a551d7bc02f2d0385f6cdc84222b3a56b6ea1
Edgar Costa <edgarc@mit.edu>
2020-01-25 16:16:51 -0500

    typo

--- lmfdb/genus2_curves/main.py
+++ lmfdb/genus2_curves/main.py
@@ -514,7 +514,7 @@ class G2CSearchArray(SearchArray):
         )
 
         torsion_structure = TextBox(
-            name="torsion_structure",
+            name="torsion",
             knowl="g2c.torsion",
             label="Torsion structure",
             short_label="Torsion",
