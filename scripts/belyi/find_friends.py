f0781eb3dfe70e13e17f0866d1e7cea160cb7845
Sam Schiavone <sschiavo@mit.edu>
2020-03-28 22:01:53 -0400

    fixed pyflakes bugs, moved files

--- scripts/belyi/find_friends.py
+++ scripts/belyi/find_friends.py
@@ -1,5 +1,5 @@
 from lmfdb import db
-from lmfdb.ecnf.WebEllipticCurve import parse_NFelt, parse_ainvs
+from lmfdb.ecnf.WebEllipticCurve import parse_ainvs
 from lmfdb.belyi.main import hyperelliptic_polys_to_ainvs, curve_string_parser
 from scripts.ecnf.import_utils import NFelt
 from ast import literal_eval
@@ -41,51 +41,40 @@ def genus1_lookup_equation_QQ(rec):
     ainvs = hyperelliptic_polys_to_ainvs(f,h)
     E = EllipticCurve(ainvs)
     j = E.j_invariant()
-    for r in db.ec_curves.search({"jinv":str(j)}): # is there a better way to search than by j-invariant? Conductor?
+    for r in db.ec_curves.search({"jinv":str(j)}):
         ainvs2 = r['ainvs']
         E2 = EllipticCurve(ainvs2)
         if E.is_isomorphic(E2):
             return r['lmfdb_label']
-    print "Curve not found in database"
+    #print("Curve not found in database")
     return None
 
 def genus1_lookup_equation_nf(rec):
     assert rec['g'] == 1
     # make base field
-    R = PolynomialRing(QQ, "x")
-    x = R.gens()[0]
     nf_matches = list(db.nf_fields.search({'coeffs':rec['base_field']}))
     if len(nf_matches) == 0:
-        print "Base field not found in database"
         return None
     else:
-        #print "Base field found"
         nf_rec = nf_matches[0]
         # make curve
         f,h = curve_string_parser(rec)
         ainvs = hyperelliptic_polys_to_ainvs(f,h)
         E = EllipticCurve(ainvs)
         K = E.base_field()
-        a = K.gens()[0]
         j = E.j_invariant()
         j_str = NFelt(j)
-        #cond_nrm = (E.conductor()).norm()
-        #print "Curve has j-invariant %s over %s" % (j, K)
-        #print "\nCurve has j-invariant %s, represented as %s" % (j, j_str)
-        j_matches = list(db.ec_nfcurves.search({"field_label":nf_rec['label'] , "jinv":j_str})) # is there a better way to search than by j-invariant? Conductor norm?
-        #j_matches = list(db.ec_nfcurves.search({"field_label":nf_rec['label'] , "jinv":j_str, 'conductor_norm':cond_nrm})) # throws weird Postgres error
-        #print "Found %d curves with same j-invariant" % len(j_matches)
+        j_matches = list(db.ec_nfcurves.search({"field_label":nf_rec['label'] , "jinv":j_str}))
         for r in j_matches:
             ainvs2 = parse_ainvs(K, r['ainvs'])
             E2 = EllipticCurve(ainvs2)
             assert E.base_field() == E2.base_field()
             if E.is_isomorphic(E2):
                 return r['label']
-    print "Curve not found in database"
+    #print("Curve not found in database")
     return None
 
-# TODO: problems if curve defined over smaller field than Belyi map.
-# This code won't find base changes.
+# This code might not find base changes.
 def genus1_lookup_equation(rec):
     if rec['base_field'] == [-1, 1]: # if defined over QQ
         return genus1_lookup_equation_QQ(rec)
@@ -94,11 +83,11 @@ def genus1_lookup_equation(rec):
 
 def find_curve_label(rec):
     if rec['g'] == 1:
-        print("Searched for curve for %s") % rec['label']
+        #print("Searched for curve for %s") % rec['label']
         return genus1_lookup_equation(rec)
     elif rec['g'] == 2:
         if rec['base_field'] == [-1, 1]: # currently LMFDB only has g2 curves over QQ
-            print("Searched for curve for %s") % rec['label']
+            #print("Searched for curve for %s") % rec['label']
             return genus2_lookup_equation(rec)
 
 def find_curve_url(rec):
