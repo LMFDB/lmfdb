2ac217bb5816de70617450742ddf7fb64ee0088f
AndrewVSutherland <drew@math.mit.edu>
2020-06-07 11:25:42 -0400

    backward compatibility

--- lmfdb/users/main.py
+++ lmfdb/users/main.py
@@ -334,7 +334,10 @@ def restart():
     else:
         command = None
     if command:
-        out = Popen(command, stdout=PIPE, encoding='utf-8').communicate()[0] if sys.version_info[0] == 3 else Popen(command, stdout=PIPE).communicate()[0]
+        if sys.version_info[0] == 3:
+            out = Popen(command, stdout=PIPE, encoding='utf-8').communicate()[0]
+        else:
+            Popen(command, stdout=PIPE).communicate()[0]
         return out.replace('\n', '<br>')
     else:
         return "Only supported in beta.lmfdb.org, prodweb1.lmfdb.xyz, and prodweb2.lmfdb.xyz"
