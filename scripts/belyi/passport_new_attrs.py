8a364f74652f94a569ac61ba0f78d9bd7605ddd0
Sam Schiavone <sschiavo@mit.edu>
2020-06-08 23:28:09 -0400

    added passport triples script

---
+++ scripts/belyi/passport_new_attrs.py
@@ -0,0 +1,22 @@
+from lmfdb import db
+def assign_passport_triples(rec):
+    """
+    Given a passport rec, find all galmaps belonging to rec and save their triples to rec['triples']
+    """
+    galmaps = db.belyi_galmaps.search({'plabel':rec['plabel']})
+    trips = []
+    for g in galmaps:
+        trips.extend(g['triples'])
+    rec['triples'] = trips
+    return rec
+
+def assign_automorphism_group(rec):
+    """
+    Given a passport rec, compute its automorphism group as the centralizer of its monodromy group
+    """
+    gp = rec['group']
+    d,k = [ZZ(el) for el in gp.split("T")]
+    G = TransitiveGroup(d,k)
+    S = SymmetricGroup(d)
+    C = S.centralizer(G)
+    return C
