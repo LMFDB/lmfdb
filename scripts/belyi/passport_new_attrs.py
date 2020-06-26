b3f0cfccd9e31b9b0c1410b872586686a2a95086
Sam Schiavone <sschiavo@mit.edu>
2020-06-26 13:48:47 -0400

    aut group script

--- scripts/belyi/passport_new_attrs.py
+++ scripts/belyi/passport_new_attrs.py
@@ -10,14 +10,10 @@ def assign_passport_triples(rec):
     rec['triples'] = trips
     return rec
 
-# can actually pull from galmaps like triples above
 def assign_automorphism_group(rec):
     """
     Given a passport rec, compute its automorphism group as the centralizer of its monodromy group
     """
-    gp = rec['group']
-    d,k = [ZZ(el) for el in gp.split("T")]
-    G = TransitiveGroup(d,k)
-    S = SymmetricGroup(d)
-    C = S.centralizer(G)
-    return C
+    galmap = db.belyi_galmaps.lucky({'plabel':rec['plabel']})
+    rec['aut_group'] = galmap['aut_group']
+    return rec
