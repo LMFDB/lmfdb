62c18f35f9ccd8d47c0d619b168ab09506f3a1bc
AndrewVSutherland <drew@math.mit.edu>
2020-07-20 21:17:01 -0400

    downloads

--- lmfdb/maass_forms/web_maassform.py
+++ lmfdb/maass_forms/web_maassform.py
@@ -150,6 +150,11 @@ class WebMaassForm(object):
 
 
 class MaassFormDownloader(Downloader):
+    table = db.maass_newforms
+    title = 'Maass forms'
+    columns = ['maass_id', 'level', 'weight', 'conrey_index', 'spectral_parameter', 'symmetry', 'fricke_eigenvalue']
+    column_wrappers = {'eqn':literal_eval}
+    function_body = {'text':[]}
 
     def download(self, label, lang='text'):
         data = db.maass_newforms.lookup(label)
