6d83cf5d8ff821362743b949177688e6acc994aa
AndrewVSutherland <drew@math.mit.edu>
2020-07-25 09:51:44 -0400

    fix abvar test

--- lmfdb/abvar/fq/test_av.py
+++ lmfdb/abvar/fq/test_av.py
@@ -102,8 +102,8 @@ class AVTest(LmfdbTest):
         Check that the property box displays.
         """
         page = self.tc.get("/Variety/Abelian/Fq/2/4/ad_g").get_data(as_text=True).replace("\n", "").replace(" ", "")
-        assert '<divclass="properties-body"><table><tr><tdclass="label">Label</td><td>2.4.ad_g</td></tr><tr>' in page
-        assert r'<tdclass="label">Base field</td><td>$\F_{2^{2}}$</td></tr><tr><tdclass="label">Dimension</td><td>' in page
+        assert r'<divclass="properties-body"><table><tr><tdclass="label">Label</td><td>2.4.ad_g</td></tr><tr>' in page
+        assert r'<tdclass="label">Basefield</td><td>$\F_{2^{2}}$</td></tr><tr><tdclass="label">Dimension</td><td>' in page
         self.check_args("/Variety/Abelian/Fq/2/79/ar_go", "Principally polarizable")
 
     def test_split_Frobenius_angles(self):
