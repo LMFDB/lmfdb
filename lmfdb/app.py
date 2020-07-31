8e856c4bb20ec7b26edb9b27f9a5be06c687f531
Edgar Costa <edgarc@mit.edu>
2020-07-31 13:14:36 -0400

    reversed proxy

--- lmfdb/app.py
+++ lmfdb/app.py
@@ -18,7 +18,18 @@ LMFDB_VERSION = "LMFDB Release 1.1.1"
 #         Main app         #
 ############################
 
+class ReverseProxied(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        scheme = environ.get('HTTP_X_FORWARDED_PROTO')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
 app = Flask(__name__)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 ############################
 # App attribute functions  #
