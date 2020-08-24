0332eb1671a26d29dd9729b91c0c57e8f4fbcb22
rbommel <bommel@mit.edu>
2020-08-24 07:32:01 -0400

    There was another hidden merge conflict.

--- lmfdb/genus2_curves/web_g2c.py
+++ lmfdb/genus2_curves/web_g2c.py
@@ -744,11 +744,7 @@ class WebG2C(object):
                 data['two_torsion_field_knowl'] = r"splitting field of \(%s\) with Galois group %s" % (intlist_to_poly(t[1]),group_display_knowl(t[2][0],t[2][1]))
 
             tamalist = [[item['p'],item['tamagawa_number']] for item in tama]
-<<<<<<< HEAD
-            data['local_table'] = local_table (data['abs_disc'],data['cond'],tamalist,data['bad_lfactors_pretty'],clus)
-=======
             data['local_table'] = local_table (data['cond'],data['abs_disc'],tamalist,data['bad_lfactors_pretty'])
->>>>>>> upstream/master
 
         else:
             # invariants specific to isogeny class
