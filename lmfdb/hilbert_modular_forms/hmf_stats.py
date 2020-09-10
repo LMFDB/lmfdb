a8b60e17d63c59091be310b8737ee5b7cfeb695a
David Roe <roed.math@gmail.com>
2020-09-10 03:33:44 -0400

    Working on hmf stats

--- lmfdb/hilbert_modular_forms/hmf_stats.py
+++ lmfdb/hilbert_modular_forms/hmf_stats.py
@@ -3,89 +3,69 @@ from flask import url_for
 from lmfdb import db
 from lmfdb.app import app
 from lmfdb.utils import comma
+from lmfdb.utils.display_stats import StatsDisplay, proportioners, totaler
 from lmfdb.logger import make_logger
 from lmfdb.number_fields.web_number_field import nf_display_knowl
 from sage.misc.cachefunc import cached_method
 
-def field_sort_key(F):
-    dsdn = F.split(".")
-    return (int(dsdn[0]), int(dsdn[2]))  # by degree then discriminant
-
 logger = make_logger("hmf")
 
-the_HMFstats = None
-
-def get_counts():
-    global the_HMFstats
-    if the_HMFstats is None:
-        the_HMFstats = HMFstats()
-    return the_HMFstats.counts()
-
-def get_stats(d=None):
-    global the_HMFstats
-    if the_HMFstats is None:
-        the_HMFstats = HMFstats()
-    return the_HMFstats.stats(d)
-
-def hmf_summary():
-    counts = get_stats().counts()
-    hmf_knowl = '<a knowl="mf.hilbert">Hilbert modular forms</a>'
-    nf_knowl = '<a knowl="nf.totally_real">totally real number fields</a>'
-    return ''.join([r'The database currently contains %s ' % counts['nforms_c'],
-                    hmf_knowl,
-                    r', over %s ' % counts['nfields'],
-                    nf_knowl, r' (not including $\mathbb{Q}$)',
-                    ' of degree up to %s' % counts['maxdeg']
-                ])
-
-@app.context_processor
-def ctx_hmf_summary():
-    return {'hmf_summary': hmf_summary}
-
-def hmf_degree_summary(d):
-    stats = get_stats(d)
-    hmf_knowl = '<a knowl="mf.hilbert">Hilbert modular forms</a>'
-    nf_knowl = '<a knowl="nf.totally_real">totally real number fields</a>'
-    level_knowl = '<a knowl="mf.hilbert.level_norm">level norm</a>'
-    return ''.join([r'The database currently contains %s ' % stats['nforms'],
-                    hmf_knowl,
-                    r' defined over %s ' % stats['nfields'],
-                    nf_knowl,
-                    r' of degree %s, with ' % d,
-                    level_knowl,
-                    r' up to %s.' % stats['maxnorm']])
-
-def hmf_field_summary(F):
-    stats = get_stats()
-    hmf_knowl = '<a knowl="mf.hilbert">Hilbert modular forms</a>'
-    nf_knowl = '<a knowl="nf.totally_real">totally real number field</a>'
-    level_knowl = '<a knowl="mf.hilbert.level_norm">level norm</a>'
-    d = F.split(".")[0]
-    return ''.join([r'The database currently contains %s ' % stats[d][F]['nforms'],
-                    hmf_knowl,
-                    r' defined over the totally real ',
-                    nf_knowl,
-                    r', with ',
-                    level_knowl,
-                    r' up to %s.' % stats[d][F]['maxnorm']])
-
-class HMFstats(object):
+class HMFstats(StatsDisplay):
     """
     Class for creating and displaying statistics for Hilbert modular forms
     """
-
     def __init__(self):
-        logger.debug("Constructing an instance of HMFstats")
+        self.nforms = db.hmf_forms.count()
+
+    table = db.hmf_forms
+    baseurl_func = ".hilbert_modular_form_render_webpage"
+
+    stat_list = [
+        {'cols': ['level_norm', 'deg'],
+         'totaler': totaler(),
+         'proportioner': proportioners.per_col_total},
+        {'cols': ['level_norm', 'dimension'],
+         'totaler': totaler(),
+         'proportioner': proportioners.per_col_total},
+    ]
+    buckets = {'level_norm': ['1', '2-10', '11-100', '101-1000', '1001-10000'],
+               'dimension': ['1', '2', '3', '4', '5-10', '11-20', '21-100', '101-1000']}
+    knowls = {'level_norm': 'mf.hilbert.level_norm',
+              'dimension': 'mf.hilbert.dimension',
+              'deg': 'nf.degree'}
+    top_titles = {'deg': 'degree'}
+
+    @property
+    def short_summary(self):
+        return "The database currently contains %s Hilbert modular forms over fields up to degree %s.  Here are some <a href='%s'>further statistics</a>." % (comma(self.nforms), self.counts()["maxdeg"], url_for(".statistics"))
+
+    @property
+    def summary(self):
+        return self.short_summary
+
+    def degree_summary(self, d):
+        stats = self.statistics(d)
+        hmf_knowl = '<a knowl="mf.hilbert">Hilbert modular forms</a>'
+        nf_knowl = '<a knowl="nf.totally_real">totally real number fields</a>'
+        level_knowl = '<a knowl="mf.hilbert.level_norm">level norm</a>'
+        return ''.join([r'The database currently contains %s ' % stats['nforms'],
+                        hmf_knowl,
+                        r' defined over %s ' % stats['nfields'],
+                        nf_knowl,
+                        r' of degree %s, with ' % d,
+                        level_knowl,
+                        r' up to %s.' % stats['maxnorm']])
 
     @cached_method
     def counts(self):
         counts = {}
 
-        nforms = db.hmf_forms.count()
-        counts['nforms']  = nforms
-        counts['nforms_c']  = comma(nforms)
 
-        fields = list(db.hmf_fields.search({}, ["degree", "discriminant", "label"]))
+        counts['nforms']  = self.nforms
+        counts['nforms_c']  = comma(self.nforms)
+
+        attrs = ["degree", "discriminant", "label"]
+        fields = list(db.hmf_fields.search({}, attrs, sort=attrs))
         degrees = sorted(set(F["degree"] for F in fields))
         by_deg = {d: [F for F in fields if F["degree"] == d] for d in degrees}
         counts["degrees"] = degrees
@@ -99,9 +79,9 @@ class HMFstats(object):
         return counts
 
     @cached_method
-    def stats(self, d=None):
-        if d:
-            return self.stats()[d]
+    def statistics(self, d=None):
+        if d is not None:
+            return self.statistics()[int(d)]
         nstats = db.hmf_forms.stats.numstats("level_norm", "field_label")
         counts = db.hmf_forms.stats.column_counts("field_label")
         nstats_by_deg = db.hmf_forms.stats.numstats("level_norm", "deg")
@@ -110,7 +90,7 @@ class HMFstats(object):
         stats = {d: {"fields": C["fields_by_degree"][d],
                      "nfields": C["nfields_by_degree"][d],
                      "nforms": counts_by_deg[d],
-                     "max_norm": nstats_by_deg[d]["max"],
+                     "maxnorm": nstats_by_deg[d]["max"],
                      "counts": {F: {"nforms": counts[F],
                                     "maxnorm": nstats[F]["max"],
                                     "field_knowl": nf_display_knowl(F, F),
