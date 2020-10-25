10f3e695f64eecc97f258208cdb7802d8229fdc4
AndrewVSutherland <drew@math.mit.edu>
2020-10-25 11:08:48 -0400

    Hilbert

--- lmfdb/hilbert_modular_forms/hmf_stats.py
+++ lmfdb/hilbert_modular_forms/hmf_stats.py
@@ -41,18 +41,22 @@ class HMFstats(StatsDisplay):
 
     @property
     def summary(self):
-        return "The database currently contains %s %s over %s fields up to degree %s." % (comma(self.nforms), display_knowl('mf.hilbert', 'Hilbert modular forms'), self.counts()["nfields"], self.counts()["maxdeg"])
+        hmf_knowl = '<a knowl="mf.hilbert">Hilbert modular forms</a>'
+        nf_knowl = '<a knowl="nf.totally_real">totally real number fields</a>'
+        deg_knowl = '<a knowl="nf.degree">degree</a>'
+        return "The database currently contains %s %s over %s %s of %s up to %s." % (comma(self.nforms), hmf_knowl, self.counts()["nfields"], nf_knowl, deg_knowl, self.counts()["maxdeg"])
 
     def degree_summary(self, d):
         stats = self.statistics(d)
         hmf_knowl = '<a knowl="mf.hilbert">Hilbert modular forms</a>'
         nf_knowl = '<a knowl="nf.totally_real">totally real number fields</a>'
+        deg_knowl = '<a knowl="nf.degree">degree</a>'
         level_knowl = '<a knowl="mf.hilbert.level_norm">level norm</a>'
         return ''.join([r'The database currently contains %s ' % stats['nforms'],
                         hmf_knowl,
                         r' defined over %s ' % stats['nfields'],
                         nf_knowl,
-                        r' of degree %s, with ' % d,
+                        r' of %s %s, with ' % (deg_knowl, d),
                         level_knowl,
                         r' up to %s.' % stats['maxnorm']])
 
