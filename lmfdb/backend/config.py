c042ba6ce79fd9bc96536867390048cbb7c1cf86
Edgar Costa <edgarc@mit.edu>
2020-10-28 23:46:20 -0400

    doc

--- lmfdb/backend/config.py
+++ lmfdb/backend/config.py
@@ -36,7 +36,7 @@ class Configuration(object):
       - ``postgresql_user`` -- the username when connecting to the database
       - ``postgresql_password`` -- the password for connecting to the database
       - ``writeargstofile`` - a boolean, if config file doesn't exist, it determines if command line arguments are written to the config file instead of the default arguments
-      - ``writeargstofile`` - a boolean, if config file doesn't exist, it determines if command line arguments are written to the config file instead of the default arguments
+      - ``readargs`` - a boolean, if determines if command line arguments are read
     """
     def __init__(self, parser=None, defaults={}, writeargstofile=False, readargs=None):
         if readargs is None:
