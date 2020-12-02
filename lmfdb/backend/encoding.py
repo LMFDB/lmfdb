dd834f6c86eb22364ed3181e7252cd364802893f
Edgar Costa <edgarc@mit.edu>
2020-12-01 23:51:49 -0500

    legacy error

--- lmfdb/backend/encoding.py
+++ lmfdb/backend/encoding.py
@@ -15,7 +15,7 @@ try:
     from sage.all import ceil
     try:
         from sage.rings.complex_mpfr import ComplexNumber
-    except ModuleNotFoundError:
+    except ImportError:
         from sage.rings.complex_number import ComplexNumber
     from sage.rings.complex_field import ComplexField
     from sage.rings.real_mpfr import RealLiteral, RealField, RealNumber
