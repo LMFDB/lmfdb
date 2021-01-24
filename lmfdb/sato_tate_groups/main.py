9b2f6b521bf34c3b1cfc0f632c97c7d21dda2970
AndrewVSutherland <drew@math.mit.edu>
2021-01-24 15:14:31 -0500

    debug

--- lmfdb/sato_tate_groups/main.py
+++ lmfdb/sato_tate_groups/main.py
@@ -754,7 +754,7 @@ def render_by_label(label):
     if data.get("character_diagonal"):
         d = data["character_diagonal"]
         info["character_diagonal"] = r"\ \ \ \mathrm{E}\left[\chi_i^2\right] = " + string_matrix([[d[i] for i in range(len(d))]])
-    n = data['components']
+    n = QQ(data['components'])
     if data.get('zvector'):
         z = data['zvector']
         print(z)
