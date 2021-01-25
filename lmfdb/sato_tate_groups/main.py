e0bbb1bb79ca8d9e39694a8a8fdf58875e6fb8de
AndrewVSutherland <drew@math.mit.edu>
2021-01-25 12:22:31 -0500

    z-vectors in genus 2

--- lmfdb/sato_tate_groups/main.py
+++ lmfdb/sato_tate_groups/main.py
@@ -780,7 +780,7 @@ def render_by_label(label):
                 s += '<tr><th></th><th>$-$</th><th>$a_2\\in\\mathbb{Z}$</th><th>' + '</th><th>'.join(["$a_2=%s$" % (i) for i in range(-1,3)]) + '</th></tr>'
                 s += '<tr><th>$-$</th><td align="center">$1$</td><td align="center">$%s$</td><td align="center">' % (sum(z[1:5])/n)
                 s += '</td><td align="center">'.join(["$%s$" % (z[1+i]/n) for i in range(4)]) + '</td></tr>'
-                s += '<tr><th>$a_1=0$</td><td align="center">$%s$</td><td align="center">$%s$</td><td>' % (z[0]/n,sum(z[5:9])/n)
+                s += '<tr><th>$a_1=0$</td><td align="center">$%s$</td><td align="center">$%s$</td><td align="center">' % (z[0]/n,sum(z[5:9])/n)
                 s += '</td><td align="center">'.join(["$%s$" % (z[5+i]/n) for i in range(4)]) + "</td></tr>"
                 s += "</table>"
             info['probabilities'] = s
