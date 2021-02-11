5354542217633f68f46c73f4a342abcef145fc94
John Jones <jj@asu.edu>
2021-02-11 16:38:39 -0500

    Fix js logic on icons

--- lmfdb/static/raw_typeset.js
+++ lmfdb/static/raw_typeset.js
@@ -1,33 +1,41 @@
-  function SelectText(element) {
-    var txt = document.getElementById(element);
-    var selection = window.getSelection();
-    var range = document.createRange();
-    range.selectNodeContents(txt);
-    selection.removeAllRanges();
-    selection.addRange(range);
-  }
+function SelectText(element) {
+  var txt = document.getElementById(element);
+  var selection = window.getSelection();
+  var range = document.createRange();
+  range.selectNodeContents(txt);
+  selection.removeAllRanges();
+  selection.addRange(range);
+}
 
-function setallraw () {
+function setallraw (iconpath) {
   $(".tset-raw").each(function (i,elt) {
-  if ($(elt).attr("israw") == "0") {
-    $(elt).attr("tset", $(elt).html());
-    $(elt).html($(elt).attr("raw"));
-    $(elt).attr("israw", "1");
+    var eltid = $(elt).prop("id");
+    var matchinfo = eltid.match(/tset-raw-(\d+)$/);
+    var eltidnum = matchinfo[1];
+    if ($(elt).attr("israw") == "0") {
+      $(elt).attr("tset", $(elt).html());
+      $(elt).html($(elt).attr("raw"));
+      $(elt).attr("israw", "1");
+      $("#tset-raw-icon-"+eltidnum)[0].src=iconpath;
   }});
 }
 
-function clearallraw () {
+function clearallraw (iconpath) {
   $(".tset-raw").each(function (i,elt) {
-  if ($(elt).attr("israw") == "1") {
-    $(elt).html($(elt).attr("tset"));
-    $(elt).attr("israw", "0");
+    var eltid = $(elt).prop("id");
+    var matchinfo = eltid.match(/tset-raw-(\d+)$/);
+    var eltidnum = matchinfo[1];
+    if ($(elt).attr("israw") == "1") {
+      $(elt).html($(elt).attr("tset"));
+      $(elt).attr("israw", "0");
+      $("#tset-raw-icon-"+eltidnum)[0].src=iconpath;
   }});
 }
 
 function rawtset (clickidorig) {
   var clickid = "#"+clickidorig;
   if ($(clickid).attr("israw")=="0") {
-    clearallraw();
+    clearallraw("foo");
     $(clickid).attr("israw","1");
     $(clickid).attr("tset", $(clickid).html());
     $(clickid).html($(clickid).attr("raw"));
@@ -44,8 +52,8 @@ function iconrawtset(idnum) {
   var iconid = "tset-raw-icon-"+idnum;
   var iconsrc = $("#"+iconid)[0].src;
   var iconRe = /^(.*)(.2.)\.png$/;
-  matcharray = iconsrc.match(iconRe);
-  if (matcharray[2] == "t2r") {
+  var matcharray = iconsrc.match(iconRe);
+  if ($("#"+spanid).attr("israw") == "0") {
     $("#"+iconid)[0].src = matcharray[1]+"r2t.png"
     $("#"+spanid).attr("tset", $("#"+spanid).html());
     $("#"+spanid).attr("israw", "1");
@@ -61,12 +69,12 @@ function iconrawtsetall() {
   var iconid = "tset-raw-icon-all";
   var iconsrc = $("#"+iconid)[0].src;
   var iconRe = /^(.*)(.2.)\.png$/;
-  matcharray = iconsrc.match(iconRe);
+  var matcharray = iconsrc.match(iconRe);
   if (matcharray[2] == "t2r") {
-    $("#"+iconid)[0].src = matcharray[1]+"r2t.png"
-    setallraw();
+    $("#"+iconid)[0].src = matcharray[1]+"r2t.png";
+    setallraw(matcharray[1]+"r2t.png");
   } else {
-    $("#"+iconid)[0].src = matcharray[1]+"t2r.png"
-    clearallraw();
+    $("#"+iconid)[0].src = matcharray[1]+"t2r.png";
+    clearallraw(matcharray[1]+"t2r.png");
   }
 }
