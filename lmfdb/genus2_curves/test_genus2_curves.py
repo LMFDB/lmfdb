0176e5cbdef6189751a7b302e575ee77d985c66b
AndrewVSutherland <drew@math.mit.edu>
2021-03-12 14:03:59 -0500

    tweak

--- lmfdb/genus2_curves/test_genus2_curves.py
+++ lmfdb/genus2_curves/test_genus2_curves.py
@@ -282,7 +282,7 @@ class Genus2Test(LmfdbTest):
                     ),
                 ('/Genus2Curve/Q/576/a/',
                     ('L-function',
-                        'Elliptic curve isogeny class 2.2.8.1-9.1-a',
+                        'Elliptic curve 2.2.8.1-9.1-a',
                         'Modular form 24.2.d.a',
                         'Hilbert modular form 2.2.8.1-9.1-a',)
                     )
