5e8356b955ce940f69864a8388770f6f78baa24e
AndrewVSutherland <drew@math.mit.edu>
2021-03-19 22:10:01 -0400

    fix mu display

--- lmfdb/sato_tate_groups/main.py
+++ lmfdb/sato_tate_groups/main.py
@@ -623,9 +623,12 @@ def mu_info(n):
     rec['moments'] = [['x'] + [ r'\mathrm{E}[x^{%d}]'%m for m in range(13)]]
     rec['moments'] += [['a_1'] + ['1' if m % n == 0  else '0' for m in range(13)]]
     rec['trace_moments'] = trace_moments(rec['moments'])
+    rec['second_trace_moment'] = 1 if n <= 2 else 0
+    rec['fourth_trace_moment'] = 1 if n <= 2 else 0
     rational_traces = [1] if n%2 else [1,-1]
     rec['counts'] = [['a_1', [[t,1] for t in rational_traces]]]
-    rec['probabilities'] = [[r'\mathrm{Pr}[a_1=%d]=\frac{1}{%d}'%(m,n) for m in rational_traces]]
+    T = [['$\\mathrm{Pr}[a_1=%s]=\frac{1}{%s}$'%(m,n) for m in rational_traces]]
+    rec['probabilities'] = "<table><tr>" + "<tr></tr>".join(["<td>" + "<td></td".join(r) + "</td>" for r in T]) + "</tr></table>"
     return rec
 
 def mu_portrait(n):
