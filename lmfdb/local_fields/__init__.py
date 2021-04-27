bb0db1f7a77dcf8c0e745795abf62c0fed8c4cbf
John Jones <jj@asu.edu>
2021-04-27 18:06:52 -0400

    Hopefully final LocalNumberField -> padicField changes

--- lmfdb/local_fields/__init__.py
+++ lmfdb/local_fields/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import
 from lmfdb.app import app
 from lmfdb.logger import make_logger
-from flask import Blueprint
+from flask import Blueprint, request, redirect
 from lmfdb.api2.searchers import register_search_function
 
 local_fields_page = Blueprint("local_fields", __name__, template_folder='templates', static_folder="static")
@@ -16,8 +16,17 @@ def body_class():
 from . import main
 assert main
 
+from six.moves.urllib.parse import urlparse, urlunparse
+@local_fields_page.before_request
+def redirect_local():
+        urlparts = urlparse(request.url)
+        if 'LocalNumberField' in urlparts.path:
+            urlparts = urlparts._replace(path=urlparts.path.replace('LocalNumberField', 'padicField'))
+            return redirect(urlunparse(urlparts), 301)
+        return
+
+
 app.register_blueprint(local_fields_page, url_prefix="/padicField")
-app.register_blueprint(local_fields_page, url_prefix="/LocalNumberField")
 
 register_search_function(
     "$p$-adic_fields",
