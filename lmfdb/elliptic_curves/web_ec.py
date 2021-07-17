ac015d54bda0af768d065772efa1dadb5ca838db
AndrewVSutherland <drew@math.mit.edu>
2021-07-17 13:17:32 -0400

    add degrees

--- lmfdb/elliptic_curves/web_ec.py
+++ lmfdb/elliptic_curves/web_ec.py
@@ -84,6 +84,17 @@ def gl2_subgroup_data(label):
         info += row_wrap('Rouse & Zureick-Brown label', "<a href={prefix}{label}.html>{label}</a>".format(prefix= RZB_URL_PREFIX, label=data['RZBlabel']))
     if data.get('SZlabel'):
         info += row_wrap('Sutherland & Zywina label', data['SZlabel'])
+    N = ZZ(data['level'])
+    ell = N.prime_divisors()[0]
+    e = N.valuation(ell)
+    if e == 1:
+        info += row_wrap("Cyclic %s-isogeny field degree" % (ell), min([r[1] for r in data['isogeny_orbits'] if r[0] == ell]))
+        info += row_wrap("Cyclic %s-torsion field degree" % (ell), min([r[1] for r in data['orbits'] if r[0] == ell]))
+        info += row_wrap("Full %s-torsion field degree" % (ell), ell*(ell-1)*(ell-1)*(ell+1) // data['index'])
+    else:
+        info += row_wrap("Cyclic %s^n-isogeny field degrees" % (ell), ", ".join(["%s"%(min([r[1] for r in data['isogeny_orbits'] if r[0] == ell^n])) for n in range(1,e+1)]))
+        info += row_wrap("Cyclic %s^n-torsion field degrees" % (ell), ", ".join(["%s"%(min([r[1] for r in data['orbits'] if r[0] == ell^n])) for n in range(1,e+1)]))
+        info += row_wrap("Full %s^n-torsion field degrees" % (ell), ", ".join(["%s"%(ell*(ell-1)*(ell-1)*(ell+1)*ell**(4*n) // data['index']) for n in range(1,e+1)]))
     if data['genus'] > 0:
         info += row_wrap('Newforms', ''.join(['<a href="%s">%s</a>' % (cmf_url_for_label(x), x) for x in data['newforms']]))
         info += row_wrap('Analytic rank', data['rank'])
