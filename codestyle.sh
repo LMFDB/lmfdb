9d5674905ed40aed7e9426fc883902a520db8143
Edgar Costa <edgarc@mit.edu>
2021-08-31 13:28:15 -0400

    update codestyle to be less aggressive

--- codestyle.sh
+++ codestyle.sh
@@ -7,14 +7,22 @@
 # might introduce a lot of changes. That could be hard to merge!
 
 # increased line length. might get shorter in time ... for now even ignored
-ARGS='--max-line-length=120 --ignore=E501'
+#ARGS='--max-line-length=120 --ignore=E501'
+ARGS='--select=E703,E271,E272,E722'
+
+SAGE_COMMAND=$SAGE
+if [[ "$SAGE_COMMAND" == "" ]]; then
+  SAGE_COMMAND=sage
+fi
+echo "Using Sage command $SAGE_COMMAND"
 
 # WARN: we set the aggressive flag
-AUTOPEP="autopep8 -i --aggressive $ARGS"
+AUTOPEP="$SAGE_COMMAND -python -m autopep8 -i --aggressive $ARGS"
+CODESTYLE="$SAGE_COMMAND -python -m pycodestyle $ARGS"
 
 if [ -n "$1" ]; then
   $AUTOPEP "$@"
 else
   cd `dirname "$0"`
-  find lmfdb -iname '*.py' | xargs pep8 $ARGS
+  find lmfdb -iname '*.py' | xargs $CODESTYLE
 fi
