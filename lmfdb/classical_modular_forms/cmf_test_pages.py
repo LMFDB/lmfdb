52ae9e4b99c83ffff13b6ff705de5c1c41af6191
Edgar Costa <edgarc@mit.edu>
2022-02-07 14:56:18 -0500

    typo

--- lmfdb/classical_modular_forms/cmf_test_pages.py
+++ lmfdb/classical_modular_forms/cmf_test_pages.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from lmfdb.tests import LmfdbTest
-from lmfdb.lmdfdb_database import LMFDBDatabase
+from lmfdb.lmfdb_database import LMFDBDatabase
 from sage.parallel.decorate import parallel
 from sage.all import ZZ, sqrt, ceil
 import multiprocessing
