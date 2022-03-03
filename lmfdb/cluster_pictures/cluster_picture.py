98085ea4cc392266f9e362c75173428d42e10e4f
Frédéric Chapoton <chapoton@unistra.fr>
2022-03-03 20:38:04 +0100

    full pycodestyle cleanup for lmfdb/cluster_pictures/

--- lmfdb/cluster_pictures/cluster_picture.py
+++ lmfdb/cluster_pictures/cluster_picture.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-D
+# -*- coding: utf-8 -*-
 
 from flask import Markup
 
@@ -6,15 +6,17 @@ from lmfdb.app import app
 from lmfdb.cluster_pictures.web_cluster_picture import (
     cp_knowl_guts)
 
-#assert cp_logger
+# assert cp_logger
 
 CP_credit = 'Alex Best, Raymond van Bommel'
 Completename = 'Completeness of the data'
 dnc = 'data not computed'
 
+
 def cluster_picture_data(label):
     return Markup(cp_knowl_guts(label))
-    
+
+
 @app.context_processor
 def ctx_cluster_pictures():
     return {'cluster_picture_data': cluster_picture_data}
