40b949b7c95265e2a4c04081eaec304b11adc186
AndrewVSutherland <drew@math.mit.edu>
2022-06-24 07:46:59 -0600

    fix typo

--- lmfdb/elliptic_curves/web_ec.py
+++ lmfdb/elliptic_curves/web_ec.py
@@ -181,15 +181,15 @@ def homogeneous_latex_equation(ainvs):
 
 def short_latex_equation(ainvs):
     a1,a2,a3,a4,a6 = [ZZ(a) for a in ainvs]
-    c4 = -27*a1^4 - 216*a1^2*a2 + 648*a1*a3 - 432*a2^2 + 1296*a4
-    c6 = 54*a1^6 + 648*a1^4*a2 - 1944*a1^3*a3 + 2592*a1^2*a2^2 - 3888*a1^2*a4 - 7776*a1*a2*a3 + 3456*a2^3 - 15552*a2*a4 + 11664*a3^2 + 46656*a6
-    for p in c4.gcd(c6).prime_divisors():
-        while c4.valuation(p) >= 4 and c6.valuation(p) >= 6:
-            c4 = c4.divide_knowing_divisible_by(p**4)
-            c6 = c6.divide_knowing_divisible_by(p**6)
+    A = -27*a1^4 - 216*a1^2*a2 + 648*a1*a3 - 432*a2^2 + 1296*a4
+    B = 54*a1^6 + 648*a1^4*a2 - 1944*a1^3*a3 + 2592*a1^2*a2^2 - 3888*a1^2*a4 - 7776*a1*a2*a3 + 3456*a2^3 - 15552*a2*a4 + 11664*a3^2 + 46656*a6
+    for p in A.gcd(B).prime_divisors():
+        while A.valuation(p) >= 4 and B.valuation(p) >= 6:
+            A = A.divide_knowing_divisible_by(p**4)
+            B = B.divide_knowing_divisible_by(p**6)
     return ''.join([r'\(y^2=x^3',
-                    '{:+}x'.format(c4) if abs(c4)>1 else '+x' if c4==1 else '-x' if c4==-1 else '',
-                    '{:+}'.format(c6) if c6 else '',
+                    '{:+}x'.format(A) if abs(A)>1 else '+x' if A==1 else '-x' if A==-1 else '',
+                    '{:+}'.format(B) if B else '',
                     r'\)'])
 
 def latex_equations(ainvs):
