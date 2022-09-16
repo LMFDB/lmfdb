fff4b4ff32cac718b9d8811cd88d611dab4c9eab
BarinderBanwait <b.s.banwait86@gmail.com>
2022-09-16 14:26:23 -0400

    5093 jump elliptic (#302)
    * initial version only for g2c
    
    * main thing done, now equation stuff TODO
    
    * hopefully working version for ec now

--- lmfdb/elliptic_curves/web_ec.py
+++ lmfdb/elliptic_curves/web_ec.py
@@ -26,6 +26,25 @@ lmfdb_label_regex = re.compile(r'(\d+)\.([a-z]+)(\d*)')
 sw_label_regex = re.compile(r'sw(\d+)(\.)(\d+)(\.*)(\d*)')
 weierstrass_eqn_regex = re.compile(r'\[(-?\d+),(-?\d+),(-?\d+),(-?\d+),(-?\d+)\]')
 short_weierstrass_eqn_regex = re.compile(r'\[(-?\d+),(-?\d+)\]')
+TERM_RE = r"(\+|-)?(\d*[A-Za-z]|\d+\*[A-Za-z]|\d+)(\^\d+)?"
+STERM_RE = r"(\+|-)(\d*[A-Za-z]|\d+\*[A-Za-z]|\d+)(\^\d+)?"
+ONE_TERM = r"(\+|-)?(\d*[A-Za-z]|\d+\*[A-Za-z]|\d+)"
+SHORT_WEIER_BEG = re.compile(r"^" +  ONE_TERM + r"\^2=")
+LONG_WEIER_BEG = re.compile(r"^" +  ONE_TERM + r"\^2+")
+POLY_RE = re.compile(TERM_RE + "(" + STERM_RE + ")*")
+POLYLIST_RE = re.compile(r"(\[|)" + POLY_RE.pattern + r"," + POLY_RE.pattern + r"(\]|)")
+ZLIST_RE = re.compile(r"\[(|((|-)\d+)*(,(|-)\d+)*)\]")
+ZLLIST_RE = re.compile(r"(\[|)" + ZLIST_RE.pattern + r"," + ZLIST_RE.pattern + r"(\]|)")
+G1_LOOKUP_RE = re.compile(r"(" + "|".join([elt.pattern for elt in [POLY_RE, POLYLIST_RE]]) + r")")
+
+def match_weierstrass_polys(lab):
+    return G1_LOOKUP_RE.fullmatch(lab)
+
+def match_short_weierstrass_eqn(lab):
+    return SHORT_WEIER_BEG.match(lab)
+
+def match_long_weierstrass_eqn(lab):
+    return LONG_WEIER_BEG.match(lab)
 
 def match_lmfdb_label(lab):
     return lmfdb_label_regex.fullmatch(lab)
