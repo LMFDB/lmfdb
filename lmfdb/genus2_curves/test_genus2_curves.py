e38e4a6910dea403646db287feb53db25bf74163
Barinder S Banwait <b.s.banwait86@gmail.com>
2022-10-11 17:09:28 -0400

    only run test if magma is present

--- lmfdb/genus2_curves/test_genus2_curves.py
+++ lmfdb/genus2_curves/test_genus2_curves.py
@@ -356,22 +356,28 @@ class Genus2Test(LmfdbTest):
                 'g2c_plots' in data and "data:image/png;base64" in data)
 
     def test_jump(self):
-        # Check that giving defining polynomials for f,h works
-        L = self.tc.get('/Genus2Curve/Q/?jump=x%5E5%2Bx%2B1%2Cx', follow_redirects=True)
-        assert "y^2 + xy = x^5 + x + 1" in L.get_data(as_text=True)
+        try:
+            # Check that giving defining polynomials for f,h works
+            L = self.tc.get('/Genus2Curve/Q/?jump=x%5E5%2Bx%2B1%2Cx', follow_redirects=True)
+            assert "y^2 + xy = x^5 + x + 1" in L.get_data(as_text=True)
 
-        # Check that giving a Weierstrass equation works, even without explicit multiplication '*'
-        L = self.tc.get("/Genus2Curve/Q/?jump=b%5E2%3Da%5E5-2a%5E2%2B1", follow_redirects=True)
-        assert "$y^2 = x^5 - 2x^2 + 1$" in L.get_data(as_text=True)
+            # Check that giving a Weierstrass equation works, even without explicit multiplication '*'
+            L = self.tc.get("/Genus2Curve/Q/?jump=b%5E2%3Da%5E5-2a%5E2%2B1", follow_redirects=True)
+            assert "$y^2 = x^5 - 2x^2 + 1$" in L.get_data(as_text=True)
 
-        # Check that variables are only single characters
-        L = self.tc.get("/Genus2Curve/Q/?jump=(banana)%5E2%3Dx%5E5%2B1", follow_redirects=True)
-        assert "is not in two variables" in L.get_data(as_text=True)
+            # Check that variables are only single characters
+            L = self.tc.get("/Genus2Curve/Q/?jump=(banana)%5E2%3Dx%5E5%2B1", follow_redirects=True)
+            assert "is not in two variables" in L.get_data(as_text=True)
 
-        # Check that curves not of genus 2 fail
-        L = self.tc.get("/Genus2Curve/Q/?jump=y^2+%3D+x^10+-+1", follow_redirects=True)
-        assert "invalid genus 2 curve" in L.get_data(as_text=True)
+            # Check that curves not of genus 2 fail
+            L = self.tc.get("/Genus2Curve/Q/?jump=y^2+%3D+x^10+-+1", follow_redirects=True)
+            assert "invalid genus 2 curve" in L.get_data(as_text=True)
 
-        # Check that there are only two variables present
-        L = self.tc.get("/Genus2Curve/Q/?jump=y%5E2+%3D+x+%2B+a", follow_redirects=True)
-        assert "is not in two variables" in L.get_data(as_text=True)
+            # Check that there are only two variables present
+            L = self.tc.get("/Genus2Curve/Q/?jump=y%5E2+%3D+x+%2B+a", follow_redirects=True)
+            assert "is not in two variables" in L.get_data(as_text=True)
+        except RuntimeError as the_error:
+            if str(the_error).startswith("unable to start magma"):
+                pass
+            else:
+                raise
