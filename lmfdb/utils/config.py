48c624a8f535cda5efe0046eea8e9b9b0a6b22b6
Edgar Costa <edgarc@mit.edu>
2022-11-03 14:36:47 -0400

    moving changing port to config

--- lmfdb/utils/config.py
+++ lmfdb/utils/config.py
@@ -16,11 +16,13 @@ via optional command-line arguments.
 
 
 import argparse
+import getpass
 import os
 import sys
 import random
 import string
 import __main__
+from requests import get
 
 
 root_lmfdb_path = os.path.abspath(
@@ -267,6 +269,29 @@ class Configuration(_Configuration):
         for opt in ["use_debugger", "use_reloader", "profiler"]:
             if opt in extopts:
                 self.flask_options[opt] = extopts[opt]
+
+        if "COCALC_PROJECT_ID" in os.environ:
+            # we must accept external connections
+            self.flask_options["host"] = "0.0.0.0"
+            cocalchost = "cocalc.com"
+            external_ip = get('https://api.ipify.org').content.decode('utf8')
+            if external_ip == "18.18.21.20": # saint-germain
+                cocalchost = "saint-germain.mit.edu"
+                # randomify port, we have only container
+                if self.flask_options["port"] == 37777: # default
+                    username = getpass.getuser()
+                    intusername = int(username, base=36)
+                    self.flask_options["port"] = 10000 + (intusername % 55536)
+            stars = "\n" + "*" * 80
+            self.flask_options["cocalcmessage"] = (stars +
+             "\n\033[1mCocalc\033[0m environment detected!\n"
+             + "Visit"
+             + "\n  \033[1m https://"
+             + cocalchost
+             + '{}'
+             + " \033[0m"
+             + "\nto access this LMFDB instance"
+             + stars)
 
         self.color = opts["core"]["color"]
 
