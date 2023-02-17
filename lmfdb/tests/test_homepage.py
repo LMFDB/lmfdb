0b013244136bd9c3894b16c871971143c67a24e3
Edgar Costa <edgarc@mit.edu>
2023-02-17 16:17:28 -0500

    patch tests

--- lmfdb/tests/test_homepage.py
+++ lmfdb/tests/test_homepage.py
@@ -24,12 +24,12 @@ class HomePageTest(LmfdbTest):
         Check that the links in Box 2 work.
         """
         homepage = self.tc.get("/").get_data(as_text=True)
-        self.check(homepage,"/L/Riemann/", r'14.1347251417346937')
-        self.check(homepage,"/ModularForm/GL2/Q/holomorphic/1/12/a/a/", '4830')
-        self.check(homepage,"/ModularForm/GL2/Q/holomorphic/1/12/a/a/", '113643')
-        self.check(homepage,"/L/ModularForm/GL2/Q/holomorphic/1/12/a/a/", '0.792122')
-        self.check(homepage,"/EllipticCurve/Q/5077/a/1", r'y^2+y=x^3-7x+6')
-        self.check(homepage,"/L/EllipticCurve/Q/5077.a/", '5077')
+        self.check(homepage,"L/Riemann/", r'14.1347251417346937')
+        self.check(homepage,"ModularForm/GL2/Q/holomorphic/1/12/a/a/", '4830')
+        self.check(homepage,"ModularForm/GL2/Q/holomorphic/1/12/a/a/", '113643')
+        self.check(homepage,"L/ModularForm/GL2/Q/holomorphic/1/12/a/a/", '0.792122')
+        self.check(homepage,"EllipticCurve/Q/5077/a/1", r'y^2+y=x^3-7x+6')
+        self.check(homepage,"L/EllipticCurve/Q/5077.a/", '5077')
 
     # Box 3
     def test_box3(self):
@@ -37,9 +37,9 @@ class HomePageTest(LmfdbTest):
         Check that the links in Box 3 work.
         """
         homepage = self.tc.get("/").get_data(as_text=True)
-        self.check(homepage, "/L/", 'Lowest zero')
-        self.check(homepage, "/EllipticCurve/Q/", 'Label or coefficients')
-        self.check(homepage, "/NumberField/", 'x^7 - x^6 - 3 x^5 + x^4 + 4 x^3 - x^2 - x + 1')
+        self.check(homepage, "L/", 'Lowest zero')
+        self.check(homepage, "EllipticCurve/Q/", 'Label or coefficients')
+        self.check(homepage, "NumberField/", 'x^7 - x^6 - 3 x^5 + x^4 + 4 x^3 - x^2 - x + 1')
 
     # Box 4
     def test_box4(self):
@@ -47,8 +47,8 @@ class HomePageTest(LmfdbTest):
         Check that the links in Box 4 work.
         """
         homepage = self.tc.get("/").get_data(as_text=True)
-        self.check(homepage, "/L/degree4/MaassForm/", 'data on L-functions associated to Maass cusp forms for GSp(4) of level 1')
-        self.check(homepage, "/EllipticCurve/Q/102/c/", r'1 &amp; 2 &amp; 4 &amp; 4 &amp; 8 &amp; 8')
+        self.check(homepage, "L/degree4/MaassForm/", 'data on L-functions associated to Maass cusp forms for GSp(4) of level 1')
+        self.check(homepage, "EllipticCurve/Q/102/c/", r'1 &amp; 2 &amp; 4 &amp; 4 &amp; 8 &amp; 8')
 
     # Box 5
     def test_box5(self):
@@ -56,7 +56,7 @@ class HomePageTest(LmfdbTest):
         Check that the links in Box 5 work.
         """
         homepage = self.tc.get("/").get_data(as_text=True)
-        self.check(homepage, "/universe", 'universe')
+        self.check(homepage, "universe", 'universe')
         # removed in PR #1167
         #self.check(homepage, "/knowledge/", 'Recently modified Knowls')
 
