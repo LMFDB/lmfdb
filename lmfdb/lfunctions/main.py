3bc6d28bf7bdb51ce37c9629d2a2689dfeb1fdb6
Edgar Costa <edgarc@mit.edu>
2023-02-17 15:29:03 -0500

    fixing L-fun origins

--- lmfdb/lfunctions/main.py
+++ lmfdb/lfunctions/main.py
@@ -346,7 +346,7 @@ lfunc_columns = SearchColumns([
     MathCol("order_of_vanishing", "lfunction.analytic_rank", "$r$", short_title="order of vanishing", default=True),
     MathCol("z1", "lfunction.zeros", "First zero", short_title="first zero", default=True),
     ProcessedCol("origins", "lfunction.underlying_object", "Origin",
-                 lambda origins: " ".join('<a href="%s">%s</a>' % (url, name) for name, url in origins),
+                 lambda origins: " ".join(f'<a href="{url_for("index")}{url.lstrip("/")}">{name}</a>' for name, url in origins),
                  default=True)],
     db_cols=['algebraic', 'analytic_conductor', 'bad_primes', 'central_character', 'conductor', 'degree', 'instance_urls', 'label', 'motivic_weight', 'mu_real', 'mu_imag', 'nu_real_doubled', 'nu_imag', 'order_of_vanishing', 'primitive', 'rational', 'root_analytic_conductor', 'root_angle', 'self_dual', 'z1'])
 lfunc_columns.dummy_download = True
