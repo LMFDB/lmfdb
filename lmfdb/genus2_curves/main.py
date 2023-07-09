6cd715b76ba193851abe7c3c52147d41195fe93b
Andrew Sutherland <AndrewVSutherland@users.noreply.github.com>
2023-07-09 06:48:40 -0400

    Update main.py

--- lmfdb/genus2_curves/main.py
+++ lmfdb/genus2_curves/main.py
@@ -1167,7 +1167,6 @@ class G2CSearchArray(SearchArray):
         ]
 
     _default = ["cond", "class", "abs_disc", "disc_sign", "label"]
-    sort_knowl = "g2c.sort_order"
     sorts = [("", "conductor", _default),
              ("disc", "absolute discriminant", ["abs_disc"] + _default),
              ("num_rat_pts", "rational points", ["num_rat_pts"] + _default),
