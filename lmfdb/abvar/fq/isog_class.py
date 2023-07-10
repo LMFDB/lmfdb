0e4f8e1bfe053ec4960905abb7d2144c06523918
Edgar Costa <edgarc@mit.edu>
2023-07-10 14:14:39 -0400

    reuse polarization knowl

--- lmfdb/abvar/fq/isog_class.py
+++ lmfdb/abvar/fq/isog_class.py
@@ -668,7 +668,7 @@ class AbvarFq_isoclass():
         ('aut_group', 'ag.aut_group', 'Automorphism Group'),
         #('geom_aut_group', 'av.geom_aut_group', 'Geometric automorphism group'),
         ('endomorphism_ring', 'ag.endomorphism_ring', 'Endomorphism ring'),
-        ('kernel', 'av.polarization_kernel', 'Kernel'),
+        ('kernel', 'av.polarization', 'Kernel'),
     ]
 
     @lazy_attribute
