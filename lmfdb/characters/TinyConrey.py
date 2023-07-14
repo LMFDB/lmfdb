ed82d865aabbb11f4aafa82b6b6312287f01940d
Pascal <molin.maths@gmail.com>
2023-07-14 20:39:55 +0000

    use same limit=30 as in template multiplicative_order() -> order use pari interface switch Pari to pari

--- lmfdb/characters/TinyConrey.py
+++ lmfdb/characters/TinyConrey.py
@@ -1,4 +1,4 @@
-from sage.all import (gcd, Mod, Integer, Integers, Rational, pari, Pari,
+from sage.all import (gcd, Mod, Integer, Integers, Rational, pari,
                       DirichletGroup, CyclotomicField, euler_phi, lcm)
 from sage.misc.cachefunc import cached_method
 from sage.modular.dirichlet import DirichletCharacter
@@ -65,27 +65,50 @@ class PariConreyGroup():
 
     def __init__(self, modulus):
         self.modulus = int(modulus)
-        self.G = Pari("znstar({},1)".format(modulus))
+        self.G = pari(f"znstar({modulus},1)")
 
     def gens(self):
         return Integers(self.modulus).unit_gens()
 
     def invariants(self):
-        return pari("znstar({},1).cyc".format(self.modulus))
+        return pari(f"{self.G}.cyc")
+
+    @cached_method
+    def first_chars(self, limit=31):
+        if self.modulus == 1:
+            return [1]
+        r = []
+        for i,c in enumerate(Integers(self.modulus).list_of_elements_of_multiplicative_group()):
+            r.append(c)
+            if i > limit:
+                self.rowtruncate = True
+                break
+        return r
+
+    @cached_method
+    def first_chars_with_orbit(self, limit=31):
+        """ would be nice to compute those directly
+            instead of querying each to db
+        """
+        pass
+
+
 
 
 class ConreyCharacter():
     """
-    tiny implementation on Conrey index only
+    minimal implementation of character from its Conrey index
+    use Pari/GP functions when available
     """
 
     def __init__(self, modulus, number):
         assert gcd(modulus, number)==1
         self.modulus = Integer(modulus)
         self.number = Integer(number)
-        self.G = Pari("znstar({},1)".format(modulus))
-        self.G_gens = Integers(self.modulus).unit_gens()
-        self.chi_pari = pari("znconreylog(%s,%d)"%(self.G,self.number))
+        self.conrey = Mod(number,modulus)
+        self.G = pari("znstar({},1)".format(modulus))
+        self.G_gens = Integers(self.modulus).unit_gens() # use sage generators
+        self.chi_pari = self.G.znconreylog(self.number)
         self.chi_0 = None
         self.indlabel = None
 
@@ -100,7 +123,7 @@ class ConreyCharacter():
 
     @cached_method
     def conductor(self):
-        B = pari("znconreyconductor(%s,%s,&chi0)"%(self.G, self.chi_pari))
+        B = pari(f"znconreyconductor({self.G},{self.chi_pari},&chi0)")
         if B.type() == 't_INT':
             # means chi is primitive
             self.chi_0 = self.chi_pari
@@ -108,8 +131,8 @@ class ConreyCharacter():
             return int(B)
         else:
             self.chi_0 = pari("chi0")
-            G_0 = Pari("znstar({},1)".format(B))
-            self.indlabel = int(pari("znconreyexp(%s,%s)"%(G_0,self.chi_0)))
+            G_0 = pari(f"znstar({B},1)")
+            self.indlabel = int(G_0.znconreyexp(self.chi_0))
             return int(B[0])
 
     @cached_method
@@ -118,17 +141,7 @@ class ConreyCharacter():
 
     @cached_method
     def parity(self):
-        number = self.number
-        par = 0
-        for p,e in self.modfactor():
-            if p == 2:
-                if number % 4 == 3:
-                    par = 1 - par
-            else:
-                phi2 = (p-1)/Integer(2) * p **(e-1)
-                if Mod(number, p ** e)**phi2 != 1:
-                    par = 1 - par
-        return par
+        return self.G.zncharisodd(self.chi_pari)
 
     def is_odd(self):
         return self.parity() == 1
@@ -136,13 +149,9 @@ class ConreyCharacter():
     def is_even(self):
         return self.parity() == 0
 
-    @cached_method
-    def multiplicative_order(self):
-        return Mod(self.number, self.modulus).multiplicative_order()
-
     @property
     def order(self):
-        return self.multiplicative_order()
+        return self.conrey.multiplicative_order()
 
     @property
     def genvalues(self):
@@ -164,7 +173,7 @@ class ConreyCharacter():
         return kronecker_symbol(symbol_numerator(c, p))
 
     def conreyangle(self,x):
-        return Rational(pari("chareval(%s,znconreylog(%s,%d),%d)"%(self.G,self.G,self.number,x)))
+        return Rational(self.G.chareval(self.chi_pari,x))
 
     def gauss_sum_numerical(self, a):
         # There seems to be a bug in pari when a is a multiple of the modulus,
@@ -175,7 +184,7 @@ class ConreyCharacter():
             else:
                 return Integer(0)
         else:
-            return pari("znchargauss(%s,%s,a=%d)"%(self.G,self.chi_pari,a))
+            return self.G.znchargauss(self.chi_pari,a)
 
     def sage_zeta_order(self, order):
         return 1 if self.modulus <= 2 else lcm(2,order)
@@ -194,49 +203,53 @@ class ConreyCharacter():
         return DirichletCharacter(H,M(order_corrected_genvalues))
 
     @cached_method
-    def galois_orbit(self, limit=200):
+    def galois_orbit(self, limit=31):
+        """
+        orbit under Galois of the value field,
+        can be used to find first conjugate or list of first conjugates
+        """
+        logger.debug(f"## galois_orbit({limit})")
         order = self.order
         if order == 1:
             return [1]
         elif order < limit or order * order < limit * self.modulus:
             logger.debug(f"compute all conjugate characters and return first {limit}")
             return self.galois_orbit_all(limit)
-        elif self.modulus < 30 * order:
+        elif limit == 1 or self.modulus < 30 * order:
             logger.debug(f"compute {limit} first conjugate characters")
             return self.galois_orbit_search(limit)
         else:
             logger.debug(f"galois orbit of size {order} too expansive, give up")
             return []
 
-    def galois_orbit_all(self, limit=200):
+    def galois_orbit_all(self, limit=31):
         # construct all Galois orbit, assume not too large
         order = self.order
-        chi = chik = Mod(self.number, self.modulus)
+        chik = self.conrey
         output = []
-        for k in range(2,order):
+        for k in range(1,order):
             if gcd(k,order) == 1:
                 output.append(Integer(chik))
-                chik *= chi
+            chik *= self.conrey
         output.sort()
-        return output[:100]
+        return output[:limit]
 
-    def galois_orbit_search(self, limit=200):
+    def galois_orbit_search(self, limit=31):
         # fishing strategy, assume orbit relatively dense
         order = self.order
         num = self.number
         mod = self.modulus
-        kmax = limit * 50
+        kmax = min(mod,limit * 50)
         cmd = f"a=Mod({num},{mod});my(valid(k)=my(l=znlog(k,a,{order}));l&&gcd(l,{order})==1);[ k | k <- [1..{kmax}], gcd(k,{mod})==1 && valid(k) ]"
         return list(map(Integer,pari(cmd)[:limit]))
-
-    @cached_method
-    def kernel_field_poly(self):
-        k = pari("charker(%s,%s)"%(self.G, self.chi_pari))
-        pol = pari("galoissubcyclo(%s,%s)"%(self.G, k))
-        if self.order <= 12:
-            pol = pari("polredabs(%s)"%(pol))
-        return pol
 
     @property
     def min_conrey_conj(self):
         return self.galois_orbit(1)[0]
+
+    @cached_method
+    def kernel_field_poly(self):
+        pol = self.G.galoissubcyclo(self.G.charker(self.chi_pari))
+        if self.order <= 12:
+            pol = pol.polredabs()
+        return pol
