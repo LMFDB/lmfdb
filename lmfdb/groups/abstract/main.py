071ff04419666b9dba0594cf36d833c41905a119
Jen Paulhus <jenpaulhus@gmail.com>
2023-11-30 17:44:06 -0500

    part way through

--- lmfdb/groups/abstract/main.py
+++ lmfdb/groups/abstract/main.py
@@ -1120,6 +1120,96 @@ def subgroup_search(info, query={}):
     parse_regex_restricted(info, query, "ambient", regex=abstract_group_label_regex)
     parse_regex_restricted(info, query, "quotient", regex=abstract_group_label_regex)
 
+
+#JP HERE
+
+#FIX THIS
+complex_char_columns = SearchColumns([
+
+    LinkCol("passport_label", "dq.curve.highergenus.aut.label", "Refined passport label",
+	    lambda label: f"/HigherGenus/C/Aut/{label}"),
+    MathCol("genus", "ag.curve.genus", "Genus"),
+    MathCol("g0", "curve.highergenus.aut.quotientgenus", "Quotient genus", default=False),
+    ProcessedCol("group", "group.small_group_label", "Group", group_display, mathmode=True, align="center"),
+    MathCol("group_order", "group.order", "Group order"),
+    MathCol("dim", "curve.highergenus.aut.dimension", "Dimension"),
+    ProcessedCol("signature", "curve.highergenus.aut.signature", "Signature", lambda sig: sign_display(ast.literal_eval(sig)), mathmode=True),
+    CheckCol("hyperelliptic", "ag.hyperelliptic_curve", "Hyperelliptic", unknown="", default=False),
+    CheckCol("cyclic_trigonal", "ag.cyclic_trigonal", "Cyclic trigonal", unknown="", default=False),
+    ProcessedCol("gen_vectors", "curve.highergenus.aut.generatingvector", "Generating vectors", display_gen_vecs, mathmode=True, default=False)])
+hgcwa_columns.languages = ['gap', 'magma']
+
+
+    LinkCol("label", "group.subgroup_label", "Label", get_sub_url, th_class=" border-right", td_class=" border-right"),
+    ColGroup("subgroup_cols", None, "Subgroup", [
+	MultiProcessedCol("sub_name", "group.name", "Name",
+                          ["subgroup", "subgroup_tex"],
+                          display_url,
+                          short_title="Sub. name", apply_download=False),
+	ProcessedCol("subgroup_order", "group.order", "Order", show_factor, align="center", short_title="Sub. order"),
+	CheckCol("normal", "group.subgroup.normal", "norm", short_title="Sub. normal"),
+        CheckCol("characteristic", "group.characteristic_subgroup", "char", short_title="Sub. characteristic"),
+        CheckCol("cyclic", "group.cyclic", "cyc", short_title="Sub. cyclic"),
+        CheckCol("abelian", "group.abelian", "ab", short_title="Sub. abelian"),
+	CheckCol("solvable", "group.solvable", "solv", short_title="Sub. solvable"),
+	CheckCol("maximal", "group.maximal_subgroup", "max", short_title="Sub. maximal"),
+        CheckCol("perfect", "group.perfect", "perf", short_title="Sub. perfect"),
+        CheckCol("central", "group.central", "cent", short_title="Sub. central")]),
+    SpacerCol("", th_class=" border-right", td_class=" border-right", td_style="padding:0px;", th_style="padding:0px;"), # Can't put the right border on "subgroup_cols" (since it wouldn't be full height) or "central" (since it might be hidden by the user)                                                                                              
+    ColGroup("ambient_cols", None, "Ambient", [
+        MultiProcessedCol("ambient_name", "group.name", "Name",
+                          ["ambient", "ambient_tex"],
+                          display_url,
+                          short_title="Ambient name", apply_download=False),
+        ProcessedCol("ambient_order", "group.order", "Order", show_factor, align="center", short_title="Ambient order")]),
+    SpacerCol("", th_class=" border-right", td_class=" border-right", td_style="padding:0px;", th_style="padding:0px;"),
+    ColGroup("quotient_cols", None, "Quotient", [
+        MultiProcessedCol("quotient_name", "group.name", "Name",
+                          ["quotient", "quotient_tex"],
+                          display_url,
+                          short_title="Quo. name", apply_download=False),
+        ProcessedCol("quotient_order", "group.order", "Order", lambda n: show_factor(n) if n else "", align="center", short_title="Quo. order"),
+        CheckCol("quotient_cyclic", "group.cyclic", "cyc", short_title="Quo. cyclic"),
+        CheckCol("quotient_abelian", "group.abelian", "ab", short_title="Quo. abelian"),
+        CheckCol("quotient_solvable", "group.solvable", "solv", short_title="Quo. solvable"),
+        CheckCol("minimal_normal", "group.maximal_quotient", "max", short_title="Quo. maximal")])],
+    tr_class=["bottom-align", ""])
+
+class Complex_char_download(Downloader):
+    table = db.gps_char
+
+@search_wrap(
+    table=db.gps_char,
+    title="Complex Character search results",
+    err_title="Complex Character search input error",
+    columns=complex_char_columns,
+    shortcuts={"download": Complex_char_download()},
+    bread=lambda: get_bread([("Search Results", "")]),
+    learnmore=learnmore_list,
+    url_for_label=url_for_subgroup_label,
+)
+
+def complex_char_search(info, query={}):
+    info["search_type"] = "Complex Characters"
+    parse_ints(info, query, "dim")
+    parse_ints(info, query, "indicator")
+    parse_ints(info, query, "cyclotomic_n")
+    parse_bool(info, query, "faithful")
+    parse_regex_restricted(info, query, "group", regex=abstract_group_label_regex)
+    parse_regex_restricted(info, query, "center", regex=abstract_group_label_regex)
+    parse_regex_restricted(info, query, "image", regex=abstract_group_label_regex)
+    parse_regex_restricted(info, query, "image", regex=abstract_group_label_regex)
+    parse_regex_restricted(info, query, "kernel", regex=abstract_group_label_regex)
+    #maybe regex to get q-character need to parse rational character label
+    #maybe parse_list for nt and field.  ADD LATER
+    # parse_bool(
+    #    info, query, "sylow", process=lambda x: ({"$gt": 1} if x else {"$lte": 1})
+    # )
+    # parse_bool(
+    #     info, query, "hall", process=lambda x: ({"$gt": 1} if x else {"$lte": 1})
+    #  )
+    #   parse_bool(info, query, "nontrivproper", qfield="proper")
+ 
 def factor_latex(n):
     return "$%s$" % web_latex(factor(n), False)
 
