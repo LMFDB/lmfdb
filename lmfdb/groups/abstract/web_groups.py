ee589bd0a4b8b7f7e8645cb3f5a79aa35ca55c59
John Jones <jj@asu.edu>
2023-12-05 14:36:50 -0500

    Pyflakes

--- lmfdb/groups/abstract/web_groups.py
+++ lmfdb/groups/abstract/web_groups.py
@@ -750,8 +750,8 @@ class WebAbstractGroup(WebObj):
         return ans
 
     def properties(self):
-        nilp_str = f"yes" if self.nilpotent else "no"
-        solv_str = f"yes" if self.solvable else "no"
+        nilp_str = "yes" if self.nilpotent else "no"
+        solv_str = "yes" if self.solvable else "no"
         props = [
             ("Label", self.label),
             ("Order", web_latex(factor(self.order))),
