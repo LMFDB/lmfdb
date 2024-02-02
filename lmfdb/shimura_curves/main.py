57eb14f73d2cf5187c2a941104fd9ff3452939d8
assaferan <assaferan@gmail.com>
2024-02-02 08:56:57 -0500

    changing the headline to abelian surfaces with PQM

--- lmfdb/shimura_curves/main.py
+++ lmfdb/shimura_curves/main.py
@@ -1107,7 +1107,7 @@ class ShimCurve_stats(StatsDisplay):
     def short_summary(self):
         shimcurve_knowl = display_knowl("shimcurve", title="Shimura curves")
         return (
-            fr'The database currently contains {self.ncurves} {shimcurve_knowl} of level $N\le {self.max_level}$ parameterizing elliptic curves $E$ over $\Q$.  You can <a href="{url_for(".statistics")}">browse further statistics</a>.'
+            fr'The database currently contains {self.ncurves} {shimcurve_knowl} of level $N\le {self.max_level}$ parameterizing abelian surfacs $A$ over $\Q$ with potential quaternionic multiplication.  You can <a href="{url_for(".statistics")}">browse further statistics</a>.'
         )
 
     @property
