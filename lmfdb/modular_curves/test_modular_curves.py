1ddb7740b091f7c347413ca10e78da13f481d4e8
Jun Bo Lau <laujunbo@gmail.com>
2024-03-11 14:07:19 -0400

    add new test on searching for level

--- lmfdb/modular_curves/test_modular_curves.py
+++ lmfdb/modular_curves/test_modular_curves.py
@@ -10,3 +10,7 @@ class ModCrvTest(LmfdbTest):
         assert 'Search' in L.get_data(as_text=True)
         assert 'Find' in L.get_data(as_text=True)
         assert 'X_0(N)' in L.get_data(as_text=True)
+
+    def test_level_range(self):
+        L = self.tc.get("/ModularCurve/Q/?level=10-100")
+        assert "10.2.0.a.1" in L.get_data(as_text=True)
