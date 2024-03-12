1dfa1d3680718195fc03bb310043386d95faa148
Barinder S Banwait <b.s.banwait86@gmail.com>
2024-03-12 14:34:13 -0400

    add verif test for label

--- lmfdb/verify/gps/gps_gl2zhat_fine.py
+++ lmfdb/verify/gps/gps_gl2zhat_fine.py
@@ -7,11 +7,17 @@ class gps_gl2zhat_fine(TableChecker):
     uniqueness_constraints = [["label"]]
 
     # We can't use the default check_label, since our labels come in two flavors
-    #@overall
-    #def check_label(self):
-    #    bad_labels = self.check_string_concatenation("label", ["level", "index", "genus", "coarse_class", "coarse_num"], {"contains_negative_one":True})
-    #    bad_labels += self.check_string_concatenation("label", ["coarse_level", "coarse_index", "genus", "coarse_class", "coarse_num", "level", "fine_num"], {"contains_negative_one":False}, sep=list("....-."))
-    #    return bad_labels
+    @overall
+    def check_label(self):
+        """
+        check that label is correct
+        """
+        bad_labels = self.check_string_concatenation("label", ["coarse_class", "coarse_num"], {"contains_negative_one":True})
+        bad_labels += self.check_string_concatenation("label", ["level", "index", "genus", "coarse_level", "coarse_class_num", "coarse_num", "fine_num"],
+                                                      constraint={"contains_negative_one":False},
+                                                      sep=list("..-..."),
+                                                      convert_to_base26={"coarse_class_num": -1})
+        return bad_labels
 
     @overall
     def check_genus_equals_total_newform_dim(self):
