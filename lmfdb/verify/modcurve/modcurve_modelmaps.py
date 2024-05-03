3aa68d565df8fd7c21e1dac893877e7a3e3db46b
roed314 <roed.math@gmail.com>
2024-05-03 14:57:33 -0400

    Removing some database calls

--- lmfdb/verify/modcurve/modcurve_modelmaps.py
+++ lmfdb/verify/modcurve/modcurve_modelmaps.py
@@ -1,5 +1,6 @@
 
-from sage.all import ProjectiveSpace, PolynomialRing, QQ
+from sage.all import ProjectiveSpace, PolynomialRing, QQ, lazy_attribute
+from collections import defaultdict
 
 from lmfdb.lmfdb_database import db
 from lmfdb.modular_curves.upload import VARORDER
@@ -23,6 +24,20 @@ class modcurve_modelmaps(TableChecker):
     table = db.modcurve_modelmaps
     label_col = 'domain_label'
 
+    @lazy_attribute
+    def modcurve_points(self):
+        ans = defaultdict(list)
+        for rec in db.modcurve_points.search({}, ["curve_label", "coordinates", "jinv"]):
+            ans[rec["curve_label"]].append(rec)
+        return ans
+
+    @lazy_attribute
+    def modcurve_models(self):
+        ans = defaultdict(list)
+        for rec in db.modcurve_models.search({}, ["modcurve", "model_type", "equation", "number_variables"]):
+            ans[rec["modcurve"], rec["model_type"]] = (rec["equation"], rec["number_variables"])
+        return ans
+
     @slow(projection=['domain_label', 'codomain_label', 'coordinates', 'domain_model_type', 'codomain_model_type'])
     def check_rat_pts_map_to_rat_pts(self, rec):
         """
@@ -31,9 +46,9 @@ class modcurve_modelmaps(TableChecker):
         """
 
         # First look up ratpts on the domain
-
-        for point in db.modcurve_points.search({'curve_label':rec['domain_label'],
-                                                'coordinates':{'$exists':True}}):
+        for point in self.modcurve_points[rec["domain_label"]]:
+            if not point.get("coordinates"):
+                continue
             try:
                 relevant_pts = point['coordinates'][rec['domain_model_type']]  # these are the pts that can be hit with the map
             except KeyError:
@@ -49,27 +64,20 @@ class modcurve_modelmaps(TableChecker):
                     return False
 
                 # TEST 2: check this is actually a point on the codomain
-                codomain_info = db.modcurve_models.lookup({'modcurve':rec['codomain_label'],
-                                                       'model_type':rec['codomain_model_type']},
-                                                       projection=['equation', 'number_variables'])
-                codomain_info = list(codomain_info)
-                assert len(codomain_info) == 1  # multiple entries is bad
-                codomain_info = codomain_info[0]
-                Pol = PolynomialRing(QQ, codomain_info['number_variables'], names=VARORDER[:codomain_info['number_variables']])
-                for f_str in codomain_info['equation']:
+                equation, number_variables = self.modcurve_models[rec["codomain_label"], rec["codomain_model_type"]]
+                Pol = PolynomialRing(QQ, number_variables, names=VARORDER[:number_variables])
+                for f_str in equation:
                     assert Pol(f_str)(pt_on_codomain_as_list) == 0, "point not on codomain"
                 
                 ### TEST 3: check this pt is accounted for in modcurve_points
 
                 # 3a. first get the points on the codomain
-                points_on_codomain_with_this_j = db.modcurve_points.lookup({'curve_label':rec['codomain_label'],
-                                                'jinv':jinv_this_pt})
-                points_on_codomain_with_this_j = list(points_on_codomain_with_this_j)
+                points_on_codomain_with_this_j = [rec for rec in self.modcurve_points[rec["codomain_label"]] if rec["jinv"] == jinv_this_pt]
                 assert len(points_on_codomain_with_this_j) == 1
 
                 # 3b. now check the point is among them
-                pts_on_codomain_as_str = points_on_codomain_with_this_j['coordinates'][rec['codomain_model_type']]  # a list of strings
-                P = ProjectiveSpace(QQ, codomain_info['number_variables'] - 1, names=VARORDER[:codomain_info['number_variables']])
+                pts_on_codomain_as_str = points_on_codomain_with_this_j[0]['coordinates'][rec['codomain_model_type']]  # a list of strings
+                P = ProjectiveSpace(QQ, number_variables - 1, names=VARORDER[:number_variables])
                 assert P(pt_on_codomain_as_list) in [P(pt_str.split(':')) for pt_str in pts_on_codomain_as_str], "missing point"
         
         return True
