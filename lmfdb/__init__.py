81bb92cb190b408ab65ae0cac47ad568558aae8a
David Roe <roed.math@gmail.com>
2024-05-10 13:37:42 -0400

    Make linter happy

--- lmfdb/__init__.py
+++ lmfdb/__init__.py
@@ -3,6 +3,7 @@
 # psycodict was added as a dependency on May 10, 2024; we provide a useful error message for people upgrading
 try:
     import psycodict
+    assert psycodict
 except ImportError:
     print('Missing dependency; try running "sage -pip install -r requirements.txt" in the LMFDB home folder.')
     raise
