34342c253d67d448001a8c53594aca72c65470ec
AndrewVSutherland <drew@math.mit.edu>
2024-05-27 16:15:49 -0400

    delint

--- lmfdb/classical_modular_forms/main.py
+++ lmfdb/classical_modular_forms/main.py
@@ -14,9 +14,9 @@ from lmfdb.utils import (
     parse_noop, parse_equality_constraints, integer_options, parse_subset,
     search_wrap, display_float, factor_base_factorization_latex,
     flash_error, to_dict, comma, display_knowl, bigint_knowl, num2letters,
-    SearchArray, TextBox, SelectBox, TextBoxWithSelect, YesNoBox,
+    SearchArray, TextBox, TextBoxNoEg, SelectBox, TextBoxWithSelect, YesNoBox,
     DoubleSelectBox, RowSpacer, HiddenBox, SearchButtonWithSelect,
-    SubsetBox, ParityMod, CountBox, SelectBoxNoEg,
+    SubsetBox, ParityMod, CountBox,
     StatsDisplay, proportioners, totaler, integer_divisors,
     redirect_no_cache)
 from psycodict.utils import range_formatter
