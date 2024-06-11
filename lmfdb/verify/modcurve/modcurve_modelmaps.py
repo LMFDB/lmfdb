26817603ac62967d3f708abdbcbed7b423f61ba8
Barinder S Banwait <b.s.banwait86@gmail.com>
2024-06-11 12:30:40 -0400

    update test

--- lmfdb/verify/modcurve/modcurve_modelmaps.py
+++ lmfdb/verify/modcurve/modcurve_modelmaps.py
@@ -45,18 +45,15 @@ class modcurve_modelmaps(TableChecker):
         given a rational point on a modular curve, and a map from this modular curve
         to another one, check that the image is a rational point on the latter curve
         """
-
         # First look up ratpts on the domain
         for point in self.modcurve_points[rec["domain_label"]]:
             if not point.get("coordinates"):
                 continue
             try:
                 relevant_pts = point['coordinates'][str(rec['domain_model_type'])]  # these are the pts that can be hit with the map
-                print(f"Found point on {rec['domain_label']}")
             except KeyError:
                 # means there are no points on the model required by the map
                 continue
-            jinv_this_pt = point['jinv']  # needed for the third test below
             for rel_pt in relevant_pts:
                 pt_on_codomain_as_list = apply_map_to_pt(rec['coordinates'], rel_pt)
 
@@ -67,22 +64,9 @@ class modcurve_modelmaps(TableChecker):
                 # TEST 2: check this is actually a point on the codomain
                 equation, number_variables = self.modcurve_models[rec["codomain_label"], rec["codomain_model_type"]]
                 Pol = PolynomialRing(QQ, number_variables, names=VARORDER[:number_variables])
-                for f_str in equation:
-                    assert Pol(f_str)(pt_on_codomain_as_list) == 0, "point not on codomain"
 
-                ### TEST 3: check this pt is accounted for in modcurve_points
+                if not all([Pol(f_str)(pt_on_codomain_as_list) == 0 for f_str in equation]):
+                    return False
 
-                # 3a. first get the points on the codomain
-                points_on_codomain_with_this_j = [rec for rec in self.modcurve_points[rec["codomain_label"]] if rec["jinv"] == jinv_this_pt]
-                assert len(points_on_codomain_with_this_j) == 1
-
-                # 3b. now check the point is among them
-                if str(rec['codomain_model_type']) in points_on_codomain_with_this_j[0]['coordinates']:
-                    pts_on_codomain_as_str = points_on_codomain_with_this_j[0]['coordinates'][str(rec['codomain_model_type'])]
-                else:
-                    # Means we don't have coordinates on the model we're dealing with, so can't do anything
-                    continue
-                P = ProjectiveSpace(QQ, number_variables - 1, names=VARORDER[:number_variables])
-                assert P(pt_on_codomain_as_list) in [P(pt_str.split(':')) for pt_str in pts_on_codomain_as_str], "missing point"
         return True
 
