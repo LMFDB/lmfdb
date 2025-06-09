7e17583593e1be87073d6aaf6d09ba5457705d26
roed314 <roed.math@gmail.com>
2025-06-09 12:24:45 -0400

    Update test_abstract_groups.py
    Fixing some comment spacing

--- lmfdb/groups/abstract/test_abstract_groups.py
+++ lmfdb/groups/abstract/test_abstract_groups.py
@@ -10,7 +10,7 @@ class AbGpsTest(LmfdbTest):
         self.check_args("/Groups/Abstract/60.5", "nonsolvable")
         self.check_args("/Groups/Abstract/32.51", "solvable")
 
-# To do:  Test a lot more data,  also more property box tests
+    # To do:  Test a lot more data,  also more property box tests
 
     def test_property_box(self):
         r"""
@@ -18,12 +18,13 @@ class AbGpsTest(LmfdbTest):
         """
         page = self.tc.get("/Groups/Abstract/256.14916").get_data(as_text=True).replace("\n", "").replace(" ", "")
         assert r'<divclass="properties-body"><table><tr><tdclass="label">Label</td><td>256.14916</td></tr><tr>' in page
- #       assert r'<tdclass="label">Order</td><td>${2^{8}}$</td></tr>' in page
-       # self.check_args("/Variety/Abelian/Fq/2/79/ar_go", "Principally polarizable")
+        # assert r'<tdclass="label">Order</td><td>${2^{8}}$</td></tr>' in page
+        # self.check_args("/Variety/Abelian/Fq/2/79/ar_go", "Principally polarizable")
 
     def test_abstract_group_download(self):
-        #        r"""
-        #        Test downloading on search results page.
+        r"""
+        Test downloading on search results page.
+        """
         response = self.tc.get("/Groups/Abstract/384.5458/download/gap")
         self.assertTrue("If the group is solvable" in response.get_data(as_text=True))
         self.assertTrue("encd:= 293961739841108398509157889" in response.get_data(as_text=True))
