0cef7bd0ed7165ba188c6cdde5ff7e15c96fa010
Edgar Costa <edgarc@mit.edu>
2025-06-20 17:16:51 -0400

    remove broken link from tests

--- lmfdb/tests/test_workshoplinks.py
+++ lmfdb/tests/test_workshoplinks.py
@@ -5,11 +5,11 @@ class HomePageTest(LmfdbTest):
     # Test external Links on workshops page
     def test_workshoplinks(self):
         homepage = self.tc.get("/acknowledgment/activities").get_data(as_text=True)
-        self.check_external(
-            homepage,
-            "http://people.oregonstate.edu/~swisherh/CRTNTconference/index.html",
-            "Galois",
-        )
+        # self.check_external(
+        #     homepage,
+        #     "http://people.oregonstate.edu/~swisherh/CRTNTconference/index.html",
+        #     "Galois",
+        # )
         self.check_external(
             homepage,
             "http://www2.warwick.ac.uk/fac/sci/maths/research/events/2013-2014/nonsymp/lmfdb/",
