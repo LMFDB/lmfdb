1035b827b4d512336b57c2f8ca0ca3a5b7f94e2d
Steven Clontz <steven.clontz@gmail.com>
2025-07-08 11:32:28 -0400

    Fix setup script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,6 @@
+sudo apt update
+sudo apt install -y make git
 sage -i gap_packages
-# sage -i database_gap # only needed if sage version < 8.6
 sage -i pip
 sage -b
 sage -pip install -r requirements.txt
-sage -c "easy_install -U python-memcached"
