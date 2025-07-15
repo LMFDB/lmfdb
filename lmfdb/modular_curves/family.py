48972e277cfd42d7a493fc73cedf9df01c356bb9
sachihashimoto <svh@bu.edu>
2025-07-15 11:45:45 -0400

    made dictionary of names, starting refactoring methods into main class

--- lmfdb/modular_curves/family.py
+++ lmfdb/modular_curves/family.py
@@ -4,31 +4,24 @@ from lmfdb.modular_curves.web_curve import get_bread
 from lmfdb.utils import display_knowl
 
 def ModCurveFamily(name):
-    if name == "X0":
-        return X0N()
-    elif name == "X1":
-        return X1N()
-    elif name == "X":
-        return XN()
-    elif name == "Xpm1":
-        return Xpm1N()
-    elif name == "Xarith1":
-        return Xarith1MMN()
-    elif name == "Xarithpm1":
-        return Xarithpm1MMN()
-    elif name == "Xsp":
-        return XspN()
-    elif name == "Xspplus":
-        return XspplusN()
-    elif name == "Xns":
-        return XnsN()
-    elif name == "Xnsplus":
-        return XnsplusN()
-    elif name == "XS4":
-        return XS4p()
-    elif name == "Xarith":
-        return XarithN()
-    raise ValueError("Invalid name")
+    FAMILY_CLASSES = {
+        "X0": X0N,
+        "X1": X1N,
+        "X": XN,
+        "Xpm1": Xpm1N,
+        "Xarith1": Xarith1MMN,
+        "Xarithpm1": Xarithpm1MMN,
+        "Xsp": XspN,
+        "Xspplus": XspplusN,
+        "Xns": XnsN,
+        "Xnsplus": XnsplusN,
+        "XS4": XS4p,
+        "Xarith": XarithN,
+    }
+    try:
+        return FAMILY_CLASSES[name]()
+    except KeyError:
+        raise ValueError(f"Invalid name '{name}'. Valid names: {list(FAMILY_CLASSES.keys())}")
 
 class ModCurveFamily_base():
     @lazy_attribute
@@ -38,6 +31,10 @@ class ModCurveFamily_base():
     @lazy_attribute
     def title(self):
         return f"Modular curve family ${self.name}$"
+    
+    @lazy_attribute
+    def cusps_display(self):
+        return self.cusps
 
 
 class X0N(ModCurveFamily_base):
@@ -59,13 +56,6 @@ class X0N(ModCurveFamily_base):
     hypell_description = r'[<a href="https://mathscinet.ams.org/mathscinet/article?mr=364259">MR:364259</a>] showed that there are only 19 hyperellipic curves in this family and [<a href="https://mathscinet.ams.org/mathscinet/article?mr=1150566">MR:1150566</a>] provided equations for these curves. See the [<a href="https://beta.lmfdb.org/ModularCurve/Q/?level=22%2C+23%2C+26%2C+28%2C+29%2C+30%2C+31%2C+33%2C+35%2C+37%2C+39%2C+40%2C+41%2C+46%2C+47%2C+48%2C+50%2C+59%2C+71&family=X0">following table</a>] for the full list.'
     biell_description = r'[<a href="https://mathscinet.ams.org/mathscinet/article?mr=1688168">MR:1688168</a>] showed that there are only 41 biellipic curves in this family and [<a href="https://mathscinet.ams.org/mathscinet/article?mr=3086199">MR:3086199</a>] provided equations for these curves. Among them, there are 30 values for which they are non-hyperelliptic. See the [<a href="https://beta.lmfdb.org/ModularCurve/Q/?level=22%2C+26%2C+28%2C+30%2C+33%2C+34%2C+35%2C+37%2C+38%2C+39%2C+40%2C+42%2C+43%2C+44%2C+45%2C+48%2C+50%2C+51%2C+53%2C+54%2C+55%2C+56%2C+60%2C+61%2C+62%2C+63%2C+64%2C+65%2C+69%2C+72%2C+75%2C+79%2C+81%2C+83%2C+89%2C+92%2C+94%2C+95%2C+101%2C+119%2C+131&family=X0">following table</a>] for the full list and the [<a href="https://beta.lmfdb.org/ModularCurve/Q/?level=34%2C+38%2C+42%2C+43%2C+44%2C+45%2C+51%2C+53%2C+54%2C+55%2C+56%2C+60%2C+61%2C+62%2C+63%2C+64%2C+65%2C+69%2C+72%2C+75%2C+79%2C+81%2C+83%2C+89%2C+92%2C+94%2C+95%2C+102%2C+119%2C+131&family=X0">following table</a>] for the non-hyperelliptic list.'
 
-    @lazy_attribute
-    def cusps_display(self):
-        return self.cusps
-
-    @lazy_attribute
-    def cusps_width_display(self):
-        return "unknown"
 
     @lazy_attribute
     def cusps_orbits_display(self):
@@ -98,13 +88,6 @@ class X1N(ModCurveFamily_base):
     hypell_description = r'[<a href="https://mathscinet.ams.org/mathscinet/article?mr=1138196">MR:1138196</a>] showed that there are only 3 hyperelliptic curves in this family. See the [<a href="https://beta.lmfdb.org/ModularCurve/Q/?level=13%2C16%2C18&family=X1">following table</a>] for the full list.'
     biell_description = r'[<a href="https://mathscinet.ams.org/mathscinet/article?mr=2040593">MR:2040593</a>] showed that there are only 8 bielliptic curves in this family. See the [<a href="https://beta.lmfdb.org/ModularCurve/Q/?level=13%2C16%2C17%2C18%2C20%2C21%2C22%2C24&family=X1">following table</a>] for the full list.'
 
-    @lazy_attribute
-    def cusps_display(self):
-        return self.cusps
-
-    @lazy_attribute
-    def cusps_width_display(self):
-        return "unknown"
 
     @lazy_attribute
     def cusps_orbits_display(self):
@@ -134,14 +117,6 @@ class XN(ModCurveFamily_base):
     genus_formula = r"$$g = 1 + \frac{i}{12} - \frac{\nu_2}{4} - \frac{\nu_3}{3} - \frac{\nu_\infty}{2}$$"
     hypell_description = "None."
     biell_description = "None."
-
-    @lazy_attribute
-    def cusps_display(self):
-        return self.cusps
-
-    @lazy_attribute
-    def cusps_width_display(self):
-        return "unknown"
 
     @lazy_attribute
     def cusps_orbits_display(self):
@@ -174,14 +149,6 @@ class Xpm1N(ModCurveFamily_base):
     #biell_description = "Unknown."
 
     @lazy_attribute
-    def cusps_display(self):
-        return self.cusps
-
-    @lazy_attribute
-    def cusps_width_display(self):
-        return "unknown"
-
-    @lazy_attribute
     def cusps_orbits_display(self):
         return "one is rational"
 
@@ -209,14 +176,6 @@ class Xarith1MMN(ModCurveFamily_base):
     genus_formula = r"$$g = 1 + \frac{i}{12} - \frac{\nu_2}{4} - \frac{\nu_3}{3} - \frac{\nu_\infty}{2}$$"
     #hypell_description = "Unknown."
     #biell_description = "Unknown."
-
-    @lazy_attribute
-    def cusps_display(self):
-        return self.cusps
-
-    @lazy_attribute
-    def cusps_width_display(self):
-        return "unknown"
 
     @lazy_attribute
     def cusps_orbits_display(self):
@@ -247,14 +206,6 @@ class Xarithpm1MMN(ModCurveFamily_base):
     genus_formula = r"$$g = 1 + \frac{i}{12} - \frac{\nu_2}{4} - \frac{\nu_3}{3} - \frac{\nu_\infty}{2}$$"
     #hypell_description = "Unknown."
     #biell_description = "Unknown."
-
-    @lazy_attribute
-    def cusps_display(self):
-        return self.cusps
-
-    @lazy_attribute
-    def cusps_width_display(self):
-        return "unknown"
 
     @lazy_attribute
     def cusps_orbits_display(self):
@@ -288,14 +239,6 @@ class XspN(ModCurveFamily_base):
     #notation = r"<ul> <li>$\omega(N)$ is the number of primes dividing $N$.</li> </ul>"
 
     @lazy_attribute
-    def cusps_display(self):
-        return self.cusps
-
-    @lazy_attribute
-    def cusps_width_display(self):
-        return "unknown"
-
-    @lazy_attribute
     def cusps_orbits_display(self):
         return "one is rational"
 
@@ -325,14 +268,6 @@ class XspplusN(ModCurveFamily_base):
     #hypell_description = "Unknown."
     #biell_description = "Unknown."
     #notation = r"<ul> <li>$\delta = \begin{cases}1& \text{if } 2\mid N, \\ 0& \text{otherwise.}\end{cases}$ </li> </ul>"
-
-    @lazy_attribute
-    def cusps_display(self):
-        return self.cusps
-
-    @lazy_attribute
-    def cusps_width_display(self):
-        return "unknown"
 
     @lazy_attribute
     def cusps_orbits_display(self):
@@ -366,14 +301,6 @@ class XnsN(ModCurveFamily_base):
     notation = r"<ul> <li>$\omega(N)$ is the number of primes dividing $N$.</li> </ul>"
 
     @lazy_attribute
-    def cusps_display(self):
-        return self.cusps
-
-    @lazy_attribute
-    def cusps_width_display(self):
-        return "unknown"
-
-    @lazy_attribute
     def cusps_orbits_display(self):
         return "one is rational"
 
@@ -403,14 +330,6 @@ class XnsplusN(ModCurveFamily_base):
     #hypell_description = "Unknown."
     #biell_description = "Unknown."
     notation = r"<ul> <li>$\omega(N)$ is the number of primes dividing $N$.</li> <li>$S = \{a+b\alpha \in (\mathbb{Z}/N\mathbb{Z})[\alpha]^\times/\pm 1 : N(a+b\alpha) = -1, \operatorname{gcd}(b,N) >1\}.$ </li> </ul>"
-
-    @lazy_attribute
-    def cusps_display(self):
-        return self.cusps
-
-    @lazy_attribute
-    def cusps_width_display(self):
-        return "unknown"
 
     @lazy_attribute
     def cusps_orbits_display(self):
@@ -443,14 +362,6 @@ class XS4p(ModCurveFamily_base):
     #biell_description = "Unknown."
 
     @lazy_attribute
-    def cusps_display(self):
-        return self.cusps
-
-    @lazy_attribute
-    def cusps_width_display(self):
-        return "unknown"
-
-    @lazy_attribute
     def cusps_orbits_display(self):
         return "one is rational"
 
@@ -481,23 +392,16 @@ class XarithN(ModCurveFamily_base):
     hypell_description = r'[<a href="https://mathscinet.ams.org/mathscinet/article?mr=3145452">MR:3145452</a>] showed that there are no hyperelliptic curves in this family.'
     biell_description = r'[<a href="https://mathscinet.ams.org/mathscinet/article?mr=3145452">MR:3145452</a>] showed that there are only 2 bielliptic curves in this family. $X_{\textup{arith}}(7)$ is isomorphic to the Klein quartic and $X_{\textup{arith}}(8)$ is isomorphic to the Wiman curve. See the [<a href="https://beta.lmfdb.org/ModularCurve/Q/?level=7%2C8&family=Xarith">following table</a>] for the full list.'
 
-    @lazy_attribute
-    def cusps_display(self):
-        return self.cusps
-
-    @lazy_attribute
-    def cusps_width_display(self):
-        return "unknown"
 
     @lazy_attribute
     def cusps_orbits_display(self):
         return "one is rational"
 
     elliptic_points = "There are no elliptic points, unless N=1, in which case there are 2"
-
     @lazy_attribute
     def properties(self):
         return [("Level", "$N$"),
                 (r"$SL_2$-level", str(self.sl2level)),
                 ("Index", str(self.index)),
                 ("Genus", str(self.genus))]
+
