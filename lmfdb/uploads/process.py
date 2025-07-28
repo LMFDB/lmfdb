75179ca954dca364236689f83100fb9373db15d8
Edgar Costa <edgarc@mit.edu>
2025-07-28 13:39:13 -0400

    try to use utc_now_naive everywhere it makes sense

--- lmfdb/uploads/process.py
+++ lmfdb/uploads/process.py
@@ -8,8 +8,7 @@ import os
 import sys
 import tempfile
 from collections import defaultdict
-from datetime import datetime
-from lmfdb.utils.datetime_utils import UTC
+from lmfdb.utils.datetime_utils import utc_now_naive
 here = os.path.dirname(os.path.abspath(__file__))
 data_folder = os.path.join(here, "data")
 upone, _ = os.path.split(here)
@@ -52,7 +51,7 @@ def process_all():
             else:
                 status = 3
                 comment = ""
-            timestamp = datetime.now(UTC).isoformat()
+            timestamp = utc_now_naive().isoformat()
             status_update[rec["section"]][rec["id"]] = (status, timestamp, comment)
 
         # There are some sections (like gonality propagation) that want to do more
@@ -66,7 +65,7 @@ def process_all():
         db.data_uploads.update_from_file(F.name, "id")
         db.data_uploads.cleanup_from_reload()
         os.unlink(F.name)
-    timestamp = datetime.now(UTC).isoformat().replace(":", "-").replace("T", "-").replace(".", "-")
+    timestamp = utc_now_naive().isoformat().replace(":", "-").replace("T", "-").replace(".", "-")
     uploads = []
     for (table, newrows), lines in by_table.items():
         nr = "t" if newrows else "f"
