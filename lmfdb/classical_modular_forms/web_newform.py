62f3cd0c83ec72a8cb357b3a8fc7ad74e568f771
Eran Assaf <assaferan@gmail.com>
2025-12-12 11:00:31 -0500

    fixing the display of the space in the Hecke kernels

--- lmfdb/classical_modular_forms/web_newform.py
+++ lmfdb/classical_modular_forms/web_newform.py
@@ -713,7 +713,8 @@ class WebNewform():
             return r'no (minimal twist has level %s)' % (self.minimal_twist.split('.')[0]) if self.minimal_twist else r'no'
 
     def display_newspace(self):
-        s = r'\(S_{%s}^{\mathrm{new}}('
+        e_or_s = "S" if self.is_cuspidal else "E"
+        s = r'\(' + e_or_s + '_{%s}^{\mathrm{new}}('
         if self.char_order == 1:
             s += r'\Gamma_0(%s))\)'
         else:
