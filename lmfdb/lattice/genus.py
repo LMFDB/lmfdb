8a34e7defe4818d0cbfd34eeaa8a2d00bba24213
Eran Assaf <assaferan@gmail.com>
2026-02-22 23:53:37 -0500

    updating browse buckets

--- lmfdb/lattice/genus.py
+++ lmfdb/lattice/genus.py
@@ -52,14 +52,19 @@ def learnmore_list_remove(matchstring):
 # Webpages: main, random and search results #
 #############################################
 
+def buckets_from_endpoints(endpoints):
+    return [("%s..%s" % (start, end - 1 if end != 1 else -1) if start != end-1 else "%s" % start) for start, end in zip(endpoints[:-1], endpoints[1:])]
+
 def set_index_info(info, stats):
     # Prepare list of signatures to browse (display as round brackets, internally use square brackets)
     sig_list = sum([[[n-nm, nm] for nm in range(1 + (n//2))] for n in range(1, 10)], [])
     signature_list = [[str(s).replace(' ',''), str(tuple(s)).replace(' ','')] for s in sig_list[:16]]
-    dim_list = list(range(1, 13))
-    class_number_list = list(range(1, 31))
-    det_list_endpoints = [-1000, -100, -10, 1, 10, 100, 1000]
-    det_list = ["%s..%s" % (start, end - 1 if end != 1 else -1) for start, end in zip(det_list_endpoints[:-1], det_list_endpoints[1:])]
+    dim_list_endpoints = [1, 2, 3, 4] + [8*i for i in range(1,stats.max_rank//8 + 1)] + [stats.max_rank+1]
+    dim_list = buckets_from_endpoints(dim_list_endpoints)
+    cn_list_endpoints = [1, 2, 3, 4, 10, 100, stats.max_cn+1]
+    class_number_list = buckets_from_endpoints(cn_list_endpoints)
+    det_list_endpoints = [1, 10, 100, stats.max_det+1]
+    det_list = buckets_from_endpoints(det_list_endpoints)
     info.update({'dim_list': dim_list, 'signature_list': signature_list,
                  'det_list': det_list, 'class_number_list': class_number_list,
                  })
