6d11c692e004f6b1c2df66aa3bd7541d0f7e6825
David Roe <roed.math@gmail.com>
2026-03-04 13:52:26 -0500

    remove logger

--- lmfdb/shimura_curves/__init__.py
+++ lmfdb/shimura_curves/__init__.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from lmfdb.app import app
-from lmfdb.logger import make_logger
 from flask import Blueprint
 
 shimcurve_page = Blueprint("shimcurve", __name__, template_folder='templates')
-shimcurve_logger = make_logger(shimcurve_page)
 
 from . import main
 assert main # silence pyflakes
