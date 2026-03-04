bef3c7c1c7a2d9c579ff066b5ff71b28b118cea2
David Roe <roed.math@gmail.com>
2026-03-04 13:54:59 -0500

    Remove logger

--- lmfdb/hilbert_modular_surfaces/__init__.py
+++ lmfdb/hilbert_modular_surfaces/__init__.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from lmfdb.app import app
-from lmfdb.logger import make_logger
 from flask import Blueprint
 
 hmsurface_page = Blueprint("hmsurface", __name__, template_folder='templates')
-hmsurface_logger = make_logger(hmsurface_page)
 
 from . import main
 assert main # silence pyflakes
